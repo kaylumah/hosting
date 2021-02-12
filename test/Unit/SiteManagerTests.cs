14d3be7857d78ccf69937ab613d9f2c35b2af17a
Max Hamulyák <max@kaylumah.nl>
2021-02-12 15:24:58 +0100

    fix test

--- test/Unit/SiteManagerTests.cs
+++ test/Unit/SiteManagerTests.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.FileProviders;
 using Moq;
 using Xunit;
+using System.Collections.Generic;
 
 namespace Test.Unit
 {
@@ -13,9 +14,12 @@ namespace Test.Unit
         [Fact]
         public async Task Test_SiteManager_GenerateSite()
         {
+            var directoryContentsMock = new Mock<IDirectoryContents>();
+            directoryContentsMock.Setup(dc => dc.GetEnumerator()).Returns(new List<IFileInfo>().GetEnumerator());
+            directoryContentsMock.Setup(dc => dc.Exists).Returns(false);
             var loggerMock = new Mock<ILogger<SiteManager>>();
             var fileProviderMock = new Mock<IFileProvider>();
-
+            fileProviderMock.Setup(x => x.GetDirectoryContents(It.IsAny<string>())).Returns(directoryContentsMock.Object);
             ISiteManager sut = new SiteManager(fileProviderMock.Object, loggerMock.Object);
             await sut.GenerateSite();
         }
