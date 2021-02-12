6b150e26ac73e9f2341648bbce901aa5bb5ed9b6
Max Hamulyák <max@kaylumah.nl>
2021-02-12 20:42:50 +0100

    include serializer

--- src/Component/Manager/Site/Service/LayoutMetadata.cs
+++ src/Component/Manager/Site/Service/LayoutMetadata.cs
@@ -1,7 +1,10 @@
+using YamlDotNet.Serialization;
+
 namespace Kaylumah.Ssg.Manager.Site.Service
 {
     public class LayoutMetadata
     {
+        [YamlMember(Alias = "layout")]
         public string Layout { get; set; }
     }
 }
