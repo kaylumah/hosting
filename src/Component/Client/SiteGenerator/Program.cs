dff6cb10b003794af8666db136b2727af192f821
Max Hamulyák <max@kaylumah.nl>
2021-02-12 13:14:45 +0100

    add console application

---
+++ src/Component/Client/SiteGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kaylumah.Ssg.Client.SiteGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
