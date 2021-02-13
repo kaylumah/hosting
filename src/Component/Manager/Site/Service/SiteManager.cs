aee718db5cb78014e609235fab9c87856ff53978
Max Hamulyák <max@kaylumah.nl>
2021-02-13 15:48:42 +0100

    seperate file

--- src/Component/Manager/Site/Service/SiteManager.cs
+++ src/Component/Manager/Site/Service/SiteManager.cs
@@ -106,23 +106,7 @@ namespace Kaylumah.Ssg.Manager.Site.Service
         {
         }
     }
-
-    class RenderData
-    {
-        public string Content { get;set; }
-        public SiteData Site { get;set; } = new SiteData();
-        public PageData Page { get;set; } = new PageData();
-    }
-
-    class SiteData
-    {
-        public string Title { get;set; } = $"{nameof(SiteData)}{nameof(Title)}";
-    }
-    class PageData
-    {
-        public string Title { get;set; } = $"{nameof(PageData)}{nameof(Title)}";
-    }
-
+    
     public interface IContentStrategy
     {
         bool ShouldExecute(IFileInfo file);
