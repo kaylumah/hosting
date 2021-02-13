5a4efbededbb41bad19043b63599ae75dcf16f74
Max Hamulyák <max@kaylumah.nl>
2021-02-13 21:02:07 +0100

    update post data

--- src/Component/Manager/Site/Service/SiteManager.cs
+++ src/Component/Manager/Site/Service/SiteManager.cs
@@ -227,7 +227,32 @@ namespace Kaylumah.Ssg.Manager.Site.Service
                 {
                     Data = ParseData(dataDir),
                     Collections = new Dictionary<string, object> {
-                        { "pages", new object[] { "1", "2" }}
+                        { 
+                            "pages",
+                            new object[] {
+                                new {
+                                    Uri = "https://kaylumah.nl",
+                                    Image = "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixqx=ek9gmnUEHF&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80", 
+                                    Type = "Article",
+                                    Title = "Boost your conversion rate",
+                                    Description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum."
+                                },
+                                new {
+                                    Uri = "https://kaylumah.nl",
+                                    Image = "https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=ek9gmnUEHF&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+                                    Type = "Video",
+                                    Title = "How to use search engine optimization to drive sales",
+                                    Description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium."
+                                },
+                                new {
+                                    Uri = "https://kaylumah.nl",
+                                    Image = "https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixqx=ek9gmnUEHF&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80",
+                                    Type = "Case Study",
+                                    Title = "Improve your customer experience",
+                                    Description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic."
+                                }
+                            }
+                        }
                     }
                 };
 
