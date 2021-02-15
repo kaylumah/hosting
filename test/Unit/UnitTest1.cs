52d2f3bcac36b4aacf9812686e0bef105cb63d34
Max Hamulyák <max@kaylumah.nl>
2021-02-15 17:23:54 +0100

    sanatize fake filesystem

--- test/Unit/UnitTest1.cs
+++ test/Unit/UnitTest1.cs
@@ -12,105 +12,31 @@ namespace Test.Unit
 {
     public class UnitTest1
     {
-
-        public void B(Mock<IFileProvider> fileProvider, string rootDirectory, FakeDirectory current, List<FakeDirectory> directories)
-        {
-            var subDirectories = directories.Where(x => x.FolderPath.Equals(current.Name)).ToList();
-            directories = directories.Except(subDirectories).ToList();
-            var fileInfos = new List<IFileInfo>();
-            foreach(var file in current.Files)
-            {
-                var fileMock = new Mock<IFileInfo>();
-                var relativePath = Path.Combine(current.Name, file.FilePath);
-                var physicalPath = Path.Combine(rootDirectory, relativePath);
-                fileMock.Setup(x => x.Name).Returns(file.FilePath);
-                fileInfos.Add(fileMock.Object);
-                fileProvider.Setup(x => x.GetFileInfo(It.Is<string>(p => p.Equals(relativePath))))
-                    .Returns(fileMock.Object);
-            }
-
-            foreach(var directory in subDirectories)
-            {
-                var fileMock = new Mock<IFileInfo>();
-                fileMock.Setup(x => x.Name).Returns(directory.Name);
-                fileInfos.Add(fileMock.Object);
-                B(fileProvider, rootDirectory, directory, directories);
-            }
-
-            fileProvider
-                .Setup(x => x.GetDirectoryContents(It.Is<string>(p => p.Equals(current.Name))))
-                .Returns(fileInfos.CreateMock<IDirectoryContents, IFileInfo>().Object);
-        }
-
-        public void A (Mock<IFileProvider> fileProvider, string rootDirectory, List<FakeDirectory> directories)
-        {
-            var root = directories.SingleOrDefault(x => x.Name == string.Empty);
-            directories.Remove(root);
-            B(fileProvider, rootDirectory, root, directories);
-        }
-
-
-        private List<object> Process(Mock<IFileProvider> fileProvider, List<FakeDirectory> directories, string basePath = "", int depth = 1)
-        {
-            var result = new List<object>();
-            var targets = directories.Where(x => x.Depth == depth && x.FolderPath.Equals(basePath)).ToList();
-            foreach(var target in targets)
-            {
-                directories.Remove(target);
-            }
-            foreach(var target in targets)
-            {
-                var subTargets = Process(fileProvider, directories, target.Name, depth + 1);
-                
-
-
-                // result.Add(new { Name = target.Name });
-                result.AddRange(subTargets);
-            }
-            return result;
-        }
-
         [Fact]
-        public void Test2()
+        public void Test1()
         {
-            var provider = new Mock<IFileProvider>();
-            var directories = new List<FakeDirectory>()
-            {
-                new FakeDirectory("", new FakeFile[] {
+            var root = "/a/b/c/";
+            var directories = new List<FakeDirectory>() {
+                new FakeDirectory(string.Empty, new FakeFile[] {
                     new FakeFile("index.html")
                 }),
                 new FakeDirectory("assets", new FakeFile[] {}),
                 new FakeDirectory("assets/css", new FakeFile[] {
-                    new FakeFile("styles.css")
+                    new FakeFile("assets/css/styles.css")
                 })
             };
-            var root = "/a/b/c/";
-            A(provider, root, directories);
+            var providerMock = new Mock<IFileProvider>()
+                .SetupFileProviderMock(root, directories);
+            var fileProvider = providerMock.Object;
 
-            var build = provider.Object;
+            var rootDirectoryDirectoryContents = fileProvider.GetDirectoryContents("");
+            var rootDirectoryFileInfo = fileProvider.GetFileInfo("");
 
-            var rootDir = build.GetDirectoryContents(string.Empty);
-            var assets = build.GetDirectoryContents("assets");
-            var cssAssets = build.GetDirectoryContents("assets/css");
+            var assets = fileProvider.GetDirectoryContents("assets");
+            var cssAssets = fileProvider.GetDirectoryContents("assets/css");
 
-            var index = build.GetFileInfo("index.html");
-            var css = build.GetFileInfo("assets/css/styles.css");
-        }
-
-        [Fact]
-        public void Test1()
-        {
-            var provider = new Mock<IFileProvider>();
-            var directories = new List<FakeDirectory>()
-            {
-                new FakeDirectory("assets", new FakeFile[] {}),
-                new FakeDirectory("assets/css", new FakeFile[] {}),
-                new FakeDirectory("assets/js", new FakeFile[] {}),
-                new FakeDirectory("assets/images", new FakeFile[] {}),
-                // new FakeDirectory("posts", new FakeFile[] {}),
-                new FakeDirectory("posts/january", new FakeFile[] {})
-            };
-            var y = Process(provider, directories);
+            var index = fileProvider.GetFileInfo("index.html");
+            var css = fileProvider.GetFileInfo("assets/css/styles.css");
         }
     }
 }
--- test/Utilities/FileProviderExtensions.cs
+++ test/Utilities/FileProviderExtensions.cs
@@ -4,11 +4,124 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.FileProviders.Physical;
 using Moq;
 
 namespace Test.Utilities
 {
+
+    public static class FileProviderExtensions
+    {
+        public static Mock<IFileProvider> SetupFileProviderMock(this Mock<IFileProvider> fileProviderMock, string root, List<FakeDirectory> directories)
+        {
+            var rootDirectory = directories.SingleOrDefault(x => x.Name == string.Empty);
+            if (rootDirectory != null)
+            {
+                directories.Remove(rootDirectory);
+            }
+            ProcessDirectory(fileProviderMock, root, rootDirectory, directories);
+            return fileProviderMock;
+        }
+
+        public static IFileInfo CreateDirectory(this Mock<IFileProvider> fileProviderMock, string root, string folderPath)
+        {
+            var directoryMock = new Mock<IFileInfo>();
+            var directoryName = folderPath.Split(Path.DirectorySeparatorChar).Last();
+            directoryMock.Setup(x => x.IsDirectory).Returns(false);
+            directoryMock.Setup(x => x.Length).Throws(new FileNotFoundException());
+            directoryMock.Setup(x => x.CreateReadStream()).Throws(new UnauthorizedAccessException());
+            directoryMock.Setup(x => x.Exists).Returns(true);
+            directoryMock.Setup(x => x.PhysicalPath).Returns(Path.Combine(root, folderPath));
+            directoryMock.Setup(x => x.Name).Returns(directoryName);
+
+            var directory = directoryMock.Object;
+            fileProviderMock.Setup(x => x.GetFileInfo(It.Is<string>(p => p.Equals(folderPath)))).Returns(directory);
+            return directory;
+        }
+
+        public static IFileInfo CreateFile(this Mock<IFileProvider> fileProviderMock, string root, string filePath, byte[] bytes)
+        {
+            var fileMock = new Mock<IFileInfo>();
+            var fileName = filePath.Split(Path.DirectorySeparatorChar).Last();
+            fileMock.Setup(x => x.IsDirectory).Returns(false);
+            fileMock.Setup(x => x.Length).Returns(() => new MemoryStream(bytes).Length);
+            fileMock.Setup(x => x.CreateReadStream()).Returns(() => new MemoryStream(bytes));
+            fileMock.Setup(x => x.Exists).Returns(true);
+            fileMock.Setup(x => x.PhysicalPath).Returns(Path.Combine(root, filePath));
+            fileMock.Setup(x => x.Name).Returns(fileName);
+
+            var file = fileMock.Object;
+            fileProviderMock.Setup(x => x.GetFileInfo(It.Is<string>(p => p.Equals(filePath)))).Returns(file);
+            return file;
+        }
+
+        public static Mock<IFileProvider> AddFile(this Mock<IFileProvider> fileProviderMock, string root, string filePath, byte[] bytes)
+        {
+            fileProviderMock.CreateFile(root, filePath, bytes);
+            return fileProviderMock;
+        }
+        public static Mock<IFileProvider> AddDirectory(this Mock<IFileProvider> fileProviderMock, string root, string folderPath)
+        {
+            fileProviderMock.CreateDirectory(root, folderPath);
+            return fileProviderMock;
+        }
+
+        private static void ProcessDirectory(Mock<IFileProvider> fileProviderMock, string root, FakeDirectory current, List<FakeDirectory> directories)
+        {
+            var subDirectories = directories.Where(x => x.FolderPath.Equals(current.Name)).ToList();
+            directories = directories.Except(subDirectories).ToList();
+            var fileInfos = new List<IFileInfo>();
+            foreach (var file in current.Files)
+            {
+                var fileInfo = fileProviderMock.CreateFile(root, file.FilePath, new byte[] {});
+                fileInfos.Add(fileInfo);
+            }
+            foreach (var directory in subDirectories)
+            {
+                var directoryInfo = fileProviderMock.CreateDirectory(root, directory.FolderPath);
+                fileInfos.Add(directoryInfo);
+                ProcessDirectory(fileProviderMock, root, directory, directories);
+            }
+
+            var directoryContents = fileInfos.CreateMock<IDirectoryContents, IFileInfo>();
+            fileProviderMock
+                .Setup(x => x.GetDirectoryContents(It.Is<string>(p => p.Equals(current.Name))))
+                .Returns(directoryContents.Object);
+        }
+    }
+
+    public class FakeFile
+    {
+        public string FilePath { get; }
+        public FakeFile(string relativePath)
+        {
+            FilePath = relativePath;
+        }
+    }
+
+    public class FakeDirectory
+    {
+        public string FolderPath { get; set; }
+        public string Name { get; }
+        public int Depth { get; }
+        public FakeFile[] Files { get; }
+
+        public FakeDirectory(string name, FakeFile[] files)
+        {
+            Name = name;
+            Depth = Name.Split(Path.DirectorySeparatorChar).Count();
+            var index = Name.LastIndexOf(Path.DirectorySeparatorChar);
+            if (index > 0)
+            {
+                FolderPath = name.Substring(0, index);
+            }
+            else
+            {
+                FolderPath = string.Empty;
+            }
+
+            Files = files;
+        }
+    }
 
     public static class EnumerableExtensions
     {
@@ -22,88 +135,5 @@ namespace Test.Utilities
                 mock.Setup(m => m.GetEnumerator()).Returns(() => collection.GetEnumerator());
                 return mock;
             }
-    }
-
-    public static class FileProviderExtensions
-    {
-
-        public class FakeFile
-        {
-            public string FilePath { get; }
-            public FakeFile(string relativePath)
-            {
-                FilePath = relativePath;
-            }
-        }
-
-        public class FakeDirectory
-        {
-            public string FolderPath { get;set; }
-            public string Name { get; }
-            public int Depth { get; }
-            public FakeFile[] Files { get; }
-
-            public FakeDirectory(string name, FakeFile[] files)
-            {
-                Name = name;
-                Depth = Name.Split(Path.DirectorySeparatorChar).Count();
-                var index = Name.LastIndexOf(Path.DirectorySeparatorChar);
-                if (index > 0)
-                {
-                    FolderPath = name.Substring(0, index);
-                }
-                else
-                {
-                    FolderPath = string.Empty;
-                }
-
-                Files = files;
-            }
-        }
-
-        public static Mock<IFileProvider> SetupFileSystem(this Mock<IFileProvider> fileProviderMock,
-            string rootPath,
-            FakeDirectory[] directories)
-        {
-            var x = new List<IFileInfo>();
-            x.AddRange(new List<IFileInfo>());
-            x.AddRange(new List<IFileInfo>());
-
-            var directoryListing = x.CreateMock<IDirectoryContents, IFileInfo>().Object;
-
-            return fileProviderMock;
-        }
-
-        public static Mock<IFileProvider> AddFakeDirectory(this Mock<IFileProvider> fileProviderMock, string root, string folderPath)
-        {
-            var directoryMock = new Mock<IFileInfo>();
-            var directoryName = folderPath.Split(Path.DirectorySeparatorChar).Last();
-            directoryMock.Setup(x => x.IsDirectory).Returns(false);
-            directoryMock.Setup(x => x.Length).Throws(new FileNotFoundException());
-            directoryMock.Setup(x => x.CreateReadStream()).Throws(new UnauthorizedAccessException());
-            directoryMock.Setup(x => x.Exists).Returns(true);
-            directoryMock.Setup(x => x.PhysicalPath).Returns(Path.Combine(root, folderPath));
-            directoryMock.Setup(x => x.Name).Returns(directoryName);
-
-            fileProviderMock.Setup(x => x.GetFileInfo(It.Is<string>(p => p.Equals(folderPath)))).Returns(
-                directoryMock.Object);
-            return fileProviderMock;
-        }
-
-        public static Mock<IFileProvider> AddFakeFile(this Mock<IFileProvider> fileProviderMock, string root, string filePath, byte[] bytes)
-        {
-            var fileMock = new Mock<IFileInfo>();
-            var fileName = filePath.Split(Path.DirectorySeparatorChar).Last();
-            fileMock.Setup(x => x.IsDirectory).Returns(false);
-            fileMock.Setup(x => x.Length).Returns(() => new MemoryStream(bytes).Length);
-            fileMock.Setup(x => x.CreateReadStream()).Returns(() => new MemoryStream(bytes));
-            fileMock.Setup(x => x.Exists).Returns(true);
-            fileMock.Setup(x => x.PhysicalPath).Returns(Path.Combine(root, filePath));
-            fileMock.Setup(x => x.Name).Returns(fileName);
-
-            fileProviderMock.Setup(x => x.GetFileInfo(It.Is<string>(p => p.Equals(filePath)))).Returns(
-                fileMock.Object);
-            return fileProviderMock;
-        }
     }
 }
