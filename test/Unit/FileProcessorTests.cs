baa2fbba861d34d33f089d4740d68c445e93b29e
Max Hamulyák <max@kaylumah.nl>
2021-02-22 11:02:59 +0100

    add initial frontmatter tests

--- test/Unit/FileProcessorTests.cs
+++ test/Unit/FileProcessorTests.cs
@@ -20,6 +20,59 @@ namespace Test.Unit
     public class FileProcessorTests
     {
         [Fact]
+        public async Task Test_FileProcessor_FrontMatter()
+        {
+            var root = "/a/b/c";
+            var optionsMock = Options.Create(new SiteInfo() { });
+            var loggerMock = new Mock<ILogger<FileProcessor>>();
+            var fileProviderMock = new Mock<IFileProvider>()
+                .SetupFileProviderMock(
+                    root,
+                    new List<FakeDirectory>() {
+                        new FakeDirectory(string.Empty, new FakeFile[] {
+                            // File without any frontmatter
+                            new FakeFile("a.txt", Encoding.UTF8.GetBytes(string.Empty)),
+                            // File with empty frontmatter
+                            new FakeFile("b.txt", Encoding.UTF8.GetBytes(
+                                CreateFrontMatter()
+                            )),
+                            // File with frontmatter but only new keys
+                            new FakeFile("c.txt", Encoding.UTF8.GetBytes(
+                                CreateFrontMatter(new Dictionary<string, object> {
+                                    { "tags", new string[] { "A" } }
+                                })
+                            )),
+                            // File writh frontmatter, but overwritten keys
+                            new FakeFile("d.txt", Encoding.UTF8.GetBytes(
+                                CreateFrontMatter(new Dictionary<string, object> {
+                                   
+                                })
+                            )),
+                        })
+                    }
+                );
+            var fileSystem = new FileSystem(fileProviderMock.Object);
+            var sut = new FileProcessor(fileSystem, loggerMock.Object, new IContentPreprocessorStrategy[] { }, optionsMock);
+            var result = await sut.Process(new FileFilterCriteria
+            {
+                DirectoriesToSkip = new string[] { },
+                FileExtensionsToTarget = new string[] { ".txt" }
+            });
+            result.Should().NotBeEmpty();
+            result.Count().Should().Be(4);
+
+            var fileA = result.Single(x => x.Name.Equals("a.txt"));
+            var fileB = result.Single(x => x.Name.Equals("b.txt"));
+            var fileC = result.Single(x => x.Name.Equals("c.txt"));
+            var fileD = result.Single(x => x.Name.Equals("d.txt"));
+
+            fileA.MetaData.Count.Should().Be(1);
+            fileB.MetaData.Count.Should().Be(1);
+            fileC.MetaData.Count.Should().Be(2, "Default keys + one new keys equals 2");
+            fileD.MetaData.Count.Should().Be(1);
+        }
+
+        [Fact]
         public async Task Test_FileProcessor_WithoutFiles_Should_ReturnEmptyList()
         {
             var root = "/a/b/c";
