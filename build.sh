7afd2a81d0068b8192befa96f36a20ab56c94f16
Max Hamulyák <max@kaylumah.nl>
2021-02-22 20:17:00 +0100

    action names

--- build.sh
+++ build.sh
@@ -10,4 +10,8 @@ dotnet build --configuration $CONFIGURATION --no-restore
 
 dotnet test --configuration $CONFIGURATION --no-build --verbosity normal
 
+# dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=lcov
 # dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./lcov.info
+
+# Generate coverage report
+# Publish coverage report
