3ee552b91bdef595425d0963f7be1bc270b88e96
Max Hamulyák <max@kaylumah.nl>
2021-02-23 19:31:45 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -8,12 +8,13 @@ dotnet restore
 
 dotnet build --configuration $CONFIGURATION --no-restore
 
-dotnet test --configuration $CONFIGURATION --no-build --verbosity normal
+# dotnet test --configuration $CONFIGURATION --no-build --verbosity normal
+dotnet test --configuration $CONFIGURATION --no-build --verbosity normal /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=TestResults/lcov.info
 
 # dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=lcov
 # dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./lcov.info
+# dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=TestResults/lcov.info
 
 # Generate coverage report
 # Publish coverage report
 
-# dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./lcov.info
