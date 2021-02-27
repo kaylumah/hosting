5dbd46bffa325a1ae2a9ee65eed324aaaf3579d2
Max Hamulyák <max@kaylumah.nl>
2021-02-27 14:06:01 +0100

    add debugger display

--- src/Component/Manager/Site/Service/Files/File.cs
+++ src/Component/Manager/Site/Service/Files/File.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics;
+
 namespace Kaylumah.Ssg.Manager.Site.Service
 {
+    [DebuggerDisplay("File (Name={Name})")]
     public class File
     {
         public FileMetaData MetaData { get; set; }
