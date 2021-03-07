6e4301b862f17f10d38a15bf83a1cc6f6efbd0ee
Max Hamulyák <max@kaylumah.nl>
2021-03-07 16:15:51 +0100

    WIP validate stuff

--- src/Component/Client/SiteValidator/Program.cs
+++ src/Component/Client/SiteValidator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using HtmlAgilityPack;
@@ -12,12 +13,16 @@ namespace Kaylumah.Ssg.Client.SiteValidator
             var path = Path.Combine(Environment.CurrentDirectory, "dist");
             if (Directory.Exists(path))
             {
-                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories).ToList();
+                var bannedDirectories = new string[] { "NODE_MODULES" };
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories)
+                    .Where(s => !bannedDirectories.Any(d => Path.GetDirectoryName(s).ToUpper().Contains(d)))
+                    .ToList();
 
                 var htmlFiles = files.Where(file => ".html".Equals(Path.GetExtension(file))).ToList();
 
                 // https://html-agility-pack.net/knowledge-base/16645257/how-to-use-html-agility-pack-for-html-validations
                 // https://html-agility-pack.net/knowledge-base/2354653/grabbing-meta-tags-and-comments-using-html-agility-pack
+                // https://html-agility-pack.net/knowledge-base/25688847/html-agility-pack-get-all-anchors--href-attributes-on-page
                 foreach (var html in htmlFiles)
                 {
                     Console.WriteLine($"Validating {html}");
@@ -37,6 +42,33 @@ namespace Kaylumah.Ssg.Client.SiteValidator
                     var head = document.DocumentNode.SelectSingleNode("html/head");
                     var metaTags = head.SelectNodes("meta");
                     var titleTag = head.SelectSingleNode("title");
+
+                    var body = document.DocumentNode.SelectSingleNode("html/body");
+                    var links = new HashSet<string>();
+                    foreach(var link in body.SelectNodes("//a[@href]"))
+                    {
+                        string hrefValue = link.GetAttributeValue("href", string.Empty);
+                        links.Add(hrefValue);
+                    }
+                    var uris = links.Where(x => !string.IsNullOrEmpty(x) && "#" != x && "/" != x).Select(x => {
+                        if (x.StartsWith("http://") || x.StartsWith("https://"))
+                        {
+                            return new Uri(x);
+                        }
+                        return new Uri(x, UriKind.Relative);
+                    }).ToList();
+
+                    var fileUris = uris.Where(x => !x.IsAbsoluteUri);
+
+                   foreach (var fileUri in fileUris)
+                   {
+                       if (!files.Any(file => file.EndsWith(fileUri.ToString())))
+                       {
+                           Console.WriteLine($"Failed to find {fileUri}");
+                       }
+                   }
+
+                    // doc.DocumentNode.SelectNodes("")
                 }
             }
         }
