5a12c91814f4660127771530cefc3d5501a13704
Max Hamulyák <max@kaylumah.nl>
2021-03-07 15:13:10 +0100

    add SiteValidator

---
+++ src/Component/Client/SiteValidator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kaylumah.Ssg.Client.SiteValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
