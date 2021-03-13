5331120ec2d9b37610b7bfaf27af0b07dad3b631
Max Hamulyák <max@kaylumah.nl>
2021-03-13 10:14:23 +0100

    include cookie

--- tools/screenshot/test.js
+++ tools/screenshot/test.js
@@ -1,13 +1,29 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const urls = [];
+    const urls = [
+        'https://www.nuget.org'
+    ];
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     for (var i = 0; i < urls.length; i++)
     {
         var url = urls[i];
         const page = await browser.newPage();
         await page.goto(url);
+
+        // // https://stackoverflow.com/questions/59618456/pupeteer-how-can-i-accept-cookie-consent-prompts-automatically-for-any-url
+        // await page.evaluate(_ => {
+        //     function xcc_contains(selector, text) {
+        //         var elements = document.querySelectorAll(selector);
+        //         return Array.prototype.filter.call(elements, function (element) {
+        //             return RegExp(text, "i").test(element.textContent.trim());
+        //         });
+        //     }
+        //     var _xcc;
+        //     _xcc = xcc_contains('[id*=cookie] a, [class*=cookie] a, [id*=cookie] button, [class*=cookie] button', '^(Alle akzeptieren|Akzeptieren|Verstanden|Zustimmen|Okay|OK|Accept all)$');
+        //     if (_xcc != null && _xcc.length != 0) { _xcc[0].click(); }
+        // });
+
         await page.screenshot({ path: `screenshot-${i}.png` });
     }
 
