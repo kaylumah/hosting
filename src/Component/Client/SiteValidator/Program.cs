2601c1d71ddb64234d18b6a13ca8c5ded58f16c1
Max Hamulyák <max@kaylumah.nl>
2021-03-13 18:15:02 +0100

    wip validator

--- src/Component/Client/SiteValidator/Program.cs
+++ src/Component/Client/SiteValidator/Program.cs
@@ -6,10 +6,44 @@ using HtmlAgilityPack;
 
 namespace Kaylumah.Ssg.Client.SiteValidator
 {
+
+    public class MarkupErrors
+    {
+        public string ErrorCode { get; set; }
+        public string ErrorReason { get; set; }
+    }
+
+    interface IFilter
+    {
+        bool Validate(HtmlDocument document);
+    }
+
+    class TagFilter : IFilter
+    {
+        private readonly string _path;
+
+        public TagFilter(string path)
+        {
+            _path = path;
+        }
+
+        public bool Validate(HtmlDocument document)
+        {
+            var result = document.DocumentNode.SelectNodes(_path).SingleOrDefault();
+            return result != null;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var rules = new IFilter[] {
+                new TagFilter("//meta[@name='description']")
+            };
+
+
+
             var path = Path.Combine(Environment.CurrentDirectory, "dist");
             if (Directory.Exists(path))
             {
@@ -18,7 +52,7 @@ namespace Kaylumah.Ssg.Client.SiteValidator
                     .Where(s => !bannedDirectories.Any(d => Path.GetDirectoryName(s).ToUpper().Contains(d)))
                     .ToList();
 
-                var htmlFiles = files.Where(file => ".html".Equals(Path.GetExtension(file))).ToList();
+                var htmlFiles = files.Where(file => ".html".Equals(Path.GetExtension(file))).Take(1).ToList();
 
                 // https://html-agility-pack.net/knowledge-base/16645257/how-to-use-html-agility-pack-for-html-validations
                 // https://html-agility-pack.net/knowledge-base/2354653/grabbing-meta-tags-and-comments-using-html-agility-pack
@@ -33,7 +67,22 @@ namespace Kaylumah.Ssg.Client.SiteValidator
                     };
                     document.LoadHtml(File.ReadAllText(html));
 
-                    var errors = document.ParseErrors;
+                    var errors = new List<MarkupErrors>();
+                    foreach (var error in document.ParseErrors)
+                    {
+                        errors.Add(new MarkupErrors
+                        {
+                            ErrorCode = error.Code.ToString(),
+                            ErrorReason = error.Reason
+                        });
+                    }
+
+                    foreach (var rule in rules)
+                    {
+                        var result = rule.Validate(document);
+                        var type = rule.GetType();
+                    }
+
                     bool hasHead = document.DocumentNode.SelectSingleNode("html/head") != null;
                     bool hasBody = document.DocumentNode.SelectSingleNode("html/body") != null;
 
