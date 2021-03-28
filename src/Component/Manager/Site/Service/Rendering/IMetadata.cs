b6a518e63f7ef5e195aa29d27968675400e94686
Max Hamulyák <max@kaylumah.nl>
2021-03-28 11:57:11 +0200

    move to seperate files

---
+++ src/Component/Manager/Site/Service/Rendering/IMetadata.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Kaylumah, 2021. All rights reserved.
+// See LICENSE file in the project root for full license information.
+namespace Kaylumah.Ssg.Manager.Site.Service
+{
+    public interface IMetadata
+    {
+        string Title { get; }
+        string Description { get; }
+        string Language { get; }
+        string Author { get; }
+    }
+}
