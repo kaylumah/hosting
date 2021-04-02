c18c1eb2f7cac7d908f29e2338539bf1520e3825
Max Hamulyák <max@kaylumah.nl>
2021-04-02 09:48:01 +0200

    update dev script

--- build.sh
+++ build.sh
@@ -1,11 +1,42 @@
 #!/bin/sh
 
+# https://unix.stackexchange.com/questions/129391/passing-named-arguments-to-shell-scripts
+for ARGUMENT in "$@"
+do
+
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
+
+    case "$KEY" in
+            BUILD_ID)              BUILD_ID=${VALUE} ;;
+            BUILD_NUMBER)          BUILD_NUMBER=${VALUE} ;;  
+            *)   
+    esac    
+
+
+done
+
+if [ -z "$BUILD_ID" ]
+then
+      echo "BUILD_ID is empty setting default value..."
+      BUILD_ID=1
+fi
+
+if [ -z "$BUILD_NUMBER" ]
+then
+      echo "BUILD_NUMBER is empty setting default value..."
+      BUILD_NUMBER=$(date +"%Y%m%d.%H%M%S")
+fi
+
+echo "BUILD_ID = '$BUILD_ID'"
+echo "BUILD_NUMBER = '$BUILD_NUMBER'"
+
 _cwd="$PWD"
 CONFIGURATION=Release
 
 dotnet restore
 
-dotnet build --configuration $CONFIGURATION --no-restore
+dotnet build --configuration $CONFIGURATION --no-restore /p:BuildId=$BUILD_ID /p:BuildNumber=$BUILD_NUMBER
 
 # dotnet test --configuration $CONFIGURATION --no-build --verbosity normal
 #dotnet test --configuration $CONFIGURATION --no-build --verbosity normal /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=TestResults/lcov.info
