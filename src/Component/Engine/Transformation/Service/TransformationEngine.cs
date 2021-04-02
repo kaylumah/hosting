f5ece63b28b22bced1d00ea3f563efff7303b2fa
Max Hamulyák <max@kaylumah.nl>
2021-04-02 14:09:24 +0200

    add empty project

---
+++ src/Component/Engine/Transformation/Service/TransformationEngine.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Kaylumah, 2021. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Kaylumah.Ssg.Engine.Transformation.Interface;
+
+namespace Kaylumah.Ssg.Engine.Transformation.Service
+{
+    public class TransformationEngine : ITransformationEngine
+    {
+    }
+}
