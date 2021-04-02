6a78666fec34d5c88bf1e607f01c5d90eedd9116
Max Hamulyák <max@kaylumah.nl>
2021-04-02 12:41:07 +0200

    simplify script

--- build.sh
+++ build.sh
@@ -35,17 +35,10 @@ _cwd="$PWD"
 CONFIGURATION=Release
 
 dotnet restore
-
+# build with MSBuild vars
 dotnet build --configuration $CONFIGURATION --no-restore /p:BuildId=$BUILD_ID /p:BuildNumber=$BUILD_NUMBER
-
-# dotnet test --configuration $CONFIGURATION --no-build --verbosity normal
-#dotnet test --configuration $CONFIGURATION --no-build --verbosity normal /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=TestResults/lcov.info
-
-# dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=lcov
-# dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=./lcov.info
-# dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=TestResults/lcov.info
-
-# Generate coverage report
+# test with coverage
+dotnet test --configuration $CONFIGURATION --no-build --verbosity normal /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=TestResults/lcov.info
 # Publish coverage report
 dotnet "artifacts/bin/Kaylumah.Ssg.Client.SiteGenerator/$CONFIGURATION/netcoreapp3.1/Kaylumah.Ssg.Client.SiteGenerator.dll" SiteConfiguration:AssetDirectory=assets
 
