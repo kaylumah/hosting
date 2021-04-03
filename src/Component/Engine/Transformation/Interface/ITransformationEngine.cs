5bf3087c963da0e515c65ebc30e5629f51258a7b
Max Hamulyák <max@kaylumah.nl>
2021-04-03 15:17:36 +0200

    move to seperate files

--- src/Component/Engine/Transformation/Interface/ITransformationEngine.cs
+++ src/Component/Engine/Transformation/Interface/ITransformationEngine.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Kaylumah, 2021. All rights reserved.
 // See LICENSE file in the project root for full license information.
-using System.Threading.Tasks;
 
 namespace Kaylumah.Ssg.Engine.Transformation.Interface
 {
@@ -8,29 +7,11 @@ namespace Kaylumah.Ssg.Engine.Transformation.Interface
     {
     }
 
-    public interface IMetadataRenderer
-    {
-        Task<MetadataRenderResult[]> Render(MetadataRenderRequest[] requests);
-    }
 
-    public class MetadataRenderRequest
-    {
-        public IMetadata Metadata { get;set; }
-        public string Template { get;set; }
-    }
 
-    public class MetadataRenderResult
-    {
-        public string Content { get; set; }
-    }
+
+
+
     
-    public interface IMetadata
-    {
-        string Title { get; }
-        string Description { get; }
-        string Language { get; }
-        string Author { get; }
-        string Url { get; }
-        string Content { get; }
-    }
+
 }
