f7cc3a605e32bc650743ca1d6bd43a889bf5c31b
Max Hamulyák <max@kaylumah.nl>
2022-04-29 21:23:08 +0200

    add host projects

---
+++ src/Component/Engine/Transformation/Hosting/Class1.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+namespace Kaylumah.Ssg.Engine.Transformation.Hosting;
+public class Class1
+{
+
+}
