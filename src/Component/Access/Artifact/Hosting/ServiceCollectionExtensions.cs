8cb9aa3be7c68c9416a35b59650accabb4eeb857
Max Hamulyák <max@kaylumah.nl>
2022-04-30 10:07:44 +0200

    Artifact access

--- src/Component/Access/Artifact/Hosting/ServiceCollectionExtensions.cs
+++ src/Component/Access/Artifact/Hosting/ServiceCollectionExtensions.cs
@@ -1,8 +1,24 @@
 ﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using Kaylumah.Ssg.Access.Artifact.Interface;
+using Kaylumah.Ssg.Access.Artifact.Service;
+using Kaylumah.Ssg.Utilities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
 namespace Kaylumah.Ssg.Access.Artifact.Hosting;
+
 public static partial class ServiceCollectionExtensions
 {
-
+    public static IServiceCollection AddArtifactAccess(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.TryAdd(ServiceDescriptor.Singleton(typeof(ILogger<>), typeof(NullLogger<>)));
+        services.AddSingleton<IStoreArtifactsStrategy, FileSystemStoreArtifactsStrategy>();
+        services.AddSingleton<IArtifactAccess, ArtifactAccess>();
+        return services;
+    }
 }
--- test/Unit/Component/Access/Artifact/ArtifactAccesTests.cs
+++ test/Unit/Component/Access/Artifact/ArtifactAccesTests.cs
@@ -3,6 +3,7 @@
 
 using System.Text;
 using FluentAssertions;
+using Kaylumah.Ssg.Access.Artifact.Hosting;
 using Kaylumah.Ssg.Access.Artifact.Interface;
 using Kaylumah.Ssg.Access.Artifact.Service;
 using Microsoft.Extensions.Configuration;
@@ -15,25 +16,86 @@ namespace Test.Unit;
 public class ArtifactAccessTests
 {
     [Fact]
-    public async Task Test1()
+    public async Task Test_ArtifactAccess_StoreWithoutClean()
     {
+        var fileSystemMock = new FileSystemMock();
+
         var configuration = new ConfigurationBuilder().Build();
         var serviceProvider = new ServiceCollection()
+            .AddArtifactAccess(configuration)
+            .AddSingleton(fileSystemMock.Object)
             .BuildServiceProvider();
 
+        var sut = serviceProvider.GetRequiredService<IArtifactAccess>(); 
 
-        var fileSystemMock = new FileSystemMock();
-        var loggerMock = new LoggerMock<ArtifactAccess>();
-        var strategies = new IStoreArtifactsStrategy[] {
-                new FileSystemStoreArtifactsStrategy(new LoggerMock<FileSystemStoreArtifactsStrategy>().Object, fileSystemMock.Object)
-            };
-        var sut = new ArtifactAccess(loggerMock.Object, strategies);
         await sut.Store(new StoreArtifactsRequest
         {
             OutputLocation = new FileSystemOutputLocation()
             {
                 Path = "dist",
                 Clean = false
+            },
+            Artifacts = new Artifact[] {
+                    new Artifact()
+                    {
+                        Path = "test.txt",
+                        Contents = Encoding.UTF8.GetBytes(string.Empty)
+                    }
+                }
+        });
+        fileSystemMock.CreatedDirectories.Count.Should().Be(1);
+    }
+
+    [Fact]
+    public async Task Test_ArtifactAccess_StoreWithSubdirectory()
+    {
+        var fileSystemMock = new FileSystemMock();
+
+        var configuration = new ConfigurationBuilder().Build();
+        var serviceProvider = new ServiceCollection()
+            .AddArtifactAccess(configuration)
+            .AddSingleton(fileSystemMock.Object)
+            .BuildServiceProvider();
+
+        var sut = serviceProvider.GetRequiredService<IArtifactAccess>();
+
+        await sut.Store(new StoreArtifactsRequest
+        {
+            OutputLocation = new FileSystemOutputLocation()
+            {
+                Path = "dist",
+                Clean = false
+            },
+            Artifacts = new Artifact[] {
+                    new Artifact()
+                    {
+                        Path = Path.Combine("assets", "test.txt"),
+                        Contents = Encoding.UTF8.GetBytes(string.Empty)
+                    }
+                }
+        });
+        fileSystemMock.CreatedDirectories.Count.Should().Be(1);
+    }
+
+    [Fact]
+    public async Task Test_ArtifactAccess_StoreWithClean()
+    {
+        var fileSystemMock = new FileSystemMock();
+
+        var configuration = new ConfigurationBuilder().Build();
+        var serviceProvider = new ServiceCollection()
+            .AddArtifactAccess(configuration)
+            .AddSingleton(fileSystemMock.Object)
+            .BuildServiceProvider();
+
+        var sut = serviceProvider.GetRequiredService<IArtifactAccess>();
+
+        await sut.Store(new StoreArtifactsRequest
+        {
+            OutputLocation = new FileSystemOutputLocation()
+            {
+                Path = "dist",
+                Clean = true
             },
             Artifacts = new Artifact[] {
                     new Artifact()
