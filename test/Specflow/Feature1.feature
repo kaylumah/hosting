1b81e54995e003272ff30e0e4d027efda12ff7b8
Max Hamulyák <max@kaylumah.nl>
2022-05-14 09:10:53 +0200

    Initial work

---
+++ test/Specflow/Feature1.feature
@@ -0,0 +1,11 @@
+﻿Feature: Feature1
+
+A short summary of the feature
+
+@tag1
+Scenario: MyFirstScenario
+	Given scope '[string]' has the following metadata:
+        | key    | value |
+        | number | 1     |
+        | text   | abc   |
+        | expr   | true  |
---
+++ test/Specflow/FeatureSteps.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace Test.Specflow;
+
+[Binding]
+internal class FeatureSteps
+{
+    [Given("scope '(.*)' has the following metadata:")]
+    public void GivenTheFollowingData(string scope, Table table)
+    {
+        var metaData = table.ToDictionary();
+    }
+}
