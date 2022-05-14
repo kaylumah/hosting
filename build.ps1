e5173d4283437cf8f305edaefb768c8c15b1b54d
Max Hamulyák <max@kaylumah.nl>
2022-05-14 09:04:54 +0000

    add removal

--- build.ps1
+++ build.ps1
@@ -5,8 +5,19 @@ param(
     [string] $BuildNumber = (Get-Date).ToString("yyyyMMdd.hhmmss")
 )
 
+$RepoRoot = $PSScriptRoot
+# $RepoRoot = Split-Path $PSScriptRoot -Parent
+
 Write-Host "BuildId '$BuildId' BuildNumber '$BuildNumber'"
 
 $PrBuildId = $env:PR_BUILD_ID
 
 Write-Host "PRBuild is '$PrBuildId'"
+Write-Host "RepoRoot is '$RepoRoot'"
+$DistFolder = "$RepoRoot/dist"
+
+if (Test-Path $DistFolder)
+{ 
+    Write-Host "dist folder from previous run exists, removing now."
+    Remove-Item $DistFolder -Recurse -Force
+}
