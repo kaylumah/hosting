832296b1834b16ee313953e1d41711b9e232fa5e
Max Hamulyák <max@kaylumah.nl>
2022-05-14 09:22:46 +0000

    Update script

--- build.ps1
+++ build.ps1
@@ -5,21 +5,34 @@ param(
     [string] $BuildNumber = (Get-Date).ToString("yyyyMMdd.hhmmss")
 )
 
-$RepoRoot = $PSScriptRoot
+Write-Host "[args] BuildId '$BuildId' BuildNumber '$BuildNumber'"
+
+[string] $RepoRoot = $PSScriptRoot
 # $RepoRoot = Split-Path $PSScriptRoot -Parent
-
-Write-Host "BuildId '$BuildId' BuildNumber '$BuildNumber'"
-
-$PrBuildId = $env:PR_BUILD_ID
-
-Write-Host "PRBuild is '$PrBuildId'"
 Write-Host "RepoRoot is '$RepoRoot'"
-$DistFolder = "$RepoRoot/dist"
 
+[string] $DistFolder = "$RepoRoot/dist"
 if (Test-Path $DistFolder)
 { 
     Write-Host "dist folder from previous run exists, removing now."
     Remove-Item $DistFolder -Recurse -Force
 }
 
+
+[string] $BuildConfiguration = "Release"
 dotnet restore
+dotnet build --configuration $BuildConfiguration --no-restore /p:BuildId=$BuildId /p:BuildNumber=$BuildNumber
+dotnet test --configuration $BuildConfiguration --no-build --verbosity normal /p:CollectCoverage=true /p:CoverletOutputFormat=lcov /p:CoverletOutput=TestResults/lcov.info
+
+[string] $PrBuildId = $env:PR_BUILD_ID
+if ([string]::IsNullOrEmpty($PrBuildId))
+{
+    Write-Host "Production Build"
+    dotnet "artifacts/bin/Kaylumah.Ssg.Client.SiteGenerator/$BuildConfiguration/net6.0/Kaylumah.Ssg.Client.SiteGenerator.dll" SiteConfiguration:AssetDirectory=assets
+}
+else
+{
+    Write-Host "PullRequest Build ($PrBuildId)"
+    [string] $BaseUrl="https://green-field-0353fee03-$PrBuildId.westeurope.1.azurestaticapps.net"
+    dotnet "artifacts/bin/Kaylumah.Ssg.Client.SiteGenerator/$BuildConfiguration/net6.0/Kaylumah.Ssg.Client.SiteGenerator.dll" Site:Url=$BaseUrl
+}
