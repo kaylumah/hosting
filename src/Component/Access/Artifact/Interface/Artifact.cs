490bd34c04ebe9633f38457e0f091eb31ad62264
Max Hamulyák <max@kaylumah.nl>
2022-05-21 20:10:50 +0200

    add debugger display to artifact

--- src/Component/Access/Artifact/Interface/Artifact.cs
+++ src/Component/Access/Artifact/Interface/Artifact.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System.Diagnostics;
+
 namespace Kaylumah.Ssg.Access.Artifact.Interface;
 
+[DebuggerDisplay("Artifact '{Path}'")]
 public class Artifact
 {
     public byte[] Contents { get; set; }
