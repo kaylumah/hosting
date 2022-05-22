a71d0d0158cb126f335ec6f6c2fabadbe18ad833
Max Hamulyák <max@kaylumah.nl>
2022-05-22 09:40:39 +0200

    add extension method to safe byte[] from Atom10

---
+++ src/Component/Manager/Site/Service/SyndicationFeedExtensions.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Xml;
+
+namespace System.ServiceModel.Syndication
+{
+    public static class SyndicationFeedExtensions
+    {
+        public static byte[] SaveAsAtom10(this SyndicationFeed syndicationFeed)
+        {
+            var settings = new XmlWriterSettings();
+            using var stream = new MemoryStream();
+            using var writer = XmlWriter.Create(stream, settings);
+            syndicationFeed.SaveAsAtom10(writer);
+            writer.Close();
+            return stream.ToArray();
+        }
+    }
+}
