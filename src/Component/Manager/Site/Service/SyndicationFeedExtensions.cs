892205a387feb5ac0efad6f5096414f2a0863ae3
Max Hamulyák <max@kaylumah.nl>
2022-05-22 11:12:25 +0200

    attempt

--- src/Component/Manager/Site/Service/SyndicationFeedExtensions.cs
+++ src/Component/Manager/Site/Service/SyndicationFeedExtensions.cs
@@ -3,6 +3,7 @@
 
 using System.Xml;
 using Kaylumah.Ssg.Engine.Transformation.Interface;
+using Kaylumah.Ssg.Utilities;
 
 namespace System.ServiceModel.Syndication
 {
@@ -31,8 +32,43 @@ namespace System.ServiceModel.Syndication
             // https://github.com/kestrelblackmore/BlogMatrix/blob/master/App_Code/RssSyndicator.cs
             // https://github.com/kestrelblackmore/BlogMatrix/blob/master/feed.cshtml
 
-            var feed = new SyndicationFeed("Feed Title", "Feed Description", new Uri("http://Feed/Alternate/Link"), "FeedID", DateTime.Now);
+            SyndicationFeed feed = new SyndicationFeed("Feed Title", "Feed Description", new Uri("http://Feed/Alternate/Link"), "FeedID", DateTime.Now);
 
+            SyndicationPerson sp = new SyndicationPerson("jesper@contoso.com", "Jesper Aaberg", "http://Jesper/Aaberg");
+            feed.Authors.Add(sp);
+
+            SyndicationCategory category = new SyndicationCategory("FeedCategory", "CategoryScheme", "CategoryLabel");
+            feed.Categories.Add(category);
+
+            feed.Contributors.Add(new SyndicationPerson("lene@contoso.com", "Lene Aaling", "http://lene/aaling"));
+            feed.Copyright = new TextSyndicationContent("Copyright 2007");
+            feed.Description = new TextSyndicationContent("This is a sample feed");
+
+            feed.Generator = "Sample Code";
+            feed.Id = "FeedID";
+            feed.ImageUrl = new Uri("http://server/image.jpg");
+
+            TextSyndicationContent textContent = new TextSyndicationContent("Some text content");
+            SyndicationItem item = new SyndicationItem("Item Title", textContent, new Uri("http://server/items"), "ItemID", DateTime.Now);
+
+            List<SyndicationItem> items = new List<SyndicationItem>();
+            items.Add(item);
+            feed.Items = items;
+
+            feed.Language = "en-us";
+            feed.LastUpdatedTime = DateTime.Now;
+
+            SyndicationLink link = new SyndicationLink(new Uri("http://server/link"), "alternate", "Link Title", "text/html", 1000);
+            feed.Links.Add(link);
+
+            /*
+            var feed = new SyndicationFeed();
+            feed.Language = siteMetaData.Language;
+            feed.Id = siteMetaData.Id;
+            feed.Title = new TextSyndicationContent(siteMetaData.Title);
+            */
+
+            /*
             if (siteMetaData.Collections.TryGetValue("posts", out var posts))
             {
                 var feedPosts = posts
@@ -43,7 +79,7 @@ namespace System.ServiceModel.Syndication
             }
 
             feed.Items = feed.Items.OrderByDescending(x => x.PublishDate);
-
+            */
             return feed;
         }
     }
@@ -52,15 +88,20 @@ namespace System.ServiceModel.Syndication
     {
         public static SyndicationItem ToSyndicationItem(this PageMetaData pageMetaData)
         {
-            return new SyndicationItem
+            var pageUrl = GlobalFunctions.AbsoluteUrl(pageMetaData.Url);
+
+            var item = new SyndicationItem
             {
-                Id = pageMetaData.Url,
+                Id = pageUrl,
                 Title = new TextSyndicationContent(pageMetaData.Title),
                 Summary = new TextSyndicationContent(pageMetaData.Description),
                 Content = new TextSyndicationContent(pageMetaData.Content, TextSyndicationContentKind.Html),
                 PublishDate = (DateTimeOffset)pageMetaData["date"],
                 LastUpdatedTime = pageMetaData.LastModified
             };
+
+            item.Links.Add(new SyndicationLink(new Uri(pageUrl)));
+            return item;
         }
 
         public static IEnumerable<SyndicationItem> ToSyndicationItems(this IEnumerable<PageMetaData> pageMetaDatas)
