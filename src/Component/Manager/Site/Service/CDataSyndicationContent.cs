7818313933c40aa54e48997c9ec8eb8767dd79f7
Max Hamulyák <max@kaylumah.nl>
2022-05-26 08:19:16 +0200

    move to seperate files

---
+++ src/Component/Manager/Site/Service/CDataSyndicationContent.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+namespace System.ServiceModel.Syndication
+{
+    public class CDataSyndicationContent : TextSyndicationContent
+    {
+        public CDataSyndicationContent(TextSyndicationContent content)
+            : base(content)
+        {}
+
+        protected override void  WriteContentsTo(System.Xml.XmlWriter writer)
+        {
+            writer.WriteCData(Text);
+        }
+    }
+}
---
+++ src/Component/Manager/Site/Service/PageMetaDataExtensions1.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Kaylumah.Ssg.Engine.Transformation.Interface;
+using Kaylumah.Ssg.Utilities;
+
+namespace System.ServiceModel.Syndication
+{
+    public static partial class PageMetaDataExtensions
+    {
+        public static SyndicationItem ToSyndicationItem(this PageMetaData pageMetaData)
+        {
+            var pageUrl = GlobalFunctions.AbsoluteUrl(pageMetaData.Url);
+
+            var item = new SyndicationItem
+            {
+                Id = pageUrl
+            };
+
+            item.Links.Add(new SyndicationLink(new Uri(pageUrl)));
+            return item;
+        }
+
+        public static IEnumerable<SyndicationItem> ToSyndicationItems(this IEnumerable<PageMetaData> pageMetaDatas)
+        {
+            return pageMetaDatas.Select(ToSyndicationItem);
+        }
+    }
+}
