56a802e2ad54237e531326f12c73fd74517a8dfe
Max Hamulyák <max@kaylumah.nl>
2022-05-28 22:25:34 +0200

    add example

--- src/Component/Manager/Site/Service/SiteMap/SiteMapGenerator.cs
+++ src/Component/Manager/Site/Service/SiteMap/SiteMapGenerator.cs
@@ -41,6 +41,7 @@ public class SiteMapGenerator
         };
         using var stream = new MemoryStream();
         using var writer = XmlWriter.Create(stream, settings);
+        // https://github.com/microsoft/referencesource/blob/master/System.ServiceModel/System/ServiceModel/Syndication/Atom10FeedFormatter.cs
         // https://social.msdn.microsoft.com/Forums/en-US/b0c0d3fe-f3da-4102-9d6b-b1cdc80065bf/how-to-generate-sitemap-for-a-site-to-list-all-the-web-pages?forum=aspgettingstarted
         // https://rehansaeed.com/dynamically-generating-sitemap-xml-for-asp-net-mvc/
         // https://stackoverflow.com/questions/55504758/how-to-write-sitemap-file-with-xmlwriter
