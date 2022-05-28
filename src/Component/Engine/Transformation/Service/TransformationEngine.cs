1e8ae0bdaf60ae5a841f23288aef115f89febc14
Max Hamulyák <max@kaylumah.nl>
2022-05-28 15:29:34 +0200

    move the code around

--- src/Component/Engine/Transformation/Service/TransformationEngine.cs
+++ src/Component/Engine/Transformation/Service/TransformationEngine.cs
@@ -1,13 +1,9 @@
 ﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-using System.Globalization;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using Kaylumah.Ssg.Engine.Transformation.Interface;
 using Kaylumah.Ssg.Utilities;
 using Microsoft.Extensions.Logging;
-using Schema.NET;
 using Scriban;
 using Scriban.Runtime;
 using Ssg.Extensions.Metadata.Abstractions;
@@ -26,37 +22,6 @@ public class TransformationEngine : ITransformationEngine
         _logger = logger;
         _fileSystem = fileSystem;
         _metadataProvider = metadataProvider;
-    }
-
-    private static string ToLdJson(RenderData renderData)
-    {
-        ArgumentNullException.ThrowIfNull(renderData);
-        var settings = new JsonSerializerOptions()
-        {
-            AllowTrailingCommas = true,
-            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
-            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-            WriteIndented = true
-        };
-        if (renderData.Page.Type == ContentType.Article)
-        {
-            var blogPost = new BlogPosting
-            {
-                // Id = new Uri(GlobalFunctions.AbsoluteUrl(renderData.Page.Url)),
-                MainEntityOfPage = new Values<ICreativeWork, Uri>(new Uri(GlobalFunctions.AbsoluteUrl(renderData.Page.Url))),
-                Headline = renderData.Page.Title,
-                DatePublished = DateTime.Parse((string)renderData.Page["publisheddate"], CultureInfo.InvariantCulture),
-                DateModified = DateTime.Parse((string)renderData.Page["modifieddate"], CultureInfo.InvariantCulture),
-                Image = new Values<IImageObject, Uri>(new Uri(GlobalFunctions.AbsoluteUrl((string)renderData.Page["image"]))),
-                Author = new Values<IOrganization, IPerson>(new Person {
-                    Name = new OneOrMany<string>("Max Hamulyák"),
-                    Url = new OneOrMany<Uri>(new Uri(GlobalFunctions.Instance.Url))
-                }),
-                Publisher = new Values<IOrganization, IPerson>(new Organization { })
-            };
-            return blogPost.ToString(settings);
-        }
-        return null;
     }
 
     public async Task<MetadataRenderResult[]> Render(MetadataRenderRequest[] requests)
@@ -80,7 +45,7 @@ public class TransformationEngine : ITransformationEngine
                 };
                 var scriptObject = new ScriptObject();
                 scriptObject.Import(request.Metadata);
-                scriptObject.Import("ldjson", () => ToLdJson(request.Metadata));
+                scriptObject.Import("ldjson", () => request.Metadata.Page["ldjson"]);
                 // note: work-around for Build becoming part of Site
                 scriptObject.Import("build", () => request.Metadata.Site.Build);
                 context.PushGlobal(scriptObject);
