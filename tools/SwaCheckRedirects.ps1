745362860175ebefeb2d3263a8f73fec231a627d
Max Hamulyák <max@kaylumah.nl>
2022-05-29 14:50:15 +0000

    fix redirects in script

--- tools/SwaCheckRedirects.ps1
+++ tools/SwaCheckRedirects.ps1
@@ -12,13 +12,17 @@ Function GetResolvedUrl()
   param (
     [string]$RequestUrl
   )
-  Write-Host "Verifying '$RequestUrl'"
   $resp = Invoke-WebRequest -Method HEAD $RequestUrl -MaximumRedirection 0 -ErrorAction Ignore -SkipHttpErrorCheck
   $code = $resp.StatusCode
   if($code -eq 301 -or $code -eq 302) {
-    Write-Host $resp
-    # $Location = $resp.Headers.Location
-    # Write-Output $Location
+    $Location = $resp.Headers.Location
+    if ($Location.StartsWith("/"))
+    {
+      Write-Host "Location is relative for '$RequestUrl'"
+      $TempUri = [System.Uri]$RequestUrl
+      $Location = $TempUri.AbsoluteUri -Replace $TempUri.AbsolutePath, $Location
+    }
+    Write-Output $Location
   }
 }
 
