a1e0fd052707cf7cb894a020307acf1dcc61c851
Max Hamulyák <max@kaylumah.nl>
2022-05-29 15:34:53 +0200

    remove from script

--- tools/demo.ps1
+++ tools/demo.ps1
@@ -31,46 +31,24 @@ Function Get-UrlRedirection {
   process {
     try {
 
-      if ($Enumerate) { # Enumerate the whole redirection chain, from input URL to ultimate target,
-                        # assuming the max. count of redirects is not exceeded.
-        # We must walk the chain of redirections one by one.
-        # If we disallow redirections, .GetResponse() fails and we must examine
-        # the exception's .Response object to get the redirect target.
+      if ($Enumerate) {
         $nextUrl = $Url
         $urls = @( $nextUrl.AbsoluteUri ) # Start with the input Uri
         $ultimateFound = $false
-        # Note: We add an extra loop iteration so we can determine whether
-        #       the ultimate target URL was reached or not.
         foreach($i in 1..$($MaxRedirections+1)) {
           Write-Verbose "Examining: $nextUrl"
           $request = [System.Net.HttpWebRequest]::Create($nextUrl)
           $request.AllowAutoRedirect = $False
           try {
             $response = $request.GetResponse()
-            # Note: In .NET *Core* the .GetResponse() for a redirected resource
-            #       with .AllowAutoRedirect -eq $False throws an *exception*.
-            #       We only get here on *Windows*, with the full .NET Framework.
-            #       We either have the ultimate target URL, or a redirection
-            #       whose target URL is reflected in .Headers['Location']
-            #       !! Syntax `.Headers.Location` does NOT work.
             $nextUrlStr = $response.Headers['Location']
             $response.Close()
-            # If the ultimate target URL was reached (it was already
-            # recorded in the previous iteration), and if so, simply exit the loop.
             if (-not $nextUrlStr) {
               $ultimateFound = $true
               break
             }
           } catch [System.Net.WebException] {
-            # The presence of a 'Location' header implies that the
-            # exception must have been triggered by a HTTP redirection 
-            # status code (3xx). 
-            # $_.Exception.Response.StatusCode contains the specific code
-            # (as an enumeration value that can be case to [int]), if needed.
-            # !! Syntax `.Headers.Location` does NOT work.
             $nextUrlStr = try { $_.Exception.Response.Headers['Location'] } catch {}
-            # Not being able to get a target URL implies that an unexpected
-            # error ocurred: re-throw it.
             if (-not $nextUrlStr) { Throw }
           }
           Write-Verbose "Raw target: $nextUrlStr"
@@ -81,23 +59,15 @@ Function Get-UrlRedirection {
           }
           if ($i -le $MaxRedirections) { $urls += $nextUrl.AbsoluteUri }          
         }
-        # Output the array of URLs (chain of redirections) as a *single* object.
         Write-Output -NoEnumerate $urls
         if (-not $ultimateFound) { Write-Warning "Enumeration of $Url redirections ended before reaching the ultimate target." }
 
-      } else { # Resolve just to the ultimate target,
-                # assuming the max. count of redirects is not exceeded.
-
-                # Note that .AllowAutoRedirect defaults to $True.
-        # This will fail, if there are more redirections than the specified 
-        # or default maximum.
+      } else {
         $request = [System.Net.HttpWebRequest]::Create($Url)
         if ($PSBoundParameters.ContainsKey('MaxRedirections')) {
           $request.MaximumAutomaticRedirections = $MaxRedirections
         }
         $response = $request.GetResponse()
-        # Output the ultimate target URL.
-        # If no redirection was involved, this is the same as the input URL.
         $response.ResponseUri.AbsoluteUri
         $response.Close()
 
