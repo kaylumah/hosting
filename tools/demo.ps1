ad344531458f44aa3dfa7adc53c976e47958a8ea
Max Hamulyák <max@kaylumah.nl>
2022-05-29 13:37:12 +0000

    wip

--- tools/demo.ps1
+++ tools/demo.ps1
@@ -4,21 +4,6 @@ param (
     [string]$Url = "http://localhost:4280"
 )
 
-# $RepoRoot = Split-Path $PSScriptRoot -Parent
-# $DistDirectory = "$RepoRoot/dist"
-
-# [System.Net.HttpWebRequest]::Create('http://localhost:4280/feed').GetResponse().ResponseUri.AbsoluteUri
-
-# $url="https://jigsaw.w3.org/HTTP/300/301.html"
-# $resp = Invoke-WebRequest -Method HEAD $url -MaximumRedirection 0 -ErrorAction Ignore
-# $code = $resp.StatusCode
-# Write-Output "URL: $url"
-# Write-Output "ErrorCode: $code"
-# if($code -eq 301) {
-#     $loc = $resp.Headers.Location
-#     Write-Output "New URL: $loc"
-# }
-
 # https://stackoverflow.com/questions/45574479/powershell-determine-new-url-of-a-permanently-moved-redirected-resource
 Function Get-UrlRedirection {
   [CmdletBinding()]
