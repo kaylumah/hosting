2cf64e4daf1208ce24b2a16f200b5c4d5d1f3d29
Max Hamulyák <max@kaylumah.nl>
2022-05-31 21:12:45 +0200

    Add PowerShell to fetch urls from rss and sitemap (#104)

---
+++ tools/Scan-Website.ps1
@@ -0,0 +1,33 @@
+#requires -Version 7.2
+
+param (
+    [Parameter(Mandatory=$true, HelpMessage = "The base URL for website")]
+    [string] $BaseUrl
+)
+
+$ErrorActionPreference  = "Stop"
+$ScriptRoot = $PSScriptRoot
+
+$AtomUrl = "$BaseUrl/feed.xml"
+Write-Host "========================================"
+Write-Host "Scan: $AtomUrl"
+Write-Host "========================================"
+$AtomFeedUrlsScript = "$ScriptRoot/Get-AtomFeedUrls.ps1"
+$AtomFeedUrls = & $AtomFeedUrlsScript -AtomFeedUrl $AtomUrl
+foreach ($Url in $AtomFeedUrls)
+{
+   Write-Host "$Url"
+}
+
+Write-Host ""
+
+$SiteMapUrl = "$BaseUrl/sitemap.xml"
+Write-Host "========================================"
+Write-Host "Scan: $SiteMapUrl"
+Write-Host "========================================"
+$SiteMapUrlsScript = "$ScriptRoot/Get-SiteMapUrls.ps1"
+$SiteMapUrls = & $SiteMapUrlsScript -SiteMapUrl $SiteMapUrl
+foreach ($Url in $SiteMapUrls)
+{
+   Write-Host "$Url"
+}
