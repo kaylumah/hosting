0421096e64cfc3aba7fe98af289f2138120d6d6a
Max Hamulyák <max@kaylumah.nl>
2022-06-01 20:31:54 +0000

    update script

--- tools/Test-StaticWebApp.ps1
+++ tools/Test-StaticWebApp.ps1
@@ -1,6 +1,7 @@
 #requires -Version 7.2
 
 param (
+    [uri] $BaseUrl
 )
 
 $ErrorActionPreference  = "Stop"
@@ -9,11 +10,11 @@ $ScriptRoot = $PSScriptRoot
 $GetUrlRedirectsScript = "$ScriptRoot/Get-UrlRedirects.ps1"
 $GetUrlContentTypeScript = "$ScriptRoot/Get-UrlContentType.ps1"
 
+$HttpBaseUrl = "http://$($BaseUrl.Authority)"
+$TestUrl = "$HttpBaseUrl/blog"
 
-$TestUrl = "http://kaylumah.nl/blog"
 $Urls = & $GetUrlRedirectsScript -Url $TestUrl
-
-$FinalRedirectUrl = $($Urls[$Urls.Count -1]).Trim()
+$FinalRedirectUrl = $($Urls | Select-Object -Last 1).Trim()
 $ContentType = $(& $GetUrlContentTypeScript -Url $FinalRedirectUrl).Trim()
 
 Write-Host "For '$TestUrl' the resolved url is '$FinalRedirectUrl' with content-type '$ContentType'"
