93fa383cde1718e10acf34576fe1b33d668b691f
Max Hamulyák <max@kaylumah.nl>
2022-06-01 20:03:21 +0000

    wip

---
+++ tools/Get-UrlRedirects.ps1
@@ -0,0 +1,38 @@
+#requires -Version 7.2
+
+param (
+    [string] $Url = "http://kaylumah.nl/blog"
+)
+
+$ErrorActionPreference  = "Stop"
+
+[System.Collections.ArrayList] $Urls = @()
+$ContinueLoop = $false
+[uri] $RequestUrl = $Url
+Do
+{
+    # Write-Host "Requesting '$RequestUrl'"
+    $Response = Invoke-WebRequest -Method HEAD $RequestUrl -MaximumRedirection 0 -ErrorAction Ignore -SkipHttpErrorCheck
+    $ResponseStatusCode = $Response.StatusCode
+    # Write-Host "Status for '$RequestUrl' is '$ResponseStatusCode'"
+    if ($ResponseStatusCode -eq 301 -or $ResponseStatusCode -eq 302)
+    {
+        [string] $ResolvedLocation = $Response.Headers.Location
+        if ($ResolvedLocation.StartsWith("/"))
+        {
+            # Write-Host "Location '$ResolvedLocation' is relative"
+            $ResolvedLocation = $RequestUrl.AbsoluteUri -Replace $RequestUrl.AbsolutePath, $ResolvedLocation
+        }
+        # Write-Host "Redirect Location is '$ResolvedLocation'"
+        $Urls.Add($ResolvedLocation) > $null
+        $RequestUrl = $ResolvedLocation
+        $ContinueLoop = $true
+    }
+    else
+    {
+        $ContinueLoop = $false
+    }
+}
+While ($ContinueLoop)
+
+$Urls | Foreach-Object { $_ | Out-String } 
