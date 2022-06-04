215eecb867410fe4d594721745276275224be39a
Max Hamulyák <max@kaylumah.nl>
2022-06-04 12:21:48 +0000

    wip

--- src/Component/Manager/Site/Service/SiteInfo.cs
+++ src/Component/Manager/Site/Service/SiteInfo.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-using System.ComponentModel.DataAnnotations;
-
 namespace Kaylumah.Ssg.Manager.Site.Service;
 
 public class SiteInfo
@@ -10,7 +8,6 @@ public class SiteInfo
     public string Lang { get; set; }
     public string BaseUrl { get; set; }
     public string Url { get; set; }
-    [Required]
     public string Title { get; set; }
     public string Description { get; set; }
     public Collections Collections { get; set; } = new Collections();
