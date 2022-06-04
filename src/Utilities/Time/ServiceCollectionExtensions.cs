45b0e0149c0b726de09526703fb721065ba056d6
Max Hamulyák <max@kaylumah.nl>
2022-06-04 20:33:43 +0000

    add ext method

---
+++ src/Utilities/Time/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kaylumah.Ssg.Utilities.Time;
+
+public static partial class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSystemClock(this IServiceCollection services)
+    {
+        return services;
+    }
+}
