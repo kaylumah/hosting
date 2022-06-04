efd54a5f4fd880937e5d12d8e0b99651f742b654
Max Hamulyák <max@kaylumah.nl>
2022-06-04 20:30:14 +0000

    add initial clock

---
+++ src/Utilities/Time/ISystemClock.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+namespace Kaylumah.Ssg.Utilities.Time;
+
+public interface ISystemClock
+{
+    DateTimeOffset UtcNow { get; }
+    // TimeZoneInfo LocalZone { get; }
+    // DateTimeOffset LocalNow => TimeZoneInfo.ConvertTime(UtcNow, LocalZone);
+}
+
+public class SystemClock : ISystemClock
+{
+    // https://github.com/dotnet/runtime/issues/36617
+    public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
+}
