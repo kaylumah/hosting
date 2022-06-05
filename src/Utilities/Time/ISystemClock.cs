b55bd1a80af555ba1aa69ae05d1721dbe0f3d006
Max Hamulyák <max@kaylumah.nl>
2022-06-05 10:08:24 +0200

    update clock

--- src/Utilities/Time/ISystemClock.cs
+++ src/Utilities/Time/ISystemClock.cs
@@ -5,9 +5,9 @@ namespace Kaylumah.Ssg.Utilities.Time;
 
 public interface ISystemClock
 {
+    DateTimeOffset LocalNow { get; }
     DateTimeOffset UtcNow { get; }
-    // TimeZoneInfo LocalZone { get; }
-    // DateTimeOffset LocalNow => TimeZoneInfo.ConvertTime(UtcNow, LocalZone);
+    long UtcNowTicks { get; }
 }
 
 public class SystemClock : ISystemClock
@@ -17,5 +17,15 @@ public class SystemClock : ISystemClock
     // https://github.com/dotnet/aspnetcore/blob/a450cb69b5e4549f5515cdb057a68771f56cefd7/src/Servers/Kestrel/Core/src/Internal/Infrastructure/ISystemClock.cs#L11
     // https://github.com/dotnet/aspnetcore/issues/16844
     // https://github.com/dotnet/runtime/issues/36617
+
+    private readonly TimeZoneInfo _timeZone;
+
+    public SystemClock()
+    {
+        _timeZone = TimeZoneInfo.FindSystemTimeZoneById("Europe/Amsterdam");
+    }
+
+    public DateTimeOffset LocalNow => TimeZoneInfo.ConvertTime(UtcNow, _timeZone);
     public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
+    public long UtcNowTicks => UtcNow.Ticks;
 }
