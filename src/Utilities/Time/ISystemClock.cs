19672a9f5e13ce476d4787d4c5b0cc71bdd1aa20
Max Hamulyák <max@kaylumah.nl>
2022-06-05 09:53:58 +0200

    add some links

--- src/Utilities/Time/ISystemClock.cs
+++ src/Utilities/Time/ISystemClock.cs
@@ -12,6 +12,9 @@ public interface ISystemClock
 
 public class SystemClock : ISystemClock
 {
+    // https://github.com/dotnet/aspnetcore/blob/a450cb69b5e4549f5515cdb057a68771f56cefd7/src/Servers/Kestrel/Core/src/Internal/Infrastructure/SystemClock.cs#L11
+    // https://github.com/dotnet/aspnetcore/blob/a450cb69b5e4549f5515cdb057a68771f56cefd7/src/Servers/Kestrel/shared/test/MockSystemClock.cs#L10
+    // https://github.com/dotnet/aspnetcore/blob/a450cb69b5e4549f5515cdb057a68771f56cefd7/src/Servers/Kestrel/Core/src/Internal/Infrastructure/ISystemClock.cs#L11
     // https://github.com/dotnet/aspnetcore/issues/16844
     // https://github.com/dotnet/runtime/issues/36617
     public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
