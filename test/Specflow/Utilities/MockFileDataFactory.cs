9c5d69b52de97a7c564feef7ae8dbc4b7e0c5cfd
Max Hamulyák <max@kaylumah.nl>
2022-07-02 15:09:39 +0200

    begin factory

---
+++ test/Specflow/Utilities/MockFileDataFactory.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.IO.Abstractions.TestingHelpers;
+using System.Text;
+
+namespace Test.Specflow.Utilities;
+#pragma warning disable CS3002
+public class MockFileDataFactory
+{
+    private Encoding _encoding;
+    public MockFileDataFactory WithUtf8Encoding() => WithEncoding(new System.Text.UTF8Encoding(false));
+    private MockFileDataFactory WithEncoding(Encoding encoding)
+    {
+        _encoding = encoding;
+        return this;
+    }
+
+    public MockFileData Create()
+    {
+        var data = string.Empty;
+        var bytes = _encoding.GetBytes(data);
+        return new MockFileData(bytes);
+    }
+    
+    public static MockFileData EmptyFile()
+    {
+        return new MockFileDataFactory()
+            .WithUtf8Encoding()
+            .Create();
+    }
+}
+#pragma warning restore CS3002
