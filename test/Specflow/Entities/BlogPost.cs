69c24b03e292c0ff3a8234abe6df90eed67dab6f
Max Hamulyák <max@kaylumah.nl>
2022-07-02 15:09:28 +0200

    WIP

--- test/Specflow/Entities/BlogPost.cs
+++ test/Specflow/Entities/BlogPost.cs
@@ -5,7 +5,7 @@ namespace Test.Specflow.Entities;
 
 public class BlogPost
 {
-    public string? Title { get;set; }
-    public string? Description { get;set; }
+    public string Title { get;set; }
+    public string Description { get;set; }
     public string[] Tags { get;set; } = Array.Empty<string>();
 }
