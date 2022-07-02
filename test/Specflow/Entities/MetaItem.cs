b71579d54eeafbb2e007ef0bd3a211fa3ffc3b24
Max Hamulyák <max@kaylumah.nl>
2022-07-02 17:49:48 +0200

    Delete MetaItem.cs

--- test/Specflow/Entities/MetaItem.cs
+++
@@ -1,6 +0,0 @@
-﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
-// See LICENSE file in the project root for full license information.
-
-namespace Test.Specflow.Entities;
-
-public readonly record struct MetaItem(string scope, string path, string key, string value);
