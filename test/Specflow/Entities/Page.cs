f357ad2201ca3f5eade01de8c245da2896d93e78
Max Hamulyák <max@kaylumah.nl>
2022-07-02 17:43:08 +0200

    Delete Page.cs

--- test/Specflow/Entities/Page.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
-// See LICENSE file in the project root for full license information.
-
-namespace Test.Specflow.Entities;
-
-public class Page
-{
-    public string Uri { get; set; }
-}
