9cf40ff02a908a22913ba5b0e9c6336f6f4e742a
Max Hamulyák <max@kaylumah.nl>
2022-07-02 11:29:05 +0200

    add project for specflow

---
+++ test/Specflow/UnitTest1.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+namespace Test.Specflow;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/Specflow/Usings.cs
@@ -0,0 +1,4 @@
+// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+global using Xunit;
