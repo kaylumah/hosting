b8971c79a17d6685de2677bf22219533cf5d5093
Max Hamulyák <max@kaylumah.nl>
2022-07-03 11:43:09 +0200

    split filesystem

---
+++ test/Specflow/Steps/FileSystemStepDefinitions.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.IO.Abstractions.TestingHelpers;
+using Test.Specflow.Utilities;
+
+namespace Test.Specflow.Steps;
+
+[Binding]
+public class FileSystemStepDefinitions
+{
+    private readonly MockFileSystem _mockFileSystem;
+    private readonly string _postsDirectory = Path.Combine("_site", "_posts");
+
+    public FileSystemStepDefinitions(MockFileSystem mockFileSystem)
+    {
+        _mockFileSystem = mockFileSystem;
+    }
+    
+    [Given("'(.*)' is an empty post:")]
+    public void GivenIsAnEmptyPost(string fileName)
+    {
+        var articleDirectory = Path.Combine(_postsDirectory, fileName);
+        _mockFileSystem.AddFile(articleDirectory, MockFileDataFactory.EmptyFile());
+    }
+
+    [Given("'(.*)' is a post with the following contents:")]
+    public void GivenIsAPostWithTheFollowingContents(string fileName, string contents)
+    {
+        var articleDirectory = Path.Combine(_postsDirectory, fileName);
+        _mockFileSystem.AddFile(articleDirectory, MockFileDataFactory.PlainFile(contents));
+    }
+}
--- test/Specflow/Steps/GlobalSteps.cs
+++ test/Specflow/Steps/GlobalSteps.cs
@@ -13,7 +13,6 @@ using Ssg.Extensions.Data.Yaml;
 using Ssg.Extensions.Metadata.YamlFrontMatter;
 using Test.Specflow.Entities;
 using Test.Specflow.Extensions;
-using Test.Specflow.Utilities;
 using File = Kaylumah.Ssg.Manager.Site.Service.Files.Processor.File;
 
 namespace Test.Specflow.Steps;
@@ -21,38 +20,21 @@ namespace Test.Specflow.Steps;
 [Binding]
 public class GlobalSteps
 {
-    private readonly MockFileSystem _mockFileSystem = new();
     private readonly IFileProcessor _fileProcessor;
-    private readonly string _postsDirectory = Path.Combine("_site", "_posts");
     private readonly List<File> _files = new();
 
-    public GlobalSteps(MetadataParserOptions metadataParserOptions, SiteInfo siteInfo)
+    public GlobalSteps(MockFileSystem mockFileSystem, MetadataParserOptions metadataParserOptions, SiteInfo siteInfo)
     {
         var metadataParser = new FileMetadataParser(NullLogger<FileMetadataParser>.Instance,
             new YamlFrontMatterMetadataProvider(new YamlParser()),
             metadataParserOptions);
-        IFileSystem fileSystem = new FileSystem(_mockFileSystem);
+        IFileSystem fileSystem = new FileSystem(mockFileSystem);
         _fileProcessor = new FileProcessor(fileSystem,
             NullLogger<FileProcessor>.Instance,
             Enumerable.Empty<IContentPreprocessorStrategy>(),
             siteInfo,
             metadataParser);
     }
-
-    [Given("'(.*)' is an empty post:")]
-    public void GivenIsAnEmptyPost(string fileName)
-    {
-        var articleDirectory = Path.Combine(_postsDirectory, fileName);
-        _mockFileSystem.AddFile(articleDirectory, MockFileDataFactory.EmptyFile());
-    }
-
-    [Given("'(.*)' is a post with the following contents:")]
-    public void GivenIsAPostWithTheFollowingContents(string fileName, string contents)
-    {
-        var articleDirectory = Path.Combine(_postsDirectory, fileName);
-        _mockFileSystem.AddFile(articleDirectory, MockFileDataFactory.PlainFile(contents));
-    }
-
     [When("the files are retrieved:")]
     public async Task WhenTheFilesAreRetrieved(FileFilterCriteria criteria)
     {
