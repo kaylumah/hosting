37186e8f85dfcad8c94e3ba176cd6f733477b4c8
Max Hamulyák <max@kaylumah.nl>
2022-07-09 12:21:30 +0200

    update mock

--- test/Specflow/Utilities/FileProcessorMock.cs
+++ test/Specflow/Utilities/FileProcessorMock.cs
@@ -2,10 +2,26 @@
 // See LICENSE file in the project root for full license information.
 
 using Kaylumah.Ssg.Manager.Site.Service.Files.Processor;
+using Moq;
 
 namespace Test.Specflow.Utilities;
 
 public class FileProcessorMock : StrictMock<IFileProcessor>
 {
-    
+    public FileProcessorMock()
+    {
+        SetupProcess();
+    }
+
+    public void SetupProcess()
+    {
+        Setup(fileProcessor =>
+                fileProcessor.Process(It.IsAny<FileFilterCriteria>()))
+            .Callback((FileFilterCriteria criteria) => { })
+            .ReturnsAsync((FileFilterCriteria criteria) =>
+            {
+                var result = Enumerable.Empty<Kaylumah.Ssg.Manager.Site.Service.Files.Processor.File>();
+                return result;
+            });
+    }
 }
