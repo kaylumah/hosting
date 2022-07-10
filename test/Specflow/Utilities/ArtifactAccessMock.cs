5b26e368d6ad005371b01b31a007da6ecfc7a4fd
Max Hamulyák <max@kaylumah.nl>
2022-07-10 09:09:09 +0200

    spit

---
+++ test/Specflow/Utilities/ArtifactAccessMock.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Kaylumah.Ssg.Access.Artifact.Interface;
+using Moq;
+
+namespace Test.Specflow.Utilities;
+
+public class ArtifactAccessMock : StrictMock<IArtifactAccess>
+{
+    public ArtifactAccessMock()
+    {
+        SetupStore();
+    }
+
+    public void SetupStore()
+    {
+        Setup(artifactAccess =>
+                artifactAccess.Store(It.IsAny<StoreArtifactsRequest>()))
+            .Returns(Task.CompletedTask);
+    }
+}
