21895b60b589d4cdc1ec92fcb45169e1786a334c
Max Hamulyák <max@kaylumah.nl>
2022-07-22 16:50:21 +0200

    Add global specflow helpers

---
+++ test/Specflow/Constants.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+namespace Test.Specflow;
+
+public static class Constants
+{
+    public const string NullIndicator = "<null>";
+}
