f301e60e60363c1a452026c3d0b10d30613d5fb8
Max Hamulyák <max@kaylumah.nl>
2022-07-23 09:21:33 +0200

    fix

--- test/Specflow/Entities/Organization.cs
+++ test/Specflow/Entities/Organization.cs
@@ -1,10 +1,13 @@
 ﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using Test.Specflow.Helpers;
+
 namespace Test.Specflow.Entities;
 
 public class Organization
 {
+    [GherkinTableHeader(0)]
     public string Id { get; set; }
 }
 
