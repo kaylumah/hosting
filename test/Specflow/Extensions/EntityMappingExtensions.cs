35a5f3f6a5728e28ab5bd67496bc31dd5ece6b0f
Max Hamulyák <max@kaylumah.nl>
2022-07-23 09:10:45 +0200

    Add author

---
+++ test/Specflow/Extensions/EntityMappingExtensions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+// ReSharper disable once CheckNamespace
+
+using Kaylumah.Ssg.Engine.Transformation.Interface;
+
+namespace Test.Specflow.Entities;
+
+public static class EntityMappingExtensions
+{
+    public static IEnumerable<AuthorMetaData> ToAuthorMetadata(this IEnumerable<Author> authors)
+    {
+        return authors.Select(ToAuthorMetadata);
+    }
+
+    public static AuthorMetaData ToAuthorMetadata(this Author author)
+    {
+        return new AuthorMetaData()
+        {
+            Id = author.Ids
+        };
+    }
+}
---
+++ test/Specflow/TableTransforms.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Test.Specflow.Entities;
+
+namespace Test.Specflow;
+
+[Binding]
+public class TableTransforms
+{
+    [StepArgumentTransformation]
+    public static AuthorCollection ToArticles(Table table)
+    {
+        ArgumentNullException.ThrowIfNull(table);
+        table.ValidateIfMappedCorrectlyTo<Author>();
+        var authorCollection = new AuthorCollection();
+        authorCollection.AddRange(table.CreateSet<Author>());
+        return authorCollection;
+    }
+}
