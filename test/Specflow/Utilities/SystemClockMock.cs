40da7f104e52f524c800e591e586e0efc3b1f1bd
Max Hamulyák <max@kaylumah.nl>
2022-08-10 08:45:50 +0200

    add systemclock

---
+++ test/Specflow/Utilities/SystemClockMock.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Kaylumah, 2022. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Kaylumah.Ssg.Utilities.Time;
+
+namespace Test.Specflow.Utilities;
+
+public class SystemClockMock : StrictMock<ISystemClock>
+{
+    public void SetupSystemTime(DateTimeOffset systemTime)
+    {
+        Setup(x => x.LocalNow).Returns(systemTime);
+    }
+}
