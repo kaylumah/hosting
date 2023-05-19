805c540766508f6051181666a0c1a68604bf9247
Max Hamulyák <max@kaylumah.nl>
2023-05-19 18:06:42 +0200

    wip

--- test/Specflow/ValidationContext.cs
+++ test/Specflow/ValidationContext.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Kaylumah, 2023. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-namespace Test.Specflow.Steps;
+namespace Test.Specflow;
 
 public class ValidationContext
 {
