3bb4d8fd373e371e97d1f58ef4b4540308d25360
Max Hamulyák <max@kaylumah.nl>
2023-05-19 13:08:58 +0200

    Update copyright header (#293)
    * Update copyright header
    
    * update license

--- test/Specflow/Helpers/PropertyNameEqualityComparer.cs
+++ test/Specflow/Helpers/PropertyNameEqualityComparer.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Kaylumah, 2022. All rights reserved.
+// Copyright (c) Kaylumah, 2023. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
 namespace Test.Specflow.Helpers;
