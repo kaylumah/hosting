2bc99f25f0ba88d44209d129411f0870e09a6b6a
Max Hamulyák <max@kaylumah.nl>
2023-05-19 16:33:16 +0200

    add initial test harness

---
+++ test/Utilities/TestHarness.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Kaylumah, 2023. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.Collections.ObjectModel;
+using Castle.DynamicProxy;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Test.Utilities;
+
+public sealed class TestHarness
+{
+    private readonly IServiceProvider _serviceProvider;
+    private static readonly ProxyGenerator ProxyGenerator = new();
+    private readonly IReadOnlyList<IInterceptor> _interceptors;
+
+    public TestHarness(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+        _interceptors = new ReadOnlyCollection<IInterceptor>(serviceProvider.GetServices<IAsyncInterceptor>().ToInterceptors());
+    }
+
+    public async Task TestService<T>(Func<T, Task> scenario) where T : class
+    {
+        var instance = _serviceProvider.GetRequiredService<T>();
+        var proxy = ProxyGenerator.CreateInterfaceProxyWithTarget(instance, _interceptors.ToArray());
+        await TestService(proxy, scenario).ConfigureAwait(false);
+    }
+
+    private static async Task TestService<T>(T instance, Func<T, Task> scenario) where T : class
+    {
+        await scenario(instance).ConfigureAwait(false);
+    }
+}
---
+++ test/Utilities/TestHarnessBuilder.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Kaylumah, 2023. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Test.Utilities;
+
+public sealed class TestHarnessBuilder
+{
+    private readonly List<Action<IConfigurationBuilder>> _configurationRegistrationActions = new();
+    private readonly List<Action<IServiceCollection, IConfiguration>> _serviceRegistrationActions = new();
+
+    private TestHarnessBuilder()
+    {
+        Register((serviceCollection) =>
+        {
+            serviceCollection.AddSingleton<TestHarness>();
+        });
+    }
+
+    public static TestHarnessBuilder Create()
+    {
+        return new TestHarnessBuilder();
+    }
+
+    public TestHarnessBuilder Configure(Action<IConfigurationBuilder> configurationRegistrationAction)
+    {
+        _configurationRegistrationActions.Add(configurationRegistrationAction);
+        return this;
+    }
+
+    public TestHarnessBuilder Register(Action<IServiceCollection, IConfiguration> serviceRegistrationAction)
+    {
+        _serviceRegistrationActions.Add(serviceRegistrationAction);
+        return this;
+    }
+
+    public TestHarnessBuilder Register(Action<IServiceCollection> serviceRegistrationAction)
+    {
+        return Register((serviceCollection, _) => serviceRegistrationAction(serviceCollection));
+    }
+
+    public TestHarness Build()
+    {
+        var configurationBuilder = new ConfigurationBuilder();
+        foreach (var configurationRegistration in _configurationRegistrationActions)
+        {
+            configurationRegistration(configurationBuilder);
+        }
+
+        var configuration = configurationBuilder.Build();
+
+        var services = new ServiceCollection();
+        foreach (var serviceRegistrationAction in _serviceRegistrationActions)
+        {
+            serviceRegistrationAction(services, configuration);
+        }
+
+        var serviceProvider = services.BuildServiceProvider(validateScopes: true);
+        var testHarness = serviceProvider.GetRequiredService<TestHarness>();
+        return testHarness;
+    }
+}
