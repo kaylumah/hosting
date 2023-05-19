b1a1f775b14a605cc89b914f33e534b77b5561e3
Max Hamulyák <max@kaylumah.nl>
2023-05-19 19:42:21 +0200

    fix

--- tools/test-reports.ps1
+++ tools/test-reports.ps1
@@ -13,28 +13,28 @@ $RepoRoot = Split-Path $PSScriptRoot -Parent
 
 try
 {
-    # Push-Location $RepoRoot
+    Push-Location $RepoRoot
 
-    $TestProject = "$RepoRoot/Test/Specflow/bin/$BuildConfiguration/$TargetFramework/Test.Specflow.dll"
-    $TestExecutionResult = "$RepoRoot/Test/Specflow/bin/$BuildConfiguration/$TargetFramework/TestExecution.json"
+    $TestProject = "./test/Specflow/bin/$BuildConfiguration/$TargetFramework/Test.Specflow.dll"
+    $TestExecutionResult = "./test/Specflow/bin/$BuildConfiguration/$TargetFramework/TestExecution.json"
 
     Write-Host "BuildConfiguration: '$BuildConfiguration'"
     Write-Host "TestProject: '$TestProject'"
     Write-Host "TestExecutionResult: '$TestExecutionResult'"
 
-    # if (!(Test-Path -Path $TestProject))
-    # {
-    #     Write-Error "Build project in '$BuildConfiguration' mode first"
-    # }
+    if (!(Test-Path -Path $TestProject))
+    {
+        Write-Error "Build project in '$BuildConfiguration' mode first"
+    }
 
-    # if (!(Test-Path -Path $TestExecutionResult))
-    # {
-    #     Write-Error "Execute 'dotnet test' before running this script"
-    # }
+    if (!(Test-Path -Path $TestExecutionResult))
+    {
+        Write-Error "Execute 'dotnet test' before running this script"
+    }
 
     dotnet livingdoc test-assembly $TestProject --test-execution-json $TestExecutionResult --output "$ReportFile"
 }
 finally
 {
-    # Pop-Location
+    Pop-Location
 }
