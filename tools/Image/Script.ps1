746456d6137918fba840bd8b92b549df82156689
Max Hamulyák <max@kaylumah.nl>
2023-09-17 12:04:09 +0200

    Replace NodeJS

---
+++ tools/Image/Script.ps1
@@ -0,0 +1,36 @@
+#Requires -Version 7.2
+
+$ToolVersion = "libwebp-1.3.2-mac-arm64"
+$ToolFile = "libwebp.tar.gz"
+
+if (-Not (Test-Path -Path $ToolFile))
+{
+    $SourceUrl = "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/$ToolVersion.tar.gz"
+    Invoke-WebRequest -Uri $SourceUrl -OutFile $ToolFile
+}
+else
+{
+    Write-Host "Already downloaded"
+}
+
+$ConversionToolBasePath = "bin/$ToolVersion"
+if (-Not (Test-Path -Path $ConversionToolBasePath))
+{
+    New-Item -ItemType Directory -Path "bin"
+    tar xvfz $ToolFile -C "bin"
+}
+else
+{
+    Write-Host "Already extracted"
+}
+
+$ConversionToolPath = "$ConversionToolBasePath/bin/cwebp"
+$Quality = 80
+
+$Images = Get-ChildItem -Recurse -Path "../../_site/assets/images/posts" -Filter cover_image.png
+foreach ($image in $Images)
+{
+    $ConvertedImageName = $image.BaseName + ".webp"
+    $ConvertedImageResultFullPath = [IO.Path]::Combine($image.DirectoryName, $ConvertedImageName)
+    &$ConversionToolPath -q $Quality $image.FullName -o $ConvertedImageResultFullPath
+}
