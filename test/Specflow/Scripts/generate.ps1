a578e53dec609e948e78766b0fa334f4014312b3
Max Hamulyák <max@kaylumah.nl>
2023-12-26 13:09:06 +0100

    cleanup scripts

--- test/Specflow/Scripts/generate.ps1
+++ test/Specflow/Scripts/generate.ps1
@@ -1,1 +1,3 @@
-../bin/Debug/net8.0/playwright.ps1 codegen
+$RepoRoot = Split-Path $PSScriptRoot -Parent
+$PlaywrightScript = "$RepoRoot/bin/Debug/net8.0/playwright.ps1"
+& $PlaywrightScript codegen
--- test/Specflow/Scripts/install.ps1
+++ test/Specflow/Scripts/install.ps1
@@ -1,1 +1,3 @@
-../bin/Debug/net8.0/playwright.ps1 install
+$RepoRoot = Split-Path $PSScriptRoot -Parent
+$PlaywrightScript = "$RepoRoot/bin/Debug/net8.0/playwright.ps1"
+& $PlaywrightScript install
