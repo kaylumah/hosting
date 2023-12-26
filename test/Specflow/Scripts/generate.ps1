d787973dcc625e6a96cdb30fd9978f5ac3713d9b
Max Hamulyák <max@kaylumah.nl>
2023-12-26 13:13:46 +0100

    make generate specific

--- test/Specflow/Scripts/generate.ps1
+++ test/Specflow/Scripts/generate.ps1
@@ -1,3 +1,3 @@
 $RepoRoot = Split-Path $PSScriptRoot -Parent
 $PlaywrightScript = "$RepoRoot/bin/Debug/net8.0/playwright.ps1"
-& $PlaywrightScript codegen
+& $PlaywrightScript codegen https://kaylumah.nl
