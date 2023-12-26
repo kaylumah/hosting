17c38642d415730cfa902ad2c2c0313ae0917068
Max Hamulyák <max@kaylumah.nl>
2023-12-26 13:22:08 +0100

    wip script

--- test/Specflow/Scripts/trace.ps1
+++ test/Specflow/Scripts/trace.ps1
@@ -1,11 +1,14 @@
-try {
-    Push-Location "../bin/Debug/net8.0"
-    # ./playwright.ps1 show-trace trace.zip
-    $TracePath = "$PWD/trace.zip"
-    ./playwright.ps1 show-trace $TracePath
-    # Test-Path -Path trace.zip
-}
-catch {}
-finally {
-    Pop-Location
-}
+$RepoRoot = Split-Path $PSScriptRoot -Parent
+$PlaywrightScript = "$RepoRoot/bin/Debug/net8.0/playwright.ps1"
+
+# try {
+#     Push-Location "../bin/Debug/net8.0"
+#     # ./playwright.ps1 show-trace trace.zip
+#     $TracePath = "$PWD/trace.zip"
+#     ./playwright.ps1 show-trace $TracePath
+#     # Test-Path -Path trace.zip
+# }
+# catch {}
+# finally {
+#     Pop-Location
+# }
