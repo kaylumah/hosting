200fbb69a108a9c42bef7c6e6bb6e70931307157
Max Hamulyák <max@kaylumah.nl>
2023-12-28 21:21:40 +0100

    fix copy

--- src/Utilities/Identifiers/GuidExtensions.cs
+++ src/Utilities/Identifiers/GuidExtensions.cs
@@ -1,7 +1,4 @@
-﻿
-
-
-// Copyright (c) Kaylumah, 2024. All rights reserved.
+﻿// Copyright (c) Kaylumah, 2024. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
 using Kaylumah.Ssg.Utilities;
--- test/Specflow/Extensions/SpecFlowExtensions.cs
+++ test/Specflow/Extensions/SpecFlowExtensions.cs
@@ -1,5 +1,4 @@
 ﻿
-
 // Copyright (c) Kaylumah, 2024. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
