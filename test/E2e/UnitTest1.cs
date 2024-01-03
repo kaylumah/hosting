db66d95727e8f3d6176f1f9bd09d7fe280e59ec3
Max Hamulyák <max@kaylumah.nl>
2024-01-03 15:21:28 +0100

    add MSTest to the project

---
+++ test/E2e/UnitTest1.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Kaylumah, 2024. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.E2e
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
