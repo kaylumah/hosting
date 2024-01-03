2ac3be4cfc6f983e78ea5f971ffc68576b597f86
Max Hamulyák <max@kaylumah.nl>
2024-01-03 16:04:20 +0100

    add test

--- test/E2e/UnitTest1.cs
+++ test/E2e/UnitTest1.cs
@@ -1,16 +1,24 @@
 // Copyright (c) Kaylumah, 2024. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Test.E2e
 {
     [TestClass]
-    public class UnitTest1
+#pragma warning disable CS3009 // Base type is not CLS-compliant
+    public class UnitTest1 : PlaywrightTest
+#pragma warning restore CS3009 // Base type is not CLS-compliant
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task TestMethod1()
         {
+            IAPIRequestContext context = await Playwright.APIRequest.NewContextAsync();
+            IAPIResponse response = await context.GetAsync("feed.xml");
+            await Expect(response).ToBeOKAsync();
         }
     }
 }
