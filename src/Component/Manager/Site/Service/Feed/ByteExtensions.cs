499b1ccc6c66302c5b5453a8e5bb045625396fff
Max Hamulyak <max.hamulyak@lateralinc.onmicrosoft.com>
2024-01-13 19:42:04 +0100

    move bute

---
+++ src/Component/Manager/Site/Service/Feed/ByteExtensions.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Kaylumah, 2024. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System.IO;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace System
+{
+    public static partial class ByteExtensions
+    {
+        public static SyndicationFeed ToSyndicationFeed(this byte[] bytes)
+        {
+            using MemoryStream stream = new MemoryStream(bytes);
+            using XmlReader xmlReader = XmlReader.Create(stream);
+            SyndicationFeed feed = SyndicationFeed.Load(xmlReader);
+            return feed;
+        }
+    }
+}
--- test/Specflow/Utilities/ArtifactAccessMockExtensions.cs
+++ test/Specflow/Utilities/ArtifactAccessMockExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Kaylumah, 2024. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System;
 using System.ServiceModel.Syndication;
 using HtmlAgilityPack;
 using Kaylumah.Ssg.Manager.Site.Service.SiteMap;
