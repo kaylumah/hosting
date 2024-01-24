7a150d5b1b24208f40ca77e2f99492bfbef6c4b2
Max Hamulyák <max@kaylumah.nl>
2024-01-24 16:04:01 +0100

    wip

--- test/E2e/UnitTest1.cs
+++ test/E2e/UnitTest1.cs
@@ -12,64 +12,9 @@ using Kaylumah.Ssg.Manager.Site.Service.SiteMap;
 using Microsoft.Playwright;
 using Xunit;
 
-#pragma warning disable CS3001 // Argument type is not CLS-compliant
-#pragma warning disable CS3003 // Type is not CLS-compliant
-#pragma warning disable CS3009 // Base type is not CLS-compliant
 #pragma warning disable CS3002 // Return type is not CLS-compliant
 namespace Test.E2e
 {
-    public class PlaywrightFixture : IAsyncLifetime
-    {
-        IPlaywright PlaywrightInstance { get; set; }
-        IBrowser Browser { get; set; }
-
-        public async Task DisposeAsync()
-        {
-            if (Browser != null)
-            {
-                await Browser.DisposeAsync();
-            }
-
-            PlaywrightInstance.Dispose();
-        }
-
-        public async Task InitializeAsync()
-        {
-            PlaywrightInstance = await Playwright.CreateAsync();
-            await GetBrowser();
-        }
-
-        public async Task<IBrowser> GetBrowser()
-        {
-            if (Browser == null)
-            {
-                Browser = await PlaywrightInstance.Chromium.LaunchAsync();
-            }
-
-            return Browser;
-        }
-
-        public async Task<IPage> GetPage()
-        {
-            BrowserNewContextOptions contextOptions = ContextOptions();
-            IBrowserContext context = await Browser.NewContextAsync(contextOptions);
-            IPage page = await context.NewPageAsync();
-            return page;
-        }
-
-        public BrowserNewContextOptions ContextOptions()
-        {
-            BrowserNewContextOptions browserNewContextOptions = new BrowserNewContextOptions();
-            browserNewContextOptions.BaseURL = GetBaseUrl();
-            return browserNewContextOptions;
-        }
-
-        public string GetBaseUrl()
-        {
-            string result = Environment.GetEnvironmentVariable("PLAYWRIGHT_TEST_BASE_URL") ?? "https://kaylumah.nl";
-            return result;
-        }
-    }
 
     public class UnitTest3 : IClassFixture<PlaywrightFixture>
     {
