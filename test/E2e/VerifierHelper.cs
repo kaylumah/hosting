d61195db1965a140e95aa05ebca4009f22b51864
Max Hamulyák <max@kaylumah.nl>
2024-01-25 17:49:57 +0100

    different regex

--- test/E2e/VerifierHelper.cs
+++ test/E2e/VerifierHelper.cs
@@ -7,7 +7,8 @@ namespace Test.E2e
 {
     public static partial class VerifierHelper
     {
-        [GeneratedRegex(@"(?<before>https://)(?<val>[a-zA-Z0-9\-\.]*(.net|.nl))(?<after>\/[\w/_-]*\.(html|xml|png|svg))?")]
+        //[GeneratedRegex(@"(?<before>https://)(?<val>[a-zA-Z0-9\-\.]*(.net|.nl))(?<after>\/[\w/_-]*\.(html|xml|png|svg))?")]
+        [GeneratedRegex(@"(?<before>https://)(?<val>(kaylumah.nl|green-field-0353fee03-[0-9]{3}.westeurope.1.azurestaticapps.net))")]
         public static partial Regex BaseUrl();
     }
 }
