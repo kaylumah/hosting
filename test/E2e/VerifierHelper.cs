fcb84feb29235f61741cf02ca97ceef5cbd3d83f
Max Hamulyák <max@kaylumah.nl>
2024-01-25 17:45:30 +0100

    WIP

--- test/E2e/VerifierHelper.cs
+++ test/E2e/VerifierHelper.cs
@@ -7,7 +7,7 @@ namespace Test.E2e
 {
     public static partial class VerifierHelper
     {
-        [GeneratedRegex(@"(?<before>https://)(?<val>[a-zA-Z0-9\-\.]*(.net|.nl))(?<after>\/[\w/_-]*\.(html|xml|png|svg))")]
+        [GeneratedRegex(@"(?<before>https://)(?<val>[a-zA-Z0-9\-\.]*(.net|.nl))(?<after>\/[\w/_-]*\.(html|xml|png|svg))?")]
         public static partial Regex BaseUrl();
     }
 }
