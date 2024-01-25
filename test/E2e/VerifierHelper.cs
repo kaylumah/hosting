69355abf743b3e7819e2bc9dba498eff4a780e73
Max Hamulyák <max@kaylumah.nl>
2024-01-25 19:34:28 +0100

    update regex

--- test/E2e/VerifierHelper.cs
+++ test/E2e/VerifierHelper.cs
@@ -8,7 +8,7 @@ namespace Test.E2e
     public static partial class VerifierHelper
     {
         //[GeneratedRegex(@"(?<before>https://)(?<val>[a-zA-Z0-9\-\.]*(.net|.nl))(?<after>\/[\w/_-]*\.(html|xml|png|svg))?")]
-        [GeneratedRegex(@"(?<before>https://)(?<val>(kaylumah.nl|green-field-0353fee03-[0-9]{3}.westeurope.1.azurestaticapps.net))")]
+        [GeneratedRegex(@"(?<before>https://)(?<val>(kaylumah.nl|green-field-0353fee03-[0-9]{3}.westeurope.1.azurestaticapps.net))(?<after>[\w\/\.\-]*)?")]
         public static partial Regex BaseUrl();
     }
 }
