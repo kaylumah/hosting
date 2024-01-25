931971bf0b682efd4fd70491e9f0d53fd2a1a899
Max Hamulyák <max@kaylumah.nl>
2024-01-25 15:45:34 +0100

    wip

--- test/E2e/UnitTest3.cs
+++ test/E2e/UnitTest3.cs
@@ -155,16 +155,16 @@ namespace Test.E2e
         {
             yield return new object[] { "2023/04/14/csharp-client-for-openapi-revistted.html" };
             yield return new object[] { "2022/09/17/how-to-use-azurite-for-testing-azure-storage-in-dotnet.html" };
-            // yield return new object[] { "2022/06/07/share-debug-configuration-with-launch-profiles.html" };
-            // yield return new object[] { "2022/02/21/working-with-azure-sdk-for-dotnet.html" };
-            // yield return new object[] { "2022/01/31/improve-code-quality-with-bannedsymbolanalyzers.html" };
-            // yield return new object[] { "2021/11/29/validated-strongly-typed-ioptions.html" };
-            // yield return new object[] { "2021/11/14/capture-logs-in-unit-tests.html" };
-            // yield return new object[] { "2021/07/17/decreasing-solution-build-time-with-filters.html" };
-            // yield return new object[] { "2021/05/23/generate-csharp-client-for-openapi.html" };
-            // yield return new object[] { "2021/04/11/an-approach-to-writing-mocks.html" };
-            // yield return new object[] { "2021/03/27/set-nuget-metadata-via-msbuild.html" };
-            // yield return new object[] { "2019/09/07/using-csharp-code-your-git-hooks.html" };
+            yield return new object[] { "2022/06/07/share-debug-configuration-with-launch-profiles.html" };
+            yield return new object[] { "2022/02/21/working-with-azure-sdk-for-dotnet.html" };
+            yield return new object[] { "2022/01/31/improve-code-quality-with-bannedsymbolanalyzers.html" };
+            yield return new object[] { "2021/11/29/validated-strongly-typed-ioptions.html" };
+            yield return new object[] { "2021/11/14/capture-logs-in-unit-tests.html" };
+            yield return new object[] { "2021/07/17/decreasing-solution-build-time-with-filters.html" };
+            yield return new object[] { "2021/05/23/generate-csharp-client-for-openapi.html" };
+            yield return new object[] { "2021/04/11/an-approach-to-writing-mocks.html" };
+            yield return new object[] { "2021/03/27/set-nuget-metadata-via-msbuild.html" };
+            yield return new object[] { "2019/09/07/using-csharp-code-your-git-hooks.html" };
         }
     }
 }
