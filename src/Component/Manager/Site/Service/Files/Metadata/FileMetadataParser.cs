4567f87fab80ec154e872572ad28c9dfafebe425
Max Hamulyák <max@kaylumah.nl>
2024-01-27 13:37:10 +0100

    loglevel

--- src/Component/Manager/Site/Service/Files/Metadata/FileMetadataParser.cs
+++ src/Component/Manager/Site/Service/Files/Metadata/FileMetadataParser.cs
@@ -16,7 +16,7 @@ namespace Kaylumah.Ssg.Manager.Site.Service.Files.Metadata
     {
         [LoggerMessage(
                EventId = 0,
-               Level = LogLevel.Information,
+               Level = LogLevel.Trace,
                Message = "Overwriting '{Key}' with '{NewValue}' instead of {OldValue} because '{Reason}'")]
         private partial void LogDataOverwriting(string key, string newValue, string oldValue, string reason);
         readonly ILogger _Logger;
