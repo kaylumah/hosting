7060a101b95a97002cab3856c423720c72534c62
Max Hamulyák <max@kaylumah.nl>
2024-01-27 15:26:35 +0100

    fix tests

---
+++ test/Unit/Steps/Utilities/FakeTimeProviderStepDefinitions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Kaylumah, 2024. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using Microsoft.Extensions.Time.Testing;
+using TechTalk.SpecFlow;
+
+namespace Test.Unit.Steps.Utilities
+{
+    [Binding]
+    public class FakeTimeProviderStepDefinitions
+    {
+        readonly FakeTimeProvider _FakeTimeProvider;
+
+        public FakeTimeProviderStepDefinitions(FakeTimeProvider fakeTimeProvider)
+        {
+            _FakeTimeProvider = fakeTimeProvider;
+        }
+
+        [Given(@"the current date is '(.*)':")]
+        public void GivenTheCurrentDateIs(DateTimeOffset systemDateTime)
+        {
+            _FakeTimeProvider.SetUtcNow(systemDateTime);
+        }
+    }
+}
