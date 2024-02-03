65701093b5f8a4291d6111c779dbde589f6e5341
Max Hamulyák <max@kaylumah.nl>
2024-02-03 17:27:47 +0100

    wip

--- test/E2e/BasePageObject.cs
+++ test/E2e/BasePageObject.cs
@@ -28,7 +28,7 @@ namespace Test.E2e
         public async Task<byte[]> ScreenshotAsync()
         {
             PageScreenshotOptions pageScreenshotOptions = new PageScreenshotOptions();
-            pageScreenshotOptions.FullPage = true;
+            pageScreenshotOptions.FullPage = false;
             byte[] bytes = await _Page.ScreenshotAsync(pageScreenshotOptions);
             return bytes;
         }
