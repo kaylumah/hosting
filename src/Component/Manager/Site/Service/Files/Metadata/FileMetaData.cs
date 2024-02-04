1984138c6d7f698101ffc458b169d7ae99534dff
Max Hamulyák <max@kaylumah.nl>
2024-02-04 16:37:18 +0100

    wip

--- src/Component/Manager/Site/Service/Files/Metadata/FileMetaData.cs
+++ src/Component/Manager/Site/Service/Files/Metadata/FileMetaData.cs
@@ -79,7 +79,13 @@ namespace Kaylumah.Ssg.Manager.Site.Service.Files.Metadata
         {
             get
             {
-                return this.GetValue<List<object>>(nameof(Tags))?.Cast<string>().ToList();
+                List<object?> result = this.GetValue<List<object?>>(nameof(Tags));
+                IEnumerable<string?> strings = result.Cast<string?>();
+                List<string> asList = strings
+                    .Where(x => x != null)
+                    .Select(x => x!)
+                    .ToList();
+                return asList;
             }
             set
             {
