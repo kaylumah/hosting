4995cfe40f34b63d484af85be63cd2efa44c4b01
Max Hamulyák <max@kaylumah.nl>
2024-02-04 13:07:30 +0100

    wip

--- src/Utilities/Ssg.Extensions.Metadata.Abstractions/Metadata.cs
+++ src/Utilities/Ssg.Extensions.Metadata.Abstractions/Metadata.cs
@@ -9,5 +9,11 @@ namespace Ssg.Extensions.Metadata.Abstractions
         { get; set; }
         public string Content
         { get; set; }
+
+        public Metadata(string content, T data)
+        {
+            Content = content;
+            Data = data;
+        }
     }
 }
