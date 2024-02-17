7a4ced530d58b31123578ee32138a22eb2f81c73
Max Hamulyák <max@kaylumah.nl>
2024-02-17 08:06:58 +0100

    formatting

--- test/E2e/VerifierHelper.cs
+++ test/E2e/VerifierHelper.cs
@@ -23,8 +23,8 @@ namespace Test.E2e
 
     public static class BasePageVerifier
     {
-            [ModuleInitializer]
-            public static void Init() => VerifyImageHash.Initialize();
+        [ModuleInitializer]
+        public static void Init() => VerifyImageHash.Initialize();
 
         public static async Task VerifyScreenshot(BasePageObject basePageObject)
         {
