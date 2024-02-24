f1d3f28cc0ec91f0a02fe74c763aca6c84b26f66
Max Hamulyák <max@kaylumah.nl>
2024-02-24 20:04:48 +0100

    WIP

--- src/Utilities/Ssg.Extensions.Metadata.Abstractions/PageMetaData.cs
+++ src/Utilities/Ssg.Extensions.Metadata.Abstractions/PageMetaData.cs
@@ -4,10 +4,114 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 
 namespace Ssg.Extensions.Metadata.Abstractions
 {
+    public static class PageMetaDataExtensions
+    {
+        public static readonly Func<PageMetaData, bool> Tags;
+
+        public static readonly Func<Article, bool> Series;
+
+        public static readonly Func<PageMetaData, bool> Html;
+
+        public static readonly Func<Article, bool> Featured;
+
+        static PageMetaDataExtensions()
+        {
+            Tags = (page) => page.Tags != null && 0 < page.Tags.Count;
+            Series = (page) => !string.IsNullOrEmpty(page.Series);
+            Html = (page) => page.IsHtml();
+            Featured = (page) => page.Featured;
+        }
+
+        public static string GetExtension(this PageMetaData pageMetaData)
+        {
+            string result = Path.GetExtension(pageMetaData.Uri);
+            return result;
+        }
+
+        public static bool IsExtension(this PageMetaData pageMetaData, string target)
+        {
+            ArgumentNullException.ThrowIfNull(target);
+            string actual = pageMetaData.GetExtension();
+            bool result = target.Equals(actual, StringComparison.Ordinal);
+            return result;
+        }
+
+        public static bool IsHtml(this PageMetaData pageMetaData)
+        {
+            bool result = pageMetaData.IsExtension(".html");
+            return result;
+        }
+
+        public static IEnumerable<PageMetaData> HasTag(this IEnumerable<PageMetaData> source)
+        {
+            IEnumerable<PageMetaData> result = source.Where(Tags);
+            return result;
+        }
+
+        public static IEnumerable<PageMetaData> FromTag(this IEnumerable<PageMetaData> source, string tag)
+        {
+            IEnumerable<PageMetaData> result = source
+                    .HasTag()
+                    .Where(page => page.Tags.Contains(tag));
+            return result;
+        }
+
+        public static IEnumerable<Article> HasSeries(this IEnumerable<Article> source)
+        {
+            IEnumerable<Article> result = source.Where(Series);
+            return result;
+        }
+
+        public static IEnumerable<Article> FromSeries(this IEnumerable<Article> source, string series)
+        {
+            IEnumerable<Article> result = source
+                    .HasSeries()
+                    .Where(page => page.Series.Equals(series, StringComparison.Ordinal));
+            return result;
+        }
+
+        public static bool IsContentType(this PageMetaData page, string contentType)
+        {
+            bool result = page.Type != null && page.Type.Equals(contentType, StringComparison.OrdinalIgnoreCase);
+            return result;
+        }
+
+        public static bool IsArticle(this PageMetaData page)
+        {
+            bool result = page.IsContentType("Article");
+            return result;
+        }
+
+        public static bool IsPage(this PageMetaData page)
+        {
+            bool result = page.IsContentType("Page");
+            return result;
+        }
+
+        public static IEnumerable<PageMetaData> IsArticle(this IEnumerable<PageMetaData> source)
+        {
+            IEnumerable<PageMetaData> result = source.Where(IsArticle);
+            return result;
+        }
+
+        public static IEnumerable<Article> IsFeatured(this IEnumerable<Article> source)
+        {
+            IEnumerable<Article> result = source.Where(Featured);
+            return result;
+        }
+
+        public static IEnumerable<PageMetaData> ByRecentlyPublished(this IEnumerable<PageMetaData> source)
+        {
+            IOrderedEnumerable<PageMetaData> result = source.OrderByDescending(x => x.Published);
+            return result;
+        }
+    }
+
     [DebuggerDisplay("PageMetaData '{Uri}'")]
     public class PageMetaData
     {
