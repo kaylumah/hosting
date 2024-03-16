47f982eb4109d59da592d1854afad76dda341175
Max Hamulyák <max@kaylumah.nl>
2024-03-16 11:06:07 +0100

    WIP

---
+++ test/E2e/SnapshotTests/TempTests.cs
@@ -0,0 +1,32 @@
+// Copyright (c) Kaylumah, 2024. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+using Microsoft.Playwright;
+using Xunit;
+
+namespace Test.E2e.SnapshotTests
+{
+    public class TempPageHtmlTests : IClassFixture<MobileFixture2>
+    {
+        readonly MobileFixture2 _MobileFixture;
+
+        public TempPageHtmlTests(MobileFixture2 mobileFixture)
+        {
+            _MobileFixture = mobileFixture;
+        }
+
+        public void Test1()
+        {
+
+        }
+    }
+
+    public class MobileFixture2 : PlaywrightFixture
+    {
+        protected override BrowserNewContextOptions CreateBrowserNewContextOptions()
+        {
+            BrowserNewContextOptions result = new BrowserNewContextOptions();
+            return result;
+        }
+    }
+}
