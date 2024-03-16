6f709c16a831dff42f5c7863dc04cbba9d7aefec
Max Hamulyák <max@kaylumah.nl>
2024-03-16 09:24:30 +0100

    WIP

--- test/Unit/FormerXunit/FileProcessorTests.cs
+++ test/Unit/FormerXunit/FileProcessorTests.cs
@@ -224,7 +224,16 @@ namespace Test.Unit.FormerXunit
             SiteInfo siteInfo = new SiteInfo();
             IYamlParser yamlParser = new YamlParser();
             YamlFrontMatterMetadataProvider metadataProvider = new YamlFrontMatterMetadataProvider(yamlParser);
-            MetadataParserOptions metadataParserOptions = new MetadataParserOptions();
+            MetadataParserOptions metadataParserOptions = new MetadataParserOptions
+            {
+                Defaults = new DefaultMetadatas {
+                    new DefaultMetadata {
+                        Path = string.Empty,
+                        Extensions = [ ".html" ],
+                        Values = new FileMetaData {}
+                    }
+                }
+            };
             FileProcessor sut = new FileProcessor(mockFileSystem, logger, preprocessorStrategies, siteInfo, metadataProvider, metadataParserOptions);
 
             MetadataCriteria criteria = new MetadataCriteria()
@@ -240,40 +249,6 @@ namespace Test.Unit.FormerXunit
             result.FrontMatter.Uri.Should().Be("file.html");
         }
 
-        //         [Fact]
-        //         public void Test_FilemetadataParser_EmptyFileWithConfigThatIsEmptyOnlyGetsDefaultValues()
-        //         {
-        //             // Arange
-        //             MetadataParserOptions metadataParserOptions = new MetadataParserOptions
-        //             {
-        //                 Defaults = new DefaultMetadatas {
-        //                     new DefaultMetadata {
-        //                         Path = string.Empty,
-        //                         Extensions = [ ".html" ],
-        //                         Values = new FileMetaData {}
-        //                     }
-        //                 }
-        //             };
-        //             Mock<IFrontMatterMetadataProvider> metadataProviderMock = new Mock<IFrontMatterMetadataProvider>();
-
-        //             metadataProviderMock
-        //                 .Setup(x => x.Retrieve<FileMetaData>(It.Is<string>(p => p.Equals(string.Empty))))
-        //                 .Returns(new ParsedFile<FileMetaData>(null, null));
-
-        //             Mock<ILogger<FileParser>> loggerMock = new Mock<ILogger<FileParser>>();
-        //             FileParser sut = new FileParser(loggerMock.Object, metadataProviderMock.Object, options);
-        //             MetadataCriteria criteria = new MetadataCriteria
-        //             {
-        //                 Content = string.Empty,
-        //                 FileName = "file.html"
-        //             };
-
-        //             // Act
-        //             ParsedFile<FileMetaData> result = sut.Parse(criteria);
-
-        //             // Assert
-        //         }
-
         [Fact]
         public void Test_FilemetadataParser_EmptyFileWithConfigTGetsDefaultValuesAndConfiguration()
         {
@@ -283,7 +258,18 @@ namespace Test.Unit.FormerXunit
             SiteInfo siteInfo = new SiteInfo();
             IYamlParser yamlParser = new YamlParser();
             YamlFrontMatterMetadataProvider metadataProvider = new YamlFrontMatterMetadataProvider(yamlParser);
-            MetadataParserOptions metadataParserOptions = new MetadataParserOptions();
+            MetadataParserOptions metadataParserOptions = new MetadataParserOptions
+            {
+                Defaults = new DefaultMetadatas {
+                            new DefaultMetadata {
+                                Path = string.Empty,
+                                Extensions = [ ".html" ],
+                                Values = new FileMetaData {
+                                    Layout = "default.html"
+                                }
+                            }
+                        }
+            };
             FileProcessor sut = new FileProcessor(mockFileSystem, logger, preprocessorStrategies, siteInfo, metadataProvider, metadataParserOptions);
 
             MetadataCriteria criteria = new MetadataCriteria()
@@ -301,43 +287,6 @@ namespace Test.Unit.FormerXunit
             result.FrontMatter.Layout.Should().Be("default.html");
         }
 
-        //         [Fact]
-        //         public void Test_FilemetadataParser_EmptyFileWithConfigTGetsDefaultValuesAndConfiguration()
-        //         {
-        //             // Arange
-        //             MetadataParserOptions metadataParserOptions = new MetadataParserOptions
-        //             {
-        //                 Defaults = new DefaultMetadatas {
-        //                     new DefaultMetadata {
-        //                         Path = string.Empty,
-        //                         Extensions = [ ".html" ],
-        //                         Values = new FileMetaData {
-        //                             Layout = "default.html"
-        //                         }
-        //                     }
-        //                 }
-        //             };
-        //             Mock<IFrontMatterMetadataProvider> metadataProviderMock = new Mock<IFrontMatterMetadataProvider>();
-
-        //             metadataProviderMock
-        //                 .Setup(x => x.Retrieve<FileMetaData>(It.Is<string>(p => p.Equals(string.Empty))))
-        //                 .Returns(new ParsedFile<FileMetaData>(null, null));
-
-        //             Mock<ILogger<FileParser>> loggerMock = new Mock<ILogger<FileParser>>();
-        //             FileParser sut = new FileParser(loggerMock.Object, metadataProviderMock.Object, options);
-        //             MetadataCriteria criteria = new MetadataCriteria
-        //             {
-        //                 Content = string.Empty,
-        //                 FileName = "file.html"
-        //             };
-
-        //             // Act
-        //             ParsedFile<FileMetaData> result = sut.Parse(criteria);
-
-        //             // Assert
-
-        //         }
-
         [Fact]
         public void Test_FilemetadataParser_EmptyFileWithConfigTGetsDefaultValuesAndMultipleConfigurations()
         {
@@ -347,7 +296,25 @@ namespace Test.Unit.FormerXunit
             SiteInfo siteInfo = new SiteInfo();
             IYamlParser yamlParser = new YamlParser();
             YamlFrontMatterMetadataProvider metadataProvider = new YamlFrontMatterMetadataProvider(yamlParser);
-            MetadataParserOptions metadataParserOptions = new MetadataParserOptions();
+            MetadataParserOptions metadataParserOptions = new MetadataParserOptions
+            {
+                Defaults = new DefaultMetadatas {
+                            new DefaultMetadata {
+                                Path = string.Empty,
+                                Extensions = [ ".html" ],
+                                Values = new FileMetaData {
+                                    Layout = "default.html"
+                                }
+                            },
+                            new DefaultMetadata {
+                                Path = "test",
+                                Extensions = [ ".html" ],
+                                Values = new FileMetaData {
+                                    Collection = "test"
+                                }
+                            }
+                        }
+            };
             FileProcessor sut = new FileProcessor(mockFileSystem, logger, preprocessorStrategies, siteInfo, metadataProvider, metadataParserOptions);
 
             MetadataCriteria criteria = new MetadataCriteria()
@@ -367,53 +334,6 @@ namespace Test.Unit.FormerXunit
             result.FrontMatter.Collection.Should().Be("test");
         }
 
-        //         [Fact]
-        //         public void Test_FilemetadataParser_EmptyFileWithConfigTGetsDefaultValuesAndMultipleConfigurations()
-        //         {
-        //             // Arange
-        //             MetadataParserOptions options = new MetadataParserOptions
-        //             {
-        //                 Defaults = new DefaultMetadatas {
-        //                     new DefaultMetadata {
-        //                         Path = string.Empty,
-        //                         Extensions = [ ".html" ],
-        //                         Values = new FileMetaData {
-        //                             Layout = "default.html"
-        //                         }
-        //                     },
-        //                     new DefaultMetadata {
-        //                         Path = "test",
-        //                         Extensions = [ ".html" ],
-        //                         Values = new FileMetaData {
-        //                             Collection = "test"
-        //                         }
-        //                     }
-        //                 }
-        //             };
-        //             Mock<IFrontMatterMetadataProvider> metadataProviderMock = new Mock<IFrontMatterMetadataProvider>();
-        //             FileMetaData data = new FileMetaData
-        //             {
-        //                 OutputLocation = "test/:name:ext"
-        //             };
-        //             metadataProviderMock
-        //                 .Setup(x => x.Retrieve<FileMetaData>(It.Is<string>(p => p.Equals(string.Empty))))
-        //                 .Returns(new ParsedFile<FileMetaData>(null, data));
-
-        //             Mock<ILogger<FileParser>> loggerMock = new Mock<ILogger<FileParser>>();
-        //             FileParser sut = new FileParser(loggerMock.Object, metadataProviderMock.Object, options);
-        //             MetadataCriteria criteria = new MetadataCriteria
-        //             {
-        //                 Content = string.Empty,
-        //                 FileName = "test/file.html"
-        //             };
-
-        //             // Act
-        //             ParsedFile<FileMetaData> result = sut.Parse(criteria);
-
-        //             // Assert
-
-        //         }
-
         [Fact]
         public void Test_FilemetadataParser_EmptyFileIfMultipleConfigurationsApplyLastOneWins()
         {
@@ -423,7 +343,26 @@ namespace Test.Unit.FormerXunit
             SiteInfo siteInfo = new SiteInfo();
             IYamlParser yamlParser = new YamlParser();
             YamlFrontMatterMetadataProvider metadataProvider = new YamlFrontMatterMetadataProvider(yamlParser);
-            MetadataParserOptions metadataParserOptions = new MetadataParserOptions();
+            MetadataParserOptions metadataParserOptions = new MetadataParserOptions
+            {
+                Defaults = new DefaultMetadatas {
+                            new DefaultMetadata {
+                                Path = string.Empty,
+                                Extensions = [ ".html" ],
+                                Values = new FileMetaData {
+                                    Layout = "default.html"
+                                }
+                            },
+                            new DefaultMetadata {
+                                Path = "test",
+                                Extensions = [ ".html" ],
+                                Values = new FileMetaData {
+                                    Layout = "other.html",
+                                    Collection = "test"
+                                }
+                            }
+                        }
+            };
             FileProcessor sut = new FileProcessor(mockFileSystem, logger, preprocessorStrategies, siteInfo, metadataProvider, metadataParserOptions);
 
             MetadataCriteria criteria = new MetadataCriteria()
@@ -443,55 +382,6 @@ namespace Test.Unit.FormerXunit
             result.FrontMatter.Collection.Should().Be("test");
         }
 
-        //         [Fact]
-        //         public void Test_FilemetadataParser_EmptyFileIfMultipleConfigurationsApplyLastOneWins()
-        //         {
-        //             // Arange
-        //             MetadataParserOptions options = new MetadataParserOptions
-        //             {
-        //                 Defaults = new DefaultMetadatas {
-        //                     new DefaultMetadata {
-        //                         Path = string.Empty,
-        //                         Extensions = [ ".html" ],
-        //                         Values = new FileMetaData {
-        //                             Layout = "default.html"
-        //                         }
-        //                     },
-        //                     new DefaultMetadata {
-        //                         Path = "test",
-        //                         Extensions = [ ".html" ],
-        //                         Values = new FileMetaData {
-        //                             Layout = "other.html",
-        //                             Collection = "test"
-        //                         }
-        //                     }
-        //                 }
-        //             };
-        //             Mock<IFrontMatterMetadataProvider> metadataProviderMock = new Mock<IFrontMatterMetadataProvider>();
-
-        //             FileMetaData meta = new FileMetaData()
-        //             {
-        //                 OutputLocation = "test/:name:ext"
-        //             };
-        //             metadataProviderMock
-        //                 .Setup(x => x.Retrieve<FileMetaData>(It.Is<string>(p => p.Equals(string.Empty))))
-        //                 .Returns(new ParsedFile<FileMetaData>(null, meta));
-
-        //             LoggerMock<FileParser> loggerMock = new LoggerMock<FileParser>();
-        //             FileParser sut = new FileParser(loggerMock.Object, metadataProviderMock.Object, options);
-        //             MetadataCriteria criteria = new MetadataCriteria
-        //             {
-        //                 Content = string.Empty,
-        //                 FileName = "test/file.html"
-        //             };
-
-        //             // Act
-        //             ParsedFile<FileMetaData> result = sut.Parse(criteria);
-
-        //             // Assert
-
-        //         }
-
         [Fact]
         public void Test_FilemetadataParser_MultipleLayers()
         {
@@ -501,7 +391,25 @@ namespace Test.Unit.FormerXunit
             SiteInfo siteInfo = new SiteInfo();
             IYamlParser yamlParser = new YamlParser();
             YamlFrontMatterMetadataProvider metadataProvider = new YamlFrontMatterMetadataProvider(yamlParser);
-            MetadataParserOptions metadataParserOptions = new MetadataParserOptions();
+            MetadataParserOptions metadataParserOptions = new MetadataParserOptions
+            {
+                Defaults = new DefaultMetadatas {
+                            new DefaultMetadata {
+                                Path = string.Empty,
+                                Extensions = [ ".html" ],
+                                Values = new FileMetaData {
+                                    Layout = "default.html"
+                                }
+                            },
+                            new DefaultMetadata {
+                                Path = "test",
+                                Extensions = [ ".html" ],
+                                Values = new FileMetaData {
+                                    Collection = "test"
+                                }
+                            }
+                        }
+            };
             FileProcessor sut = new FileProcessor(mockFileSystem, logger, preprocessorStrategies, siteInfo, metadataProvider, metadataParserOptions);
 
             MetadataCriteria criteria = new MetadataCriteria()
@@ -518,55 +426,6 @@ namespace Test.Unit.FormerXunit
             result.FrontMatter.Layout.Should().NotBeNull();
             result.FrontMatter.Layout.Should().Be("default.html");
         }
-
-        //         [Fact]
-        //         public void Test_FilemetadataParser_MultipleLayers()
-        //         {
-        //             // Arange
-        //             MetadataParserOptions options = new MetadataParserOptions
-        //             {
-        //                 Defaults = new DefaultMetadatas {
-        //                     new DefaultMetadata {
-        //                         Path = string.Empty,
-        //                         Extensions = [ ".html" ],
-        //                         Values = new FileMetaData {
-        //                             Layout = "default.html"
-        //                         }
-        //                     },
-        //                     new DefaultMetadata {
-        //                         Path = "test",
-        //                         Extensions = [ ".html" ],
-        //                         Values = new FileMetaData {
-        //                             Collection = "test"
-        //                         }
-        //                     }
-        //                 }
-        //             };
-        //             Mock<IFrontMatterMetadataProvider> metadataProviderMock = new Mock<IFrontMatterMetadataProvider>();
-
-        //             FileMetaData data = new FileMetaData
-        //             {
-        //                 OutputLocation = "posts/2021/:name:ext"
-        //             };
-
-        //             metadataProviderMock
-        //                 .Setup(x => x.Retrieve<FileMetaData>(It.Is<string>(p => p.Equals(string.Empty))))
-        //                 .Returns(new ParsedFile<FileMetaData>(null, data));
-
-        //             Mock<ILogger<FileParser>> loggerMock = new Mock<ILogger<FileParser>>();
-        //             FileParser sut = new FileParser(loggerMock.Object, metadataProviderMock.Object, options);
-        //             MetadataCriteria criteria = new MetadataCriteria
-        //             {
-        //                 Content = string.Empty,
-        //                 FileName = "posts/2021/file.html"
-        //             };
-
-        //             // Act
-        //             ParsedFile<FileMetaData> result = sut.Parse(criteria);
-
-        //             // Assert
-
-        //         }
 
         static string CreateFrontMatter(Dictionary<string, object> data = null)
         {
