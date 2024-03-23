f4def351818c726ba5a2b5dc902f79f73873bce3
Max Hamulyák <max@kaylumah.nl>
2024-03-23 22:11:37 +0100

    Redesign blog for sidemenu (#658)

--- src/Component/Manager/Site/Service/ObjectConversions.cs
+++ src/Component/Manager/Site/Service/ObjectConversions.cs
@@ -1,6 +1,9 @@
 // Copyright (c) Kaylumah, 2024. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Ssg.Extensions.Metadata.Abstractions;
 
 namespace Kaylumah.Ssg.Manager.Site.Service
@@ -10,6 +13,30 @@ namespace Kaylumah.Ssg.Manager.Site.Service
         public static AuthorId AuthorId(string author)
         {
             return author;
+        }
+
+        public static IEnumerable<Article> ArticlesForTag(SiteMetaData site, string tag, int? take = null)
+        {
+            ArgumentNullException.ThrowIfNull(site);
+            ArgumentNullException.ThrowIfNull(tag);
+
+            bool tagExists = site.Tags.TryGetValue(tag, out PageMetaData[]? resultForTag);
+            IEnumerable<Article> result;
+            if (tagExists && resultForTag != null)
+            {
+                IEnumerable<Article> asArticles = resultForTag.OfType<Article>();
+                result = asArticles.ByRecentlyPublished();
+                if (take != null)
+                {
+                    result = result.Take(take.Value);
+                }
+            }
+            else
+            {
+                result = Enumerable.Empty<Article>();
+            }
+
+            return result;
         }
     }
 }
