7b100a57e30496085c2b2d0e79e66691e5f6907a
Max Hamulyák <max@kaylumah.nl>
2024-03-23 11:58:48 +0100

    WIp

--- src/Component/Manager/Site/Service/ObjectConversions.cs
+++ src/Component/Manager/Site/Service/ObjectConversions.cs
@@ -1,6 +1,9 @@
 // Copyright (c) Kaylumah, 2024. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Ssg.Extensions.Metadata.Abstractions;
 
 namespace Kaylumah.Ssg.Manager.Site.Service
@@ -10,6 +13,13 @@ namespace Kaylumah.Ssg.Manager.Site.Service
         public static AuthorId AuthorId(string author)
         {
             return author;
+        }
+
+        public static IEnumerable<Article> ArticlesForTag(SiteMetaData site)
+        {
+            ArgumentNullException.ThrowIfNull(site);
+            IEnumerable<Article> result = Enumerable.Empty<Article>();
+            return result;
         }
     }
 }
