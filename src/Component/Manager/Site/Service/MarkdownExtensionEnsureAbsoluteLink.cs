5ae0200cd0c1fbce0b95157148c0ee23ddae0ff6
Max Hamulyák <max@kaylumah.nl>
2024-03-31 20:49:04 +0200

    WIP

--- src/Component/Manager/Site/Service/MarkdownExtensionEnsureAbsoluteLink.cs
+++ src/Component/Manager/Site/Service/MarkdownExtensionEnsureAbsoluteLink.cs
@@ -68,38 +68,6 @@ namespace Kaylumah.Ssg.Utilities
                 return result;
             };
 
-            // string anchorUrl = anchor.Url!;
-            // if (!anchor.IsImage)
-            // {
-            //     bool selfLink = anchorUrl.StartsWith('#');
-            //     if (selfLink)
-            //     {
-            //         return false;
-            //     }
-
-            //     bool isRelative = anchorUrl.StartsWith('/');
-            //     if (isRelative)
-            //     {
-            //         Uri uri = GlobalFunctions.AbsoluteUri(anchorUrl);
-            //         anchorUrl = uri.ToString();
-            //         anchor.Url = anchorUrl;
-            //     }
-
-            //     if (!anchorUrl.StartsWith(GlobalFunctions.Url.Value!, StringComparison.Ordinal))
-            //     {
-            //         anchor.GetAttributes().AddClass("external");
-            //     }
-            // }
-
-            // // TODO disable pending Medium response...
-            // if (anchor.IsImage)
-            // {
-            //     if (anchorUrl.StartsWith("/assets", StringComparison.Ordinal))
-            //     {
-            //         anchor.Url = GlobalFunctions.Url.Value + anchor.Url;
-            //     }
-            // }
-
             return false;
         }
     }
