b454fa4ba3d087fffa79f47c6d3061ecb29327da
Max Hamulyák <max@kaylumah.nl>
2024-03-31 11:34:41 +0200

    WIP

--- src/Component/Manager/Site/Service/MarkdownUtil.cs
+++ src/Component/Manager/Site/Service/MarkdownUtil.cs
@@ -60,56 +60,58 @@ namespace Kaylumah.Ssg.Utilities
                 .UseGenericAttributes()
                 .Build();
 
-            MarkdownDocument doc = Markdown.Parse(source, pipeline);
+            string result = Markdown.ToHtml(source, pipeline);
 
-            // Process headings to insert an intermediate LinkInline
-            IEnumerable<HeadingBlock> blocks = doc.Descendants<HeadingBlock>();
-            foreach (HeadingBlock headingBlock in blocks)
-            {
-                LinkInline inline = new LinkInline($"#{headingBlock.GetAttributes().Id}", null!);
-                ContainerInline previousInline = headingBlock.Inline!;
-                headingBlock.Inline = null;
-                inline.AppendChild(previousInline);
-                headingBlock.Inline = inline;
-            }
+            // MarkdownDocument doc = Markdown.Parse(source, pipeline);
 
-            IEnumerable<LinkInline> anchorTags = doc.Descendants<LinkInline>();
-            foreach (LinkInline anchor in anchorTags)
-            {
-                string anchorUrl = anchor.Url!;
-                if (!anchor.IsImage)
-                {
-                    bool isRelative = anchorUrl.StartsWith('/');
-                    if (isRelative)
-                    {
-                        Uri uri = GlobalFunctions.AbsoluteUri(anchorUrl);
-                        anchorUrl = uri.ToString();
-                        anchor.Url = anchorUrl;
-                    }
+            // // Process headings to insert an intermediate LinkInline
+            // IEnumerable<HeadingBlock> blocks = doc.Descendants<HeadingBlock>();
+            // foreach (HeadingBlock headingBlock in blocks)
+            // {
+            //     LinkInline inline = new LinkInline($"#{headingBlock.GetAttributes().Id}", null!);
+            //     ContainerInline previousInline = headingBlock.Inline!;
+            //     headingBlock.Inline = null;
+            //     inline.AppendChild(previousInline);
+            //     headingBlock.Inline = inline;
+            // }
 
-                    if (!anchorUrl.StartsWith(GlobalFunctions.Url.Value!, StringComparison.Ordinal))
-                    {
-                        anchor.GetAttributes().AddClass("external");
-                    }
-                }
+            // IEnumerable<LinkInline> anchorTags = doc.Descendants<LinkInline>();
+            // foreach (LinkInline anchor in anchorTags)
+            // {
+            //     string anchorUrl = anchor.Url!;
+            //     if (!anchor.IsImage)
+            //     {
+            //         bool isRelative = anchorUrl.StartsWith('/');
+            //         if (isRelative)
+            //         {
+            //             Uri uri = GlobalFunctions.AbsoluteUri(anchorUrl);
+            //             anchorUrl = uri.ToString();
+            //             anchor.Url = anchorUrl;
+            //         }
 
-                // TODO disable pending Medium response...
-                if (anchor.IsImage)
-                {
-                    if (anchorUrl.StartsWith("/assets", StringComparison.Ordinal))
-                    {
-                        anchor.Url = GlobalFunctions.Url.Value + anchor.Url;
-                    }
-                }
-            }
+            //         if (!anchorUrl.StartsWith(GlobalFunctions.Url.Value!, StringComparison.Ordinal))
+            //         {
+            //             anchor.GetAttributes().AddClass("external");
+            //         }
+            //     }
 
-            // Render the doc
-            StringWriter writer = new StringWriter();
-            HtmlRenderer renderer = new HtmlRenderer(writer);
-            pipeline.Setup(renderer);
-            renderer.Render(doc);
+            //     // TODO disable pending Medium response...
+            //     if (anchor.IsImage)
+            //     {
+            //         if (anchorUrl.StartsWith("/assets", StringComparison.Ordinal))
+            //         {
+            //             anchor.Url = GlobalFunctions.Url.Value + anchor.Url;
+            //         }
+            //     }
+            // }
 
-            string result = writer.ToString().Trim();
+            // // Render the doc
+            // StringWriter writer = new StringWriter();
+            // HtmlRenderer renderer = new HtmlRenderer(writer);
+            // pipeline.Setup(renderer);
+            // renderer.Render(doc);
+
+            // string result = writer.ToString().Trim();
             return result;
         }
     }
