133ef8c21b729f05fad3362e5445654c2b1446f3
Max Hamulyák <max@kaylumah.nl>
2024-03-31 17:50:59 +0200

    WIP

--- test/Unit/FormerXunit/MarkdownTests.cs
+++ test/Unit/FormerXunit/MarkdownTests.cs
@@ -1,85 +1,15 @@
 ﻿// Copyright (c) Kaylumah, 2024. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
-using System.Diagnostics;
 using System.Linq;
 using FluentAssertions;
 using HtmlAgilityPack;
 using Kaylumah.Ssg.Utilities;
 using Markdig;
-using Markdig.Renderers;
-using Markdig.Renderers.Html.Inlines;
-using Markdig.Syntax.Inlines;
 using Xunit;
 
 namespace Test.Unit.FormerXunit
 {
-
-    class PictureInline : IMarkdownExtension
-    {
-        void IMarkdownExtension.Setup(MarkdownPipelineBuilder pipeline)
-        {
-            // Empty on purpose
-        }
-
-        void IMarkdownExtension.Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
-        {
-            if (renderer is HtmlRenderer htmlRenderer)
-            {
-                LinkInlineRenderer inlineRenderer = htmlRenderer.ObjectRenderers.FindExact<LinkInlineRenderer>();
-                inlineRenderer.TryWriters.Add(TryLinkInlineRenderer);
-            }
-        }
-
-        bool TryLinkInlineRenderer(HtmlRenderer renderer, LinkInline linkInline)
-        {
-            if (linkInline == null)
-            {
-                return false;
-            }
-
-            if (linkInline.IsImage == false)
-            {
-                return false;
-            }
-
-            // handle .gif?
-
-            renderer.Write("<picture>");
-            WriteImageTag(renderer, linkInline, ".webp", "image/webp");
-            WriteImageTag(renderer, linkInline, string.Empty);
-            renderer.Write("</picture>");
-            return true;
-        }
-
-        void WriteImageTag(HtmlRenderer renderer, LinkInline link, string suffix, string type = null)
-        {
-            renderer.Write(string.IsNullOrWhiteSpace(type) ? $"<img loading=\"lazy\" src=\"" : $"<source type=\"{type}\" srcset=\"");
-            string escapeUrl = link.GetDynamicUrl != null ? link.GetDynamicUrl() ?? link.Url : link.Url;
-
-            renderer.WriteEscapeUrl($"{escapeUrl}{suffix}");
-            renderer.Write("\"");
-            renderer.WriteAttributes(link);
-            if (renderer.EnableHtmlForInline)
-            {
-                renderer.Write(" alt=\"");
-            }
-
-            bool wasEnableHtmlForInline = renderer.EnableHtmlForInline;
-            renderer.EnableHtmlForInline = false;
-            renderer.WriteChildren(link);
-            renderer.EnableHtmlForInline = wasEnableHtmlForInline;
-            if (renderer.EnableHtmlForInline)
-            {
-                renderer.Write("\"");
-            }
-
-            if (renderer.EnableHtmlForInline)
-            {
-                renderer.Write(" />");
-            }
-        }
-    }
 
     public class MarkdownTests
     {
