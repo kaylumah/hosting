1d2c2b321b8457a3cd25a531da4921406872ef89
Max Hamulyák <max@kaylumah.nl>
2024-04-04 21:00:22 +0200

    wip

--- src/Component/Manager/Site/Service/Files/Processor/FileExtensions.cs
+++ src/Component/Manager/Site/Service/Files/Processor/FileExtensions.cs
@@ -3,8 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using HtmlAgilityPack;
 using Ssg.Extensions.Metadata.Abstractions;
 
 namespace Kaylumah.Ssg.Manager.Site.Service.Files.Processor
