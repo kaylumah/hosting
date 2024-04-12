f5bb923efe69f494a4114664d31de8335265d4f2
Max Hamulyák <max@kaylumah.nl>
2024-04-12 07:42:34 +0200

    wip

--- src/Component/Manager/Site/Service/Seo/StructureDataGenerator.cs
+++ src/Component/Manager/Site/Service/Seo/StructureDataGenerator.cs
@@ -100,9 +100,9 @@ namespace Kaylumah.Ssg.Manager.Site.Service.Seo
             string keywords = string.Join(',', page.Tags);
             blogPost.Keywords = keywords;
 
-            if (page.Image != null)
+            if (page.WebImage != null)
             {
-                blogPost.Image = new Values<IImageObject, Uri>(page.Image);
+                blogPost.Image = new Values<IImageObject, Uri>(page.WebImage);
             }
 
             if (!string.IsNullOrEmpty(page.Author) && authors.TryGetValue(page.Author, out Person? person))
