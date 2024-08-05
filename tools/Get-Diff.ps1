856c669c8855214589b656886441eaefb317a74f
Max Hamulyák <max@kaylumah.nl>
2024-08-05 20:34:47 +0200

    WIP

--- tools/Get-Diff.ps1
+++ tools/Get-Diff.ps1
@@ -4,13 +4,10 @@
 $diff = git diff --name-only HEAD^ HEAD
 $diff
 
-Write-Host "------------------- histogram:"
-git diff --histogram HEAD^ HEAD
-Write-Host "------------------- stat:"
-git diff --stat HEAD^ HEAD
-Write-Host "------------------- compact:"
-git diff --compact-summary HEAD^ HEAD
-Write-Host "------------------- summary:"
-git diff --summary HEAD^ HEAD
-Write-Host "------------------- name + s:"
-git diff --name-status HEAD^ HEAD
+# git diff --stat HEAD^ HEAD
+# git diff --name-status HEAD^ HEAD
+
+# Example check with regex
+# $SiteFolderDiff = $diff | Where-Object { $_ -match '^_site/' }
+# $HasSite = $SiteFolderDiff.Length -gt 0
+# $HasSite
