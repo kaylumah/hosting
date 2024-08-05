c29125ab1449953e3c4baaf02854154b7fee4834
Max Hamulyák <max@kaylumah.nl>
2024-08-05 20:22:27 +0200

    WIP

--- tools/Get-Diff.ps1
+++ tools/Get-Diff.ps1
@@ -1,1 +1,8 @@
 #Requires -Version 7.4
+
+# See: https://git-scm.com/docs/git-diff
+$diff = git diff --name-only HEAD^ HEAD
+$diff
+
+Write-Host "-------------------"
+git diff --histogram HEAD^ HEAD
