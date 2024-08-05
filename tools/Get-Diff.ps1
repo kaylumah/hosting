cfdacb9f617ee17ef3c5d8f48d874fb6631ea3f4
Max Hamulyák <max@kaylumah.nl>
2024-08-05 20:54:19 +0200

    Check diff (#798)
    * WIP
    
    * wip
    
    * WIP
    
    * WIP
    
    * WIP
    
    * wip
    
    * wip
    
    * WIP
    
    * WIP
    
    * WIP
    
    * wip
    
    * wip
    
    * wip
    
    * WIP
    
    * cleanup
    
    * more cleanup
    
    * cleanup

---
+++ tools/Get-Diff.ps1
@@ -0,0 +1,13 @@
+#Requires -Version 7.4
+
+# See: https://git-scm.com/docs/git-diff
+$diff = git diff --name-only HEAD^ HEAD
+$diff
+
+# git diff --stat HEAD^ HEAD
+# git diff --name-status HEAD^ HEAD
+
+# Example check with regex
+# $SiteFolderDiff = $diff | Where-Object { $_ -match '^_site/' }
+# $HasSite = $SiteFolderDiff.Length -gt 0
+# $HasSite
