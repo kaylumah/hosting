96d0ce8311759281c009222cdee907bbcdebe382
Max Hamulyák <max@kaylumah.nl>
2024-08-06 13:32:24 +0200

    cleanup

--- tools/Get-Diff.ps1
+++ tools/Get-Diff.ps1
@@ -9,11 +9,6 @@ $AssestFolderChanged = $AssetFolderDiff.Length -gt 0
 # git diff --stat HEAD^ HEAD
 # git diff --name-status HEAD^ HEAD
 
-# Example check with regex
-# $SiteFolderDiff = $diff | Where-Object { $_ -match '^_site/' }
-# $HasSite = $SiteFolderDiff.Length -gt 0
-# $HasSite
-
 [PSCustomObject]@{
     ChangedFiles = $DiffOutput
     Assets = $AssestFolderChanged
