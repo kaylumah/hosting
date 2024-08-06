c6aab2bf9508aa472565783fe066e1b7ec684c5d
Max Hamulyák <max@kaylumah.nl>
2024-08-06 13:33:11 +0200

    WIP

--- tools/Get-Diff.ps1
+++ tools/Get-Diff.ps1
@@ -1,13 +1,12 @@
 #Requires -Version 7.4
 
 # See: https://git-scm.com/docs/git-diff
-$DiffOutput = git diff --name-only HEAD^ HEAD
+# $DiffOutput = git diff --name-only HEAD^ HEAD
+$DiffOutput = git diff --name-status HEAD^ HEAD
+# git diff --stat HEAD^ HEAD
 
 $AssetFolderDiff = $diff | Where-Object { $_ -match '^assets/' }
 $AssestFolderChanged = $AssetFolderDiff.Length -gt 0
-
-# git diff --stat HEAD^ HEAD
-# git diff --name-status HEAD^ HEAD
 
 [PSCustomObject]@{
     ChangedFiles = $DiffOutput
