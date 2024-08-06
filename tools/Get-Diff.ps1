1c1c7b154404e90df614824d5c9b92c2436a67ba
Max Hamulyák <max@kaylumah.nl>
2024-08-06 13:52:48 +0200

    fix script

--- tools/Get-Diff.ps1
+++ tools/Get-Diff.ps1
@@ -1,8 +1,8 @@
 #Requires -Version 7.4
 
 # See: https://git-scm.com/docs/git-diff
-# $DiffOutput = git diff --name-only HEAD^ HEAD
-$DiffOutput = git diff --name-status HEAD^ HEAD
+$DiffOutput = git diff --name-only HEAD^ HEAD
+# $DiffOutput = git diff --name-status HEAD^ HEAD
 # git diff --stat HEAD^ HEAD
 
 $AssetFolderDiff = $DiffOutput | Where-Object { $_ -match '^_site/assets/' }
