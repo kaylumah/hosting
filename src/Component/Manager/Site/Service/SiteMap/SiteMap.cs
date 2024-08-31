4a96f2fde9cc6fa9b1b4698b32eb117a64cdb36d
Max Hamulyák <max@kaylumah.nl>
2024-08-31 15:11:04 +0200

    cleanup

--- src/Component/Manager/Site/Service/SiteMap/SiteMap.cs
+++ src/Component/Manager/Site/Service/SiteMap/SiteMap.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Xml;
 
 namespace Kaylumah.Ssg.Manager.Site.Service.SiteMap
@@ -41,5 +42,48 @@ namespace Kaylumah.Ssg.Manager.Site.Service.SiteMap
         Daily,
         Hourly,
         Always
+    }
+
+    public class SiteMapFormatter
+    {
+        readonly SiteMap _SiteMap;
+
+        public SiteMapFormatter(SiteMap siteMap)
+        {
+            _SiteMap = siteMap;
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            ArgumentNullException.ThrowIfNull(writer);
+
+            writer.WriteStartElement(Constants.UrlSetTag, Constants.SiteMapNamespace);
+            WriteItems(writer, _SiteMap.Items);
+        }
+
+        static void WriteItem(XmlWriter writer, SiteMapNode item)
+        {
+            writer.WriteStartElement(Constants.UrlTag);
+            WriteItemContents(writer, item);
+            writer.WriteEndElement();
+        }
+
+        static void WriteItemContents(XmlWriter writer, SiteMapNode item)
+        {
+            writer.WriteElementString(Constants.LocationTag, item.Url);
+            if (item.LastModified.HasValue)
+            {
+                string formatted = item.LastModified.GetValueOrDefault().ToString("yyyy-MM-ddTHH:mm:sszzz", CultureInfo.InvariantCulture);
+                writer.WriteElementString(Constants.LastModifiedTag, formatted);
+            }
+        }
+
+        static void WriteItems(XmlWriter writer, IEnumerable<SiteMapNode> items)
+        {
+            foreach (SiteMapNode item in items)
+            {
+                WriteItem(writer, item);
+            }
+        }
     }
 }
