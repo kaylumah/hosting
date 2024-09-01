2991027e818ded910a8141d3b051c5adc3084662
Max Hamulyák <max@kaylumah.nl>
2024-09-01 12:28:30 +0200

    Fix cross ide run (#838)
    * remove working directory
    
    * WIP
    
    * delete VSCode stuff
    
    * regenerate vscode stuff
    
    * WP
    
    * wip
    
    * iclude

--- src/Component/Manager/Site/Service/Constants.cs
+++ src/Component/Manager/Site/Service/Constants.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Kaylumah, 2024. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System;
 using System.IO;
 
 namespace Kaylumah.Ssg.Manager.Site.Service
@@ -16,8 +17,6 @@ namespace Kaylumah.Ssg.Manager.Site.Service
 
         internal static class Directories
         {
-            internal const string SourceDirectory = "_site";
-            internal const string DestinationDirectory = "dist";
             internal const string AssetDirectory = "assets";
             internal const string DataDirectory = "_data";
             internal const string LayoutDirectory = "_layouts";
@@ -25,6 +24,8 @@ namespace Kaylumah.Ssg.Manager.Site.Service
             internal const string PostDirectory = "_posts";
             internal const string PageDirectory = "_pages";
 
+            internal static readonly string SourceDirectory;
+            internal static readonly string DestinationDirectory;
             internal static readonly string SourcePagesDirectory;
             internal static readonly string SourcePostsDirectory;
             internal static readonly string SourceAssetsDirectory;
@@ -34,6 +35,20 @@ namespace Kaylumah.Ssg.Manager.Site.Service
 
             static Directories()
             {
+                string? value = Environment.GetEnvironmentVariable("TEST");
+                string source = "_site";
+                string output = "dist";
+                if (string.IsNullOrEmpty(value))
+                {
+                    SourceDirectory = source;
+                    DestinationDirectory = output;
+                }
+                else
+                {
+                    SourceDirectory = Path.Combine(value, source);
+                    DestinationDirectory = Path.Combine(value, output);
+                }
+
                 SourcePagesDirectory = Path.Combine(SourceDirectory, PageDirectory);
                 SourcePostsDirectory = Path.Combine(SourceDirectory, PostDirectory);
                 SourceAssetsDirectory = Path.Combine(SourceDirectory, AssetDirectory);
