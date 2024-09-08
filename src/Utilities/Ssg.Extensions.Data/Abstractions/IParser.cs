199fca0c0f1102b3fa462ed4c6cc36564721cfed
Max Hamulyák <max@kaylumah.nl>
2024-09-08 09:26:32 +0200

    initial work

--- src/Utilities/Ssg.Extensions.Data/Abstractions/IParser.cs
+++ src/Utilities/Ssg.Extensions.Data/Abstractions/IParser.cs
@@ -1,10 +1,17 @@
 ﻿// Copyright (c) Kaylumah, 2024. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
+
 namespace Ssg.Extensions.Data.Abstractions
 {
     public interface IParser
     {
         T Parse<T>(string raw);
+    }
+    
+    public interface ICollectionParser
+    {
+        T[] Parse<T>(string raw);
     }
 }
---
+++ test/Unit/File.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Kaylumah, 2024. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+namespace Test.Unit
+{
+    public class File
+    {
+        
+    }
+}
