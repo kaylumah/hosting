de1de52702ad8fec63e490b2a1d7fbc11dfae5d1
Max Hamulyák <max@kaylumah.nl>
2024-11-03 20:40:50 +0100

    wip

--- api-5/WeatherForecastFunction.cs
+++ api-5/WeatherForecastFunction.cs
@@ -17,21 +17,6 @@ namespace Api
             _logger = loggerFactory.CreateLogger<HttpTrigger>();
         }
 
-        /*
-        [Function("fallback")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
-        {
-            // https://github.com/Azure/azure-functions-dotnet-worker/issues/1635
-            // https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=hostbuilder%2Cwindows#aspnet-core-integration
-            // var response = req.CreateResponse(HttpStatusCode.OK);
-            // response.WriteAsJsonAsync("result");
-            // return response;
-            var response = req.CreateResponse(System.Net.HttpStatusCode.Redirect);
-            response.Headers.Add("Location", "/404?originalUrl=");
-            return response;
-        }
-        */
-
         [Function("fallback")]
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
         {
@@ -42,13 +27,13 @@ namespace Api
             Uri? uri = new Uri(originalUrl.ToString());
             _logger.LogInformation(uri.AbsolutePath);
 
-            // RedirectResult result = new RedirectResult($"/404?originalUrl={uri.AbsolutePath}");
-            // result.Permanent = true;
-            // return result;
-
-            // Fallback to 404
-            NotFoundResult result = new NotFoundResult();
+            RedirectResult result = new RedirectResult($"/404?originalUrl={uri.AbsolutePath}");
+            result.Permanent = true;
             return result;
+
+            // Fallback to 404 (does not work)
+            // NotFoundResult result = new NotFoundResult();
+            // return result;
         }
     }
 }
