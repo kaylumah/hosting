8dafd136ea0c0036b90e0ca0a3ebae76db5938c3
Max Hamulyák <max@kaylumah.nl>
2024-12-16 16:08:01 +0100

    cleanup

--- blog-api/src/functions/httpTrigger1.ts
+++ blog-api/src/functions/httpTrigger1.ts
@@ -58,16 +58,6 @@ export async function httpTrigger1(request: HttpRequest, context: InvocationCont
             status: matchedOption.permanent ? 301 : 302,
             headers: { Location: newPath },
         };
-
-        /*
-        const newPath = path.replace(new RegExp(matchedOption.pattern), matchedOption.rewrite);
-        console.log(`Redirecting to: ${newPath}`);
-
-        return {
-            status: matchedOption.permanent ? 301 : 302,
-            headers: { Location: newPath },
-        };
-        */
     } else {
         console.log(`No redirect rule matched. Redirecting to 404 with originalUrl=${path}`);
         return {
@@ -75,24 +65,6 @@ export async function httpTrigger1(request: HttpRequest, context: InvocationCont
             headers: { Location: `/404.html?originalUrl=${encodeURIComponent(path)}` },
         };
     }
-    /*
-    context.log(`Http function processed request for url "${request.url}"`);
-
-    const originalUrl = request.headers.get("x-ms-original-url");
-
-    if (originalUrl) {
-        context.log(`Original URL: ${originalUrl}`);
-    }
-    else {
-        console.log("failed", request);
-    }
-
-    const name = request.query.get('name') || await request.text() || 'world';
-
-    // https://stackoverflow.com/questions/42931965/azure-functions-redirect-header
-    // return { body: `Hello, ${name}!` };
-    return { status: 302, headers: { Location: 'https://www.google.com' } };
-    */
 };
 
 app.http('httpTrigger1', {
