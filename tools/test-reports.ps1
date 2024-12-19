8aa52b6dddd064842d4ca7455afc772bab2f52f2
Max Hamulyák <max@kaylumah.nl>
2024-12-19 09:06:24 +0100

    Bump to net9.0

--- tools/test-reports.ps1
+++ tools/test-reports.ps1
@@ -3,7 +3,7 @@
 [CmdletBinding()]
 param(
     [string] $BuildConfiguration = "Debug",
-    [string] $TargetFramework = "net8.0",
+    [string] $TargetFramework = "net9.0",
     [string] $ReportFile = "LivingDoc.html"
 )
 
