65d7aab495abfaa129cb283d817b85c3074108ee
Max Hamulyák <max@kaylumah.nl>
2024-12-29 13:52:52 +0100

    wip

--- test/Unit/SnapshotTests.cs
+++ test/Unit/SnapshotTests.cs
@@ -1,8 +1,10 @@
 // Copyright (c) Kaylumah, 2024. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Gherkin.CucumberMessages.Types;
 using Ssg.Extensions.Metadata.Abstractions;
 using VerifyXunit;
 using Xunit;
@@ -17,6 +19,49 @@ namespace Test.Unit
             BuildData buildData = (BuildData) RuntimeHelpers.GetUninitializedObject(typeof(BuildData));
             
             SiteMetaData siteMetaData = new SiteMetaData("", "", "", "", "", "", new(), buildData, new());
+            await Verifier.Verify(siteMetaData);
+        }
+        
+        [Fact]
+        public async Task Test2()
+        {
+            BuildData buildData = (BuildData) RuntimeHelpers.GetUninitializedObject(typeof(BuildData));
+            
+            TagMetaDataCollection tagMetaDataCollection = new();
+            Dictionary<string, object> data = new() { { "tags", tagMetaDataCollection } };
+            
+            SiteMetaData siteMetaData = new SiteMetaData("", "", "", "", "", "", data, buildData, new());
+            await Verifier.Verify(siteMetaData);
+        }
+        
+        [Fact]
+        public async Task Test3()
+        {
+            BuildData buildData = (BuildData) RuntimeHelpers.GetUninitializedObject(typeof(BuildData));
+            
+            TagMetaDataCollection tagMetaDataCollection = new();
+            tagMetaDataCollection.Add(new TagMetaData() { Id = "1" });
+            Dictionary<string, object> data = new() { { "tags", tagMetaDataCollection } };
+            
+            SiteMetaData siteMetaData = new SiteMetaData("", "", "", "", "", "", data, buildData, new());
+            await Verifier.Verify(siteMetaData);
+        }
+        
+        [Fact]
+        public async Task Test4()
+        {
+            BuildData buildData = (BuildData) RuntimeHelpers.GetUninitializedObject(typeof(BuildData));
+            
+            List<BasePage> items = new();
+            Dictionary<string, object?> pageData = new()
+            {
+                ["uri"] = "example.html",
+                ["tags"] = new List<object> { "1" }
+            };
+            PageMetaData pageMetaData = new PageMetaData(pageData);
+            items.Add(pageMetaData);
+            
+            SiteMetaData siteMetaData = new SiteMetaData("", "", "", "", "", "", new(), buildData, items);
             await Verifier.Verify(siteMetaData);
         }
     }
