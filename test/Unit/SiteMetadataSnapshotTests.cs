d798da6b8a4bc49bc61bb1567cd7d3411b6b35a0
Max Hamulyák <max@kaylumah.nl>
2025-02-23 14:14:48 +0100

    wip

--- test/Unit/SiteMetadataSnapshotTests.cs
+++ test/Unit/SiteMetadataSnapshotTests.cs
@@ -3,9 +3,11 @@
 
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Kaylumah.Ssg.Manager.Site.Service;
+using Scriban.Runtime;
 using Ssg.Extensions.Metadata.Abstractions;
 using VerifyTests;
 using VerifyXunit;
@@ -150,6 +152,47 @@ namespace Test.Unit
 
             string html = ObjectConversions.ToDiagnosticHtml(siteMetaData, "json");
             await Verifier.Verify(html, _VerifySettings);
+        }
+
+        [Fact(Skip = "just for demo")]
+        public async Task Test_Scriban_Handles_NewIds()
+        {
+            BuildData buildData = (BuildData)RuntimeHelpers.GetUninitializedObject(typeof(BuildData));
+            Dictionary<string, object> data = new();
+            Dictionary<string, object?> pageData = new()
+            {
+                { "id", "1" },
+                { "published", new DateTimeOffset(2025,1,1, 0, 0,0, TimeSpan.Zero) }
+            };
+            Article pageMetaData = new Article(pageData);
+            List<BasePage> items = new List<BasePage>();
+            items.Add(pageMetaData);
+            SiteMetaData siteMetaData = new SiteMetaData(DefaultSiteId, "", "", "", "", "", data, buildData, items);
+
+            object renderData = new { site = siteMetaData };
+
+            string content =
+                """
+                {% assign perYear = site.pagesbyyears[2025] %}
+                {% assign pages = site[perYear] %}
+                {% for tag in pages %}
+                {{ tag.published }}
+                {% endfor %}
+                """;
+            Scriban.Template liquidTemplate = Scriban.Template.ParseLiquid(content);
+            Scriban.LiquidTemplateContext context = new Scriban.LiquidTemplateContext();
+            context.MemberRenamer = member =>
+            {
+                // alternative for the lowercase dictionary
+                string result = member.Name.ToLower(CultureInfo.InvariantCulture);
+                return result;
+            };
+
+            ScriptObject scriptObject = new ScriptObject();
+            scriptObject.Import(renderData);
+            context.PushGlobal(scriptObject);
+
+            string renderedContent = await liquidTemplate.RenderAsync(context);
         }
     }
 }
