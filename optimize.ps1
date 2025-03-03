9ef2766fae78e9fa1c63a81ee47bf9a93a790325
Max Hamulyák <max@kaylumah.nl>
2025-03-03 18:52:30 +0100

    WIP

--- optimize.ps1
+++ optimize.ps1
@@ -34,6 +34,7 @@ Get-ChildItem -Path "dist" -Recurse -Filter "*.js" -File | ForEach-Object {
 Get-ChildItem -Path "dist" -Recurse -Filter "*.html" -File | ForEach-Object {
     $file = $_.FullName
     Write-Output "⚡ Minifying HTML: $file"
+    # npx html-minifier-terser --collapse-whitespace --remove-comments --input-dir dist --output-dir dist --file-ext html --file-ext xml
     npx html-minifier-terser --collapse-whitespace --remove-comments --minify-css true --minify-js true --input-dir (Split-Path -Path $file) --output-dir (Split-Path -Path $file) --file-ext html
 }
 
