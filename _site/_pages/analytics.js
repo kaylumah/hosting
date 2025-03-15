07e310718f1abe44d4ddbc19b52cbb93864b5e0d
Max Hamulyák <max@kaylumah.nl>
2025-03-15 13:04:58 +0100

    Defer loading of Google Analytics (#1043)

---
+++ _site/_pages/analytics.js
@@ -0,0 +1,35 @@
+(function() {
+    let googleAnalyticsID = window.GA_ID || null;
+    if (!googleAnalyticsID || typeof googleAnalyticsID !== "string") {
+        return;
+    }
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+    gtag('config', googleAnalyticsID);
+
+    window.setGtagConsentStatus = function (mode, status) {
+        if (mode !== "update" && mode !== "default") {
+            throw new Error(`Mode ${mode} is not supported!`);
+        }
+        if (status !== "denied" && status !== "granted") {
+            throw new Error(`Status ${status} is not supported!`);
+        }
+        gtag('consent', mode, {
+            'ad_storage': 'denied',
+            'ad_user_data': 'denied',
+            'ad_personalization': 'denied',
+            'analytics_storage': status
+        });
+    };
+
+    if (window.getCookieConsent && window.getCookieConsent() === "granted") {
+        window.setGtagConsentStatus("default", "granted");
+    } else {
+        window.setGtagConsentStatus("default", "denied");
+    }
+})();
