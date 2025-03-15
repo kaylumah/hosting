ec3ac2b99879a7a1d1e33a42e2e2c8a529d5a45e
Max Hamulyák <max@kaylumah.nl>
2025-03-15 12:08:36 +0100

    wip

--- _site/_pages/analytics.js
+++ _site/_pages/analytics.js
@@ -1,31 +1,35 @@
 (function() {
-    let googleAnalyticsID = window.GA_ID;
-    if (googleAnalyticsID !== undefined) {
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){ dataLayer.push(arguments); }
+    let googleAnalyticsID = window.GA_ID || null;
+    if (!googleAnalyticsID || typeof googleAnalyticsID !== "string") {
+        return;
+    }
+    window.dataLayer = window.dataLayer || [];
 
-        gtag('js', new Date());
-        gtag('config', googleAnalyticsID);
+    function gtag() {
+        dataLayer.push(arguments);
+    }
 
-        window.setGtagConsentStatus = function(mode, status) {
-            if (mode !== "update" && mode !== "default") {
-                throw new Error(`Mode ${mode} is not supported!`);
-            }
-            if (status !== "denied" && status !== "granted") {
-                throw new Error(`Status ${status} is not supported!`);
-            }
-            gtag('consent', mode, {
-                'ad_storage': 'denied',
-                'ad_user_data': 'denied',
-                'ad_personalization': 'denied',
-                'analytics_storage': status
-            });
-        };
+    gtag('js', new Date());
+    gtag('config', googleAnalyticsID);
 
-        if (window.getCookieConsent && window.getCookieConsent() === "granted") {
-            window.setGtagConsentStatus("default", "granted");
-        } else {
-            window.setGtagConsentStatus("default", "denied");
+    window.setGtagConsentStatus = function (mode, status) {
+        if (mode !== "update" && mode !== "default") {
+            throw new Error(`Mode ${mode} is not supported!`);
         }
+        if (status !== "denied" && status !== "granted") {
+            throw new Error(`Status ${status} is not supported!`);
+        }
+        gtag('consent', mode, {
+            'ad_storage': 'denied',
+            'ad_user_data': 'denied',
+            'ad_personalization': 'denied',
+            'analytics_storage': status
+        });
+    };
+
+    if (window.getCookieConsent && window.getCookieConsent() === "granted") {
+        window.setGtagConsentStatus("default", "granted");
+    } else {
+        window.setGtagConsentStatus("default", "denied");
     }
 })();
