f3e69abed30f5314c87b8db1518f50eb0019b277
Max Hamulyák <max@kaylumah.nl>
2025-03-21 14:36:03 +0100

    update

--- _site/_pages/analytics.js
+++ _site/_pages/analytics.js
@@ -20,9 +20,9 @@
             throw new Error(`Status ${status} is not supported!`);
         }
         gtag('consent', mode, {
-            'ad_storage': 'denied',
-            'ad_user_data': 'denied',
-            'ad_personalization': 'denied',
+            'ad_storage': status,
+            'ad_user_data': status,
+            'ad_personalization': status,
             'analytics_storage': status
         });
     };
