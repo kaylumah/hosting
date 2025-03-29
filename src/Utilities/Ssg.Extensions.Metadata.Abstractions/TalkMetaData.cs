139fcda90308f9a95e78c7661af091b39fe81612
Max Hamulyák <max@kaylumah.nl>
2025-03-29 21:40:45 +0100

    wip

--- src/Utilities/Ssg.Extensions.Metadata.Abstractions/TalkMetaData.cs
+++ src/Utilities/Ssg.Extensions.Metadata.Abstractions/TalkMetaData.cs
@@ -1,14 +1,24 @@
 // Copyright (c) Kaylumah, 2025. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
 
 namespace Ssg.Extensions.Metadata.Abstractions
 {
     public class TalkMetaData : PublicationMetaData
     {
+        public Uri PresentationUri => GetPresentationUri();
+
         public TalkMetaData(Dictionary<string, object?> internalData) : base(internalData)
         {
+        }
+
+        Uri GetPresentationUri()
+        {
+            string presentationUri = GetString(nameof(PresentationUri));
+            Uri result = new Uri(presentationUri);
+            return result;
         }
     }
 }
