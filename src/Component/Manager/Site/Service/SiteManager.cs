7e7ffadd2ecd4b779be172fd1e6e8536130ea8a1
Max Hamulyák <max@kaylumah.nl>
2025-04-01 19:23:09 +0200

    wipp

--- src/Component/Manager/Site/Service/SiteManager.cs
+++ src/Component/Manager/Site/Service/SiteManager.cs
@@ -307,72 +307,37 @@ namespace Kaylumah.Ssg.Manager.Site.Service
             IEnumerable<IGrouping<string, TextFile>> filesGroupedByType = files.GroupBy(file =>
             {
                 string? type = file.MetaData.GetValue<string?>("type");
-                return type ?? "unknown";
+                return type ?? "unknown" + siteGuid;
             });
             Dictionary<string, List<TextFile>> data = filesGroupedByType
                 .ToDictionary(group => group.Key, group => group.ToList());
 
-            bool hasArticles = data.TryGetValue("Article", out List<TextFile>? articles);
-            bool hasTalks = data.TryGetValue("Talk", out List<TextFile>? talks);
-            bool hasPages = data.TryGetValue("Page", out List<TextFile>? pages);
-            bool hasStatics = data.TryGetValue("Static", out List<TextFile>? statics);
-            bool hasAnnouncements = data.TryGetValue("Announcement", out List<TextFile>? announcements);
-            bool hasCollections = data.TryGetValue("Collection", out List<TextFile>? collection);
-
             List<BasePage> result = new List<BasePage>();
 
-            if (hasStatics && statics != null)
+            Dictionary<string, Func<TextFile, BasePage>> pageParsers = new Dictionary<string, Func<TextFile, BasePage>>(StringComparer.OrdinalIgnoreCase);
+            pageParsers["Static"] = textFile => textFile.ToStatic();
+            pageParsers["Page"] = textFile => textFile.ToPage(siteGuid);
+            pageParsers["Announcement"] = textFile => textFile.ToPage(siteGuid);
+            pageParsers["Article"] = textFile => textFile.ToArticle(siteGuid);
+            pageParsers["Talk"] = textFile => textFile.ToTalk(siteGuid);
+
+            HashSet<string> knownTypes = pageParsers.Keys.ToHashSet(StringComparer.OrdinalIgnoreCase);
+            HashSet<string> seenTypes = data.Keys.ToHashSet(StringComparer.OrdinalIgnoreCase);
+            seenTypes.Remove("Collection");
+            seenTypes.Remove("unknown");
+            List<string> unknownTypes = seenTypes.Except(knownTypes).ToList();
+
+            if (0 < unknownTypes.Count)
             {
-                foreach (TextFile textFile in statics)
-                {
-                    StaticContent staticContent = textFile.ToStatic();
-                    result.Add(staticContent);
-                }
+                throw new InvalidOperationException($"Unmapped metadata types found: {string.Join(", ", unknownTypes)}");
             }
 
-            if (hasPages && pages != null)
-            {
-                foreach (TextFile textFile in pages)
-                {
-                    PageMetaData pageMetaData = textFile.ToPage(siteGuid);
-                    result.Add(pageMetaData);
-                }
-            }
-
-            if (hasAnnouncements && announcements != null)
-            {
-                foreach (TextFile textFile in announcements)
-                {
-                    PageMetaData pageMetaData = textFile.ToPage(siteGuid);
-                    result.Add(pageMetaData);
-                }
-            }
-
-            if (hasArticles && articles != null)
-            {
-                foreach (TextFile textFile in articles)
-                {
-                    ArticleMetaData articleMetaData = textFile.ToArticle(siteGuid);
-                    result.Add(articleMetaData);
-                }
-            }
-
-            if (hasTalks && talks != null)
-            {
-                foreach (TextFile textFile in talks)
-                {
-                    TalkMetaData articleMetaData = textFile.ToTalk(siteGuid);
-                    result.Add(articleMetaData);
-                }
-            }
-
-            if (hasCollections && collection != null)
+            if (data.TryGetValue("Collection", out List<TextFile>? collections))
             {
                 IEnumerable<PublicationMetaData> publicationMetaDataItems = result.OfType<PublicationMetaData>();
                 List<PublicationMetaData> publicationMetaDatas = publicationMetaDataItems.ToList();
-                foreach (TextFile file in collection)
+                foreach (TextFile file in collections)
                 {
-                    // Some parts are regular page data
                     PageMetaData pageMetaData = file.ToPage(siteGuid);
 
                     CollectionPage collectionPage = new CollectionPage(pageMetaData, publicationMetaDatas);
