dbd8c03dc1ea185530359ec2c0d6a4416b9f68e2
Max Hamulyák <max@kaylumah.nl>
2025-04-01 21:34:55 +0200

    fix

--- src/Component/Manager/Site/Service/Seo/StructureDataGenerator.cs
+++ src/Component/Manager/Site/Service/Seo/StructureDataGenerator.cs
@@ -28,11 +28,14 @@ namespace Kaylumah.Ssg.Manager.Site.Service.Seo
 
             Type pageType = renderData.Page.GetType();
 
-            Dictionary<Type, Func<BasePage, Thing>> pageParsers = new Dictionary<Type, Func<BasePage, Thing>>();
-            pageParsers[typeof(PageMetaData)] = page => ToLdJson3(page, renderData.Site);
-            pageParsers[typeof(CollectionPage)] = page => ToLdJson2(page, authors, organizations);
-            pageParsers[typeof(ArticleMetaData)] = page => ToLdJson1(page, authors, organizations);
-            pageParsers[typeof(TalkMetaData)] = page => ToLdJson4(page, authors);
+            Dictionary<Type, Func<BasePage, Thing>> pageParsers = new();
+            pageParsers[typeof(PageMetaData)] = Safe((PageMetaData page) => ToWebPage(page, renderData.Site));
+            pageParsers[typeof(CollectionPage)] = Safe((CollectionPage page) =>
+                page.Uri == "blog.html"
+                    ? ToBlog(page, authors, organizations)
+                    : ToCollectionPage(page));
+            pageParsers[typeof(ArticleMetaData)] = Safe((ArticleMetaData page) => ToBlogPosting(page, authors, organizations));
+            pageParsers[typeof(TalkMetaData)] = Safe((TalkMetaData page) => ToEvent(page, authors));
 
             bool hasConverter = pageParsers.TryGetValue(pageType, out Func<BasePage, Thing>? parser);
             if (hasConverter && parser != null)
@@ -45,53 +48,212 @@ namespace Kaylumah.Ssg.Manager.Site.Service.Seo
             return string.Empty;
         }
 
-        BlogPosting ToLdJson1(BasePage page, Dictionary<AuthorId, Person> authors, Dictionary<OrganizationId, Organization> organizations)
+        static Func<BasePage, Thing> Safe<TPage>(Func<TPage, Thing> handler)
+            where TPage : BasePage
         {
-            if (page is not ArticleMetaData article)
+            return page =>
             {
-                throw new InvalidOperationException();
-            }
+                if (page is not TPage typed)
+                {
+                    throw new InvalidCastException(
+                        $"Expected page of type {typeof(TPage).Name}, but got {page.GetType().Name}");
+                }
 
-            BlogPosting blogPostScheme = ToBlogPosting(article, authors, organizations);
-            return blogPostScheme;
+                Thing result = handler(typed);
+                return result;
+            };
         }
 
-        Thing ToLdJson2(BasePage page, Dictionary<AuthorId, Person> authors, Dictionary<OrganizationId, Organization> organizations)
+        Dictionary<AuthorId, Person> ToPersons(SiteMetaData source)
         {
-            if (page is not CollectionPage collectionPage)
+            Dictionary<AuthorId, Person> result;
+
+            if (source.AuthorMetaData == null)
             {
-                throw new InvalidOperationException();
+                result = [];
+            }
+            else
+            {
+                result = source.AuthorMetaData
+                    .ToDictionary(x => x.Id, x =>
+                    {
+                        List<Uri> uris = new List<Uri>();
+
+                        if (!string.IsNullOrEmpty(x.Links.Linkedin))
+                        {
+                            Uri linkedinUri = new Uri(x.Links.LinkedinProfileUrl!);
+                            uris.Add(linkedinUri);
+                        }
+
+                        if (!string.IsNullOrEmpty(x.Links.Twitter))
+                        {
+                            Uri twitterUri = new Uri(x.Links.TwitterProfileUrl!);
+                            uris.Add(twitterUri);
+                        }
+
+                        Person person = new Person();
+                        person.Name = x.FullName;
+                        person.Email = x.Email;
+                        person.SameAs = new OneOrMany<Uri>(uris);
+
+                        if (!string.IsNullOrEmpty(x.Uri))
+                        {
+                            Uri personUri = source.AbsoluteUri(x.Uri);
+                            person.Url = personUri;
+                        }
+
+                        if (!string.IsNullOrEmpty(x.Picture))
+                        {
+                            Uri image = source.AbsoluteUri(x.Picture);
+                            person.Image = new Values<IImageObject, Uri>(image);
+                        }
+
+                        return person;
+                    });
             }
 
-            if ("blog.html".Equals(collectionPage.Uri, StringComparison.Ordinal))
-            {
-                Blog blogScheme = ToBlog(collectionPage, authors, organizations);
-                return blogScheme;
-            }
-
-            Schema.NET.CollectionPage collectionScheme = ToCollectionPage(collectionPage);
-            return collectionScheme;
+            return result;
         }
 
-        WebPage ToLdJson3(BasePage page, SiteMetaData site)
+        Dictionary<OrganizationId, Organization> ToOrganizations(SiteMetaData source)
         {
-            if (page is not PageMetaData pageMetaData)
+            Dictionary<OrganizationId, Organization> result;
+            if (source.OrganizationMetaData == null)
             {
-                throw new InvalidOperationException();
+                result = [];
+            }
+            else
+            {
+                result = source.OrganizationMetaData
+                    .ToDictionary(x => x.Id, x =>
+                    {
+
+                        List<Uri> uris = new List<Uri>
+                        {
+                            new Uri($"https://www.linkedin.com/company/{x.Linkedin}")
+                        };
+
+                        Organization organization = new Organization();
+                        organization.Name = x.FullName;
+#pragma warning disable RS0030 // not time based
+                        organization.FoundingDate = x.Founded.Date;
+#pragma warning restore RS0030
+                        organization.SameAs = new OneOrMany<Uri>(uris);
+
+                        if (!string.IsNullOrEmpty(x.Logo))
+                        {
+                            Uri logoUri = source.AbsoluteUri(x.Logo);
+                            organization.Logo =
+                                new Values<IImageObject, Uri>(logoUri);
+                        }
+
+                        return organization;
+                    });
             }
 
+            return result;
+        }
+
+        WebPage ToWebPage(PageMetaData pageMetaData, SiteMetaData site)
+        {
             WebSite scheme = new WebSite();
             scheme.Name = pageMetaData.Title;
             scheme.Url = new Uri(site.Url);
             WebPage webPageScheme = new WebPage();
             webPageScheme.Name = pageMetaData.Title;
-            webPageScheme.Url = page.CanonicalUri;
+            webPageScheme.Url = pageMetaData.CanonicalUri;
             webPageScheme.Description = pageMetaData.Description;
             webPageScheme.IsPartOf = scheme;
             return webPageScheme;
         }
 
-        Event ToLdJson4(BasePage page, Dictionary<AuthorId, Person> authors)
+        Schema.NET.CollectionPage ToCollectionPage(CollectionPage page)
+        {
+            List<ICreativeWork> creativeWorks = new List<ICreativeWork>();
+            IEnumerable<ArticleMetaData> articles = page.RecentArticles;
+            foreach (ArticleMetaData article in articles)
+            {
+                BlogPosting blogPosting = new BlogPosting();
+                blogPosting.Headline = article.Title;
+                blogPosting.Url = article.CanonicalUri;
+                creativeWorks.Add(blogPosting);
+            }
+
+            Schema.NET.CollectionPage collectionPage = new Schema.NET.CollectionPage();
+            collectionPage.Url = page.CanonicalUri;
+            collectionPage.Name = page.Title;
+            collectionPage.Description = page.Description;
+            string keywords = string.Join(',', page.Tags);
+            collectionPage.Keywords = keywords;
+            collectionPage.HasPart = new OneOrMany<ICreativeWork>(creativeWorks);
+            return collectionPage;
+        }
+
+        Blog ToBlog(CollectionPage page, Dictionary<AuthorId, Person> authors, Dictionary<OrganizationId, Organization> organizations)
+        {
+            List<BlogPosting> posts = new List<BlogPosting>();
+            IEnumerable<ArticleMetaData> articles = page.RecentArticles;
+            foreach (ArticleMetaData article in articles)
+            {
+                BlogPosting blogPosting = ToBlogPosting(article, authors, organizations);
+                posts.Add(blogPosting);
+            }
+
+            Blog blog = new Blog();
+            blog.Url = page.CanonicalUri;
+            blog.Name = page.Title;
+            blog.Description = page.Description;
+            string keywords = string.Join(',', page.Tags);
+            blog.Keywords = keywords;
+#pragma warning disable RS0030 // DatePublished can be datetime so it is a false positive
+            blog.DatePublished = page.Published;
+            blog.DateModified = page.Modified;
+#pragma warning restore RS0030
+            blog.BlogPost = new OneOrMany<IBlogPosting>(posts);
+            return blog;
+        }
+
+        BlogPosting ToBlogPosting(ArticleMetaData page, Dictionary<AuthorId, Person> authors, Dictionary<OrganizationId, Organization> organizations)
+        {
+            Uri pageUri = page.CanonicalUri;
+            BlogPosting blogPost = new BlogPosting();
+
+            blogPost.MainEntityOfPage = pageUri;
+            blogPost.Url = pageUri;
+
+#pragma warning disable RS0030 // DatePublished can be datetime so it is a false positive
+            blogPost.DatePublished = page.Published;
+            blogPost.DateModified = page.Modified;
+#pragma warning restore RS0030
+
+            blogPost.Headline = page.Title;
+            blogPost.Description = page.Description;
+            string keywords = string.Join(',', page.Tags);
+            blogPost.Keywords = keywords;
+
+            if (page.WebImage != null)
+            {
+                blogPost.Image = new Values<IImageObject, Uri>(page.WebImage);
+            }
+
+            if (!string.IsNullOrEmpty(page.Author) && authors.TryGetValue(page.Author, out Person? person))
+            {
+                blogPost.Author = person;
+            }
+
+            if (!string.IsNullOrEmpty(page.Organization) && organizations.TryGetValue(page.Organization, out Organization? organization))
+            {
+                blogPost.Publisher = organization;
+            }
+
+            // blogPost.Name = page.Title;
+            blogPost.InLanguage = page.Language;
+            blogPost.WordCount = page.NumberOfWords;
+            blogPost.TimeRequired = page.Duration;
+            return blogPost;
+        }
+
+        Event ToEvent(BasePage page, Dictionary<AuthorId, Person> authors)
         {
             if (page is not TalkMetaData talk)
             {
@@ -145,183 +307,6 @@ namespace Kaylumah.Ssg.Manager.Site.Service.Seo
 
             return eventScheme;
 #pragma warning restore
-        }
-
-        public static Dictionary<AuthorId, Person> ToPersons(SiteMetaData source)
-        {
-            Dictionary<AuthorId, Person> result;
-
-            if (source.AuthorMetaData == null)
-            {
-                result = [];
-            }
-            else
-            {
-                result = source.AuthorMetaData
-                    .ToDictionary(x => x.Id, x =>
-                    {
-                        List<Uri> uris = new List<Uri>();
-
-                        if (!string.IsNullOrEmpty(x.Links.Linkedin))
-                        {
-                            Uri linkedinUri = new Uri(x.Links.LinkedinProfileUrl!);
-                            uris.Add(linkedinUri);
-                        }
-
-                        if (!string.IsNullOrEmpty(x.Links.Twitter))
-                        {
-                            Uri twitterUri = new Uri(x.Links.TwitterProfileUrl!);
-                            uris.Add(twitterUri);
-                        }
-
-                        Person person = new Person();
-                        person.Name = x.FullName;
-                        person.Email = x.Email;
-                        person.SameAs = new OneOrMany<Uri>(uris);
-
-                        if (!string.IsNullOrEmpty(x.Uri))
-                        {
-                            Uri personUri = source.AbsoluteUri(x.Uri);
-                            person.Url = personUri;
-                        }
-
-                        if (!string.IsNullOrEmpty(x.Picture))
-                        {
-                            Uri image = source.AbsoluteUri(x.Picture);
-                            person.Image = new Values<IImageObject, Uri>(image);
-                        }
-
-                        return person;
-                    });
-            }
-
-            return result;
-        }
-
-        public static Dictionary<OrganizationId, Organization> ToOrganizations(SiteMetaData source)
-        {
-            Dictionary<OrganizationId, Organization> result;
-            if (source.OrganizationMetaData == null)
-            {
-                result = [];
-            }
-            else
-            {
-                result = source.OrganizationMetaData
-                    .ToDictionary(x => x.Id, x =>
-                    {
-
-                        List<Uri> uris = new List<Uri>
-                        {
-                            new Uri($"https://www.linkedin.com/company/{x.Linkedin}")
-                        };
-
-                        Organization organization = new Organization();
-                        organization.Name = x.FullName;
-#pragma warning disable RS0030 // not time based
-                        organization.FoundingDate = x.Founded.Date;
-#pragma warning restore RS0030
-                        organization.SameAs = new OneOrMany<Uri>(uris);
-
-                        if (!string.IsNullOrEmpty(x.Logo))
-                        {
-                            Uri logoUri = source.AbsoluteUri(x.Logo);
-                            organization.Logo =
-                                new Values<IImageObject, Uri>(logoUri);
-                        }
-
-                        return organization;
-                    });
-            }
-
-            return result;
-
-        }
-
-        public static Schema.NET.CollectionPage ToCollectionPage(CollectionPage page)
-        {
-            List<ICreativeWork> creativeWorks = new List<ICreativeWork>();
-            IEnumerable<ArticleMetaData> articles = page.RecentArticles;
-            foreach (ArticleMetaData article in articles)
-            {
-                BlogPosting blogPosting = new BlogPosting();
-                blogPosting.Headline = article.Title;
-                blogPosting.Url = article.CanonicalUri;
-                creativeWorks.Add(blogPosting);
-            }
-
-            Schema.NET.CollectionPage collectionPage = new Schema.NET.CollectionPage();
-            collectionPage.Url = page.CanonicalUri;
-            collectionPage.Name = page.Title;
-            collectionPage.Description = page.Description;
-            string keywords = string.Join(',', page.Tags);
-            collectionPage.Keywords = keywords;
-            collectionPage.HasPart = new OneOrMany<ICreativeWork>(creativeWorks);
-            return collectionPage;
-        }
-
-        public static Blog ToBlog(CollectionPage page, Dictionary<AuthorId, Person> authors, Dictionary<OrganizationId, Organization> organizations)
-        {
-            List<BlogPosting> posts = new List<BlogPosting>();
-            IEnumerable<ArticleMetaData> articles = page.RecentArticles;
-            foreach (ArticleMetaData article in articles)
-            {
-                BlogPosting blogPosting = ToBlogPosting(article, authors, organizations);
-                posts.Add(blogPosting);
-            }
-
-            Blog blog = new Blog();
-            blog.Url = page.CanonicalUri;
-            blog.Name = page.Title;
-            blog.Description = page.Description;
-            string keywords = string.Join(',', page.Tags);
-            blog.Keywords = keywords;
-#pragma warning disable RS0030 // DatePublished can be datetime so it is a false positive
-            blog.DatePublished = page.Published;
-            blog.DateModified = page.Modified;
-#pragma warning restore RS0030
-            blog.BlogPost = new OneOrMany<IBlogPosting>(posts);
-            return blog;
-        }
-
-        public static BlogPosting ToBlogPosting(ArticleMetaData page, Dictionary<AuthorId, Person> authors, Dictionary<OrganizationId, Organization> organizations)
-        {
-            Uri pageUri = page.CanonicalUri;
-            BlogPosting blogPost = new BlogPosting();
-
-            blogPost.MainEntityOfPage = pageUri;
-            blogPost.Url = pageUri;
-
-#pragma warning disable RS0030 // DatePublished can be datetime so it is a false positive
-            blogPost.DatePublished = page.Published;
-            blogPost.DateModified = page.Modified;
-#pragma warning restore RS0030
-
-            blogPost.Headline = page.Title;
-            blogPost.Description = page.Description;
-            string keywords = string.Join(',', page.Tags);
-            blogPost.Keywords = keywords;
-
-            if (page.WebImage != null)
-            {
-                blogPost.Image = new Values<IImageObject, Uri>(page.WebImage);
-            }
-
-            if (!string.IsNullOrEmpty(page.Author) && authors.TryGetValue(page.Author, out Person? person))
-            {
-                blogPost.Author = person;
-            }
-
-            if (!string.IsNullOrEmpty(page.Organization) && organizations.TryGetValue(page.Organization, out Organization? organization))
-            {
-                blogPost.Publisher = organization;
-            }
-
-            // blogPost.Name = page.Title;
-            blogPost.InLanguage = page.Language;
-            blogPost.WordCount = page.NumberOfWords;
-            blogPost.TimeRequired = page.Duration;
-            return blogPost;
         }
     }
 }
