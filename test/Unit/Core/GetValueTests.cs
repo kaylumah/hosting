9b886bb4ec7632bef32c79b83cd9b8b06ec05ed6
Max Hamulyák <max@kaylumah.nl>
2025-04-11 21:22:03 +0200

    WIP

--- test/Unit/Core/GetValueTests.cs
+++ test/Unit/Core/GetValueTests.cs
@@ -10,61 +10,6 @@ using Xunit;
 
 namespace Test.Unit.Core
 {
-    public static class SharedTestData
-    {
-        static object GetSampleValue(Type type)
-        {
-            // todo faker?
-            Type t = Nullable.GetUnderlyingType(type) ?? type;
-
-            return t switch
-            {
-                _ when t == typeof(string) => "Value",
-                _ when t == typeof(int) => 42,
-                _ => throw new NotSupportedException($"No fuzz input for {type}")
-            };
-        }
-
-        public static IEnumerable<object?[]> ValueForTypeTestData()
-        {
-            Type[] types = ConversionCapabilityHelper.WithNullableCounterparts([
-                typeof(string),
-                typeof(int)
-            ]);
-
-            foreach (Type type in types)
-            {
-                object value = GetSampleValue(type);
-                object?[] result = [type, value];
-                yield return result;
-            }
-        }
-        
-        public static IEnumerable<object?[]> DefaultValueForNullValueTestData()
-        {
-            Type[] types = ConversionCapabilityHelper.WithNullableCounterparts(
-                [
-                    typeof(string),
-                    typeof(int),
-                    typeof(bool),
-                    typeof(Guid),
-#pragma warning disable RS0030
-                    typeof(DateTime),
-#pragma warning restore RS0030
-                    typeof(Uri)
-
-                ]
-            );
-
-            foreach (Type type in types)
-            {
-                object? defaultValue = type.DefaultForType();
-                object?[] result = [type, defaultValue];
-                yield return result;
-            }
-        }
-    }
-    
     public class GetValueTests
     {
         static readonly ConcurrentDictionary<Type, MethodInfo> _GetValueMethods;
