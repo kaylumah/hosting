71e2a5977f1d177a7467eac2567ca7ecc49abfd8
Max Hamulyák <max@kaylumah.nl>
2025-04-20 13:06:43 +0200

    WIP

--- test/Unit/ParserTests.cs
+++ test/Unit/ParserTests.cs
@@ -1,11 +1,14 @@
 // Copyright (c) Kaylumah, 2025. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
 using Kaylumah.Ssg.Extensions.Data.Abstractions;
+using IParser = Kaylumah.Ssg.Extensions.Data.Abstractions.IParser;
 using Kaylumah.Ssg.Extensions.Data.Csv;
 using Kaylumah.Ssg.Extensions.Data.Json;
 using Kaylumah.Ssg.Extensions.Data.Yaml;
 using Xunit;
+using YamlDotNet.Core;
 
 namespace Test.Unit
 {
@@ -16,45 +19,65 @@ namespace Test.Unit
         {
             ICollectionParser csvParser = new CsvParser();
         }
+
+        // TODO NULL string
+        // TODO DTO Parsing
         
         [Fact]
-        public void Test_YamlParser_1()
+        public void Test_YamlParser_EmptyString()
         {
             IYamlParser yamlParser = new YamlParser();
-
-            string input = string.Empty; // returns null
-            input = "foo: bar\n- invalidIndent"; // YamlDotNet.Core.YamlException
+            string input = """
+                           
+                           """;
             object result = yamlParser.Parse<object>(input);
-            // dictionary<string, object> // dictionary<string, object?>
-            // DTO
-            // Extra Properties (Ignored)
-            // (e.g., string instead of int)
+            Assert.Null(result);
         }
+        
+        [Fact]
+        public void Test_YamlParser_AsObjectReturnsDictionary()
+        {
+            IYamlParser yamlParser = new YamlParser();
+            string input = """
+                           title: doc1
+                           """;
+            object result = yamlParser.Parse<object>(input);
+            Assert.IsType<Dictionary<object, object>>(result);
+        }
+        
+        [Fact]
+        public void Test_YamlParser_ExplicitReturnsDictionary()
+        {
+            IYamlParser yamlParser = new YamlParser();
+            string input = """
+                           title: doc1
+                           """;
+            Dictionary<string, object> result = yamlParser.Parse<Dictionary<string, object>>(input);
+            Assert.IsType<Dictionary<string, object>>(result);
+        }
+        
+        [Fact]
+        public void Test_YamlParser_IdentThrows()
+        {
+            // Does not throw?
+            IYamlParser yamlParser = new YamlParser();
+            string input = """
+                           key1: value1
+                            key2: 123
+                           """;
+            // object result = yamlParser.Parse<object>(input);
+            input = "{}";
+            Assert.Throws<SemanticErrorException>(() => yamlParser.Parse<object>(input));
+        }
+        
+        // Pass {} [] "Hello World"
+        // Valid JSON will work
+        // Invalid JSON will not { title: "missing-quotes" }
         
         [Fact]
         public void Test3()
         {
             IParser jsonParser = new JsonParser();
         }
-        
-        /*
-         * [Fact]
-           public void Test_YamlParser_Parse_ReturnsNullOnEmptyInput()
-           {
-               string input = string.Empty;
-               Dictionary<string, object> result = _Sut.Parse<Dictionary<string, object>>(input);
-               result.Should().BeNull();
-           }
-
-           [Fact]
-           public void Test_YamlParser_Parse_CanReturnDictionary()
-           {
-               string input = "title: doc1";
-               Dictionary<string, object> result = _Sut.Parse<Dictionary<string, object>>(input);
-               result.Should().NotBeNull();
-               result.ContainsKey("title").Should().BeTrue();
-               result["title"].Should().Be("doc1");
-           }
-         */
     }
 }
