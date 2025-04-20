acd8060245dc5a5d5c25beff10ca6fb8d3de3a30
Max Hamulyák <max@kaylumah.nl>
2025-04-20 13:11:55 +0200

    WP

--- test/Unit/ParserTests.cs
+++ test/Unit/ParserTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Kaylumah, 2025. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
 using Kaylumah.Ssg.Extensions.Data.Abstractions;
 using IParser = Kaylumah.Ssg.Extensions.Data.Abstractions.IParser;
@@ -12,6 +13,179 @@ using YamlDotNet.Core;
 
 namespace Test.Unit
 {
+    #pragma warning disable
+    public class YamlParserTests
+    {
+        private readonly IYamlParser _parser = new YamlParser();
+
+        public class TestDto
+        {
+            public string? Title { get; set; }
+            public int Count { get; set; }
+        }
+
+        [Fact]
+        public void Parse_EmptyString_ReturnsNull()
+        {
+            string input = """
+                           
+                           """;
+            object? result = _parser.Parse<object>(input);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Parse_NullString_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => _parser.Parse<object>(null!));
+        }
+
+        [Fact]
+        public void Parse_YamlMapping_AsObject_ReturnsDictionaryObjectObject()
+        {
+            string input = """
+                           title: doc1
+                           """;
+            object result = _parser.Parse<object>(input);
+            Assert.IsType<Dictionary<object, object>>(result);
+        }
+
+        [Fact]
+        public void Parse_YamlMapping_AsStringObjectDictionary_ReturnsTypedDictionary()
+        {
+            string input = """
+                           title: doc1
+                           """;
+            var result = _parser.Parse<Dictionary<string, object>>(input);
+            Assert.Equal("doc1", result["title"]);
+        }
+
+        [Fact]
+        public void Parse_YamlMapping_AsDto_ReturnsPopulatedDto()
+        {
+            string input = """
+                           title: doc1
+                           count: 42
+                           """;
+            var result = _parser.Parse<TestDto>(input);
+            Assert.Equal("doc1", result.Title);
+            Assert.Equal(42, result.Count);
+        }
+
+        [Fact]
+        public void Parse_YamlWithExtraProperty_IgnoresExtraAndParsesDto()
+        {
+            string input = """
+                           title: doc1
+                           count: 1
+                           extra: ignored
+                           """;
+            var result = _parser.Parse<TestDto>(input);
+            Assert.Equal("doc1", result.Title);
+            Assert.Equal(1, result.Count);
+        }
+
+        [Fact]
+        public void Parse_YamlWithIndentError_ThrowsSyntaxError()
+        {
+            string input = """
+                           key1: value1
+                             key2: 123
+                           """;
+            Assert.Throws<YamlDotNet.Core.SyntaxErrorException>(() => _parser.Parse<object>(input));
+        }
+
+        [Fact]
+        public void Parse_JsonObject_AsDictionary_ReturnsStringObjectDictionary()
+        {
+            string input = """
+                           {
+                             "title": "json-doc",
+                             "count": 5
+                           }
+                           """;
+            var result = _parser.Parse<Dictionary<string, object>>(input);
+            Assert.Equal("json-doc", result["title"]);
+            Assert.Equal(5, Convert.ToInt32(result["count"]));
+        }
+
+        [Fact]
+        public void Parse_JsonObject_AsDto_ReturnsPopulatedDto()
+        {
+            string input = """
+                           {
+                             "title": "json-dto",
+                             "count": 10
+                           }
+                           """;
+            var result = _parser.Parse<TestDto>(input);
+            Assert.Equal("json-dto", result.Title);
+            Assert.Equal(10, result.Count);
+        }
+
+        [Fact]
+        public void Parse_InvalidJson_TrailingComma_ThrowsSyntaxError()
+        {
+            string input = """
+                           {
+                             "title": "bad json",
+                           }
+                           """;
+            Assert.Throws<YamlDotNet.Core.SyntaxErrorException>(() => _parser.Parse<object>(input));
+        }
+
+        [Fact]
+        public void Parse_JsonStringScalar_ReturnsRawString()
+        {
+            string input = """
+                           "hello"
+                           """;
+            var result = _parser.Parse<string>(input);
+            Assert.Equal("hello", result);
+        }
+
+        [Fact]
+        public void Parse_JsonArray_ReturnsIntegerList()
+        {
+            string input = """
+                           [1, 2, 3]
+                           """;
+            var result = _parser.Parse<List<int>>(input);
+            Assert.Equal(new[] { 1, 2, 3 }, result);
+        }
+
+        [Fact]
+        public void Parse_EmptyObjectMap_AsStringObjectDictionary_ReturnsEmpty()
+        {
+            string input = """
+                           {}
+                           """;
+            var result = _parser.Parse<Dictionary<string, object>>(input);
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Parse_YamlWithExplicitNullValue_ReturnsNullInDictionary()
+        {
+            string input = """
+                           title: null
+                           """;
+            var result = _parser.Parse<Dictionary<string, object?>>(input);
+            Assert.Null(result["title"]);
+        }
+
+        [Fact]
+        public void Parse_YamlTypeMismatch_ThrowsDeserializationException()
+        {
+            string input = """
+                           title: abc
+                           count: not-a-number
+                           """;
+            Assert.Throws<YamlDotNet.Core.YamlException>(() => _parser.Parse<TestDto>(input));
+        }
+    }
+    
     public class ParserTests
     {
         [Fact]
