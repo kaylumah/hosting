f3d020d6839407d1b7d36a069353b669e72083ff
Max Hamulyák <max@kaylumah.nl>
2025-06-28 09:41:30 +0200

    Cleanup

--- src/iFX/Data/Abstractions/ICollectionParser.cs
+++ src/iFX/Data/Abstractions/ICollectionParser.cs
@@ -1,10 +1,10 @@
-﻿// Copyright (c) Kaylumah, 2025. All rights reserved.
+// Copyright (c) Kaylumah, 2025. All rights reserved.
 // See LICENSE file in the project root for full license information.
 
 namespace Kaylumah.Ssg.iFX.Data.Abstractions
 {
-    public interface IParser
+    public interface ICollectionParser
     {
-        T Parse<T>(string raw);
+        T[] Parse<T>(string raw);
     }
 }
