4f1d1a11ebae963921d89ee40237a0b3ac5bd265
Max Hamulyák <max@kaylumah.nl>
2025-07-06 11:43:21 +0200

    wip

--- test/Unit/Extensions/MappingExtensions.cs
+++ test/Unit/Extensions/MappingExtensions.cs
@@ -50,7 +50,8 @@ namespace Test.Unit.Extensions
             pageDictionary.SetValue(nameof(ArticlePublicationPageMetaData.Feed), "true");
             pageDictionary.SetValue(nameof(PageMetaData.Sitemap), "true");
             pageDictionary.SetValue(nameof(PageMetaData.Layout), "default.html");
-            pageDictionary.SetValue(nameof(PageMetaData.Keywords), tags);
+            pageDictionary.SetValue(nameof(PublicationPageMetaData.Keywords), tags);
+            pageDictionary.SetValue(nameof(PublicationPageMetaData.Tags), tags);
             PageMetaData result = new PageMetaData(pageDictionary);
             return result;
         }
