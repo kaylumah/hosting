d7b96de7c2c370392d5a09a9d9ac30e82569f312
Max Hamulyák <max@kaylumah.nl>
2025-08-02 12:10:19 +0200

    WIP

--- _site/_pages/highlight.js
+++ _site/_pages/highlight.js
@@ -8,4 +8,11 @@
     }
 
     console.log("code-blocks", blocks);
+    
+    let observer = new IntersectionObserver((entries) => {
+        console.log("entries", entries);
+    }, {threshold: 0.1});
+
+    blocks.forEach(block => observer.observe(block));
+
 })();
