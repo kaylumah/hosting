e3dd1ae4dce138fb6adb83686f066ee1b3d1b125
Max Hamulyák <max@kaylumah.nl>
2025-08-02 12:08:23 +0200

    WIP

--- _site/_pages/highlight.js
+++ _site/_pages/highlight.js
@@ -2,5 +2,10 @@
     console.log("loaded highlight.js");
 
     const blocks = document.querySelectorAll('code[class^="language-"]');
+    if (!blocks.length) 
+    {
+        return;
+    }
+
     console.log("code-blocks", blocks);
 })();
